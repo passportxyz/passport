1ab7a16b4305b2e58fc4d8c45e56bbe80ae0f024
Shavina Chau <shavina.chau@gmail.com>
2022-03-29 10:38:26 -0500

    style: add eslint and prettier configs
    fixes issue #14

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
       <header className="App-header">
