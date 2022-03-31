5526a7f9f4e4483599d1d4b05af69c641292a333
Shavina Chau <shavina.chau@gmail.com>
2022-03-31 18:07:36 -0500

    test(vite-app): update App.spec.tsx test to assert on mocked web3-onboard/react fns
    also make minor edits to App.tsx to address a few linter warnings and errors

--- vite-app/src/App.spec.tsx
+++ vite-app/src/App.spec.tsx
@@ -1,37 +1,36 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import App from "./App";
+import { useConnectWallet } from "@web3-onboard/react";
 
 jest.mock("./utils/onboard.ts");
 
 test("renders connect wallet button", () => {
   expect.assertions(1);
   render(<App />);
-  const connectWalletButton = screen.getByTestId("connectWalletButton");
+  const connectWalletButton = screen.getByRole("button", {
+    name: "Connect",
+  });
   expect(connectWalletButton).toBeInTheDocument();
 });
 
-test("clicking connect wallet button renders wallet selection modal", async () => {
-  expect.assertions(2);
+test("clicking connect wallet button calls useConnectWallet:connect function", async () => {
+  expect.assertions(1);
 
   render(<App />);
-  const connectWalletButton = screen.getByTestId("connectWalletButton");
+  const connectWalletButton = screen.getByRole("button", {
+    name: "Connect",
+  });
 
-  // Click button
   userEvent.click(connectWalletButton);
 
-  const walletSelectionModal = await screen.findByText(
-    "Select your wallet from the options to get started",
-    {},
-    {
-      timeout: 100,
-    },
-  );
-  expect(walletSelectionModal).toBeInTheDocument();
-
-  const metamaskButton = await screen.getByRole("button", {
-    name: "MetaMask",
+  await waitFor(() => {
+    const [, connect] = useConnectWallet();
+    expect(connect).toBeCalledTimes(1);
   });
-  expect(metamaskButton).toBeInTheDocument();
 });
+
+// TODO tests
+// when localstorage state has previously connected wallet, restore wallet from localstorage
+// simulate @web3-onboard/react state updates when connecting the wallet -> setting address, accounts, etc
