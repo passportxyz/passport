764075e15c29940a6c2317b2e54930b9ae61b32e
Shavina Chau <shavina.chau@gmail.com>
2022-03-31 11:18:14 -0500

    refactor: create vite-app package as alternative to app, use onboard instead of web3modal
    in app package:  turn down some eslint rules to warning level
    create vite-app package: init vite config, use onboard instead of
    web3modal
    
    re #18

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Web3Modal from "web3modal";
+// import WalletConnectProvider from "@walletconnect/web3-provider";
 
-function App(): JSX.Element {
+function App() {
+  const web3Modal = new Web3Modal({
+    network: "rinkeby",
+    providerOptions: {},
+    disableInjectedProvider: false,
+  });
+
+  const web3ModalConnect = async (): Promise<void> => {
+    console.log("inside button");
+    if (web3Modal) {
+      console.log("about to connect");
+      const provider = await web3Modal.connect();
+      // const wrappedProvider = new Web3Provider(provider);
+      // return wrappedProvider;
+      console.log("connected");
+      console.log(provider);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
@@ -10,6 +29,7 @@ function App(): JSX.Element {
         <button
           data-testid="connectWalletButton"
           className="bg-yellow-100 text-gray-900"
+          onClick={web3ModalConnect}
         >
           Connect Wallet
         </button>
