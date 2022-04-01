6b7f17b7a50a202c6f14c2e621da5de551690eae
fahim Amrakori <fahimamrakori@fahims-MacBook-Pro.local>
2022-04-01 11:40:58 -0400

    tailwind style updates

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import dpoppLogofrom from "./assets/dpoppLogo.svg"
 
 import { initWeb3Onboard } from "./utils/onboard";
 import { useConnectWallet, useSetChain, useWallets } from "@web3-onboard/react";
@@ -57,11 +58,12 @@ function App() {
     ) as string[];
     if (previouslyConnectedWallets?.length) {
       /* eslint-disable no-inner-declarations */
+      ////TURN MODAL BACK ON
       async function setWalletFromLocalStorage() {
         await connect({
           autoSelect: {
             label: previouslyConnectedWallets[0],
-            disableModals: false,
+            disableModals: true,
           },
         });
       }
@@ -82,23 +84,46 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
-      <button
-        data-testid="connectWalletButton"
-        className="bg-yellow-100 text-gray-900"
-        onClick={handleConnection}
-      >
-        {address ? `Disconnect from ${label || ""}` : "Connect"}
-      </button>
-      {connectedWallets.map(({ label, accounts }) => {
-        return (
-          <div key={label}>
-            <div>{label}</div>
-            <div>Accounts: {JSON.stringify(accounts, null, 2)}</div>
+    <div className="bg-violet-700 font-librefranklin text-gray-100 min-h-max font-miriam-libre min-h-default">
+      <div className="container px-5 py-24 mx-auto">
+        <div className="mx-auto flex flex-wrap">
+          <div className="w-1/2 w-full py-6 mb-6">
+              <img src={dpoppLogofrom} className="App-logo" alt="logo" />
+                <div className="font-miriam-libre text-gray-050 mt-10 font-normal font-bold leading-relaxed">
+                  <p className="text-6xl">
+                  Gitcoin
+                  <br/>
+                  ID Passport
+                  </p>
+                </div>
+                <div className="font-libre-franklin w-1/3 mt-10">Gitcoin ID Passport is an identity aggregator of the top 
+                  identity providers in the web3 space into one transportable 
+                  identity that proves your personhood.
+                </div>
+
+                <div className="mb-10 mt-10">
+                <button
+                  data-testid="connectWalletButton"
+                  className="bg-gray-100 text-violet-500 rounded-lg py-4 px-20"
+                  onClick={handleConnection}
+                >
+                  <p className="text-base">{address ? `Disconnect from ${label || ""}` : "Get Started"}</p>
+                </button>
+                  {connectedWallets.map(({ label, accounts }) => {
+                    return (
+                      <div key={label}>
+                        <div>{label}</div>
+                        <div>Accounts: {JSON.stringify(accounts, null, 2)}</div>
+                      </div>
+                    );
+                  })}
+                </div>
+
+                <a className="underline">Why using wallet as identity?</a>
           </div>
-        );
-      })}
+          <div className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"></div>
+        </div>
+      </div>
     </div>
   );
 }
