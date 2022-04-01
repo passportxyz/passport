6188950fd769a58507f343c3d35095349941baa7
Shavina Chau <shavina.chau@gmail.com>
2022-04-01 14:24:03 -0500

    refactor(app): remove app package in favor of vite-app. remove app from husky hooks

--- app/.prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  printWidth: 80,
-  semi: true,
-  tabWidth: 2,
-  trailingComma: "all",
-};
--- app/src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders connect wallet button", () => {
-  render(<App />);
-  const connectWalletButton = screen.getByTestId("connectWalletButton");
-  expect(connectWalletButton).toBeInTheDocument();
-});
