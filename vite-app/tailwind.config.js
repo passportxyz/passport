b42aa8fcae60c8c30e98f06971cadf5061e56efa
fahim Amrakori <fahimamrakori@fahims-MacBook-Pro.local>
2022-04-01 11:40:58 -0400

    tailwind style updates

---
+++ vite-app/tailwind.config.js
@@ -0,0 +1,25 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        purple: {
+          darkpurple: "#0B0228",
+        },
+      },
+    },
+    fontFamily: {
+      miriamlibre: ["miriam libre"],
+      librefranklin: ["Libre Franklin"],
+      body: ['"Libre Franklin"'],
+    },
+    minHeight: {
+      default: "2000px",
+    },
+  },
+  plugins: [],
+}
