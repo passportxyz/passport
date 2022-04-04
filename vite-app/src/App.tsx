16b4b2a366f54fd6bfe78de7446685108957cbc4
farque65 <fahim.farque@gmail.com>
2022-04-04 10:36:28 -0400

    style: added responsive styling

--- vite-app/src/App.tsx
+++ vite-app/src/App.tsx
@@ -145,15 +145,15 @@ function App() {
                 ID Passport
               </p>
             </div>
-            <div className="font-libre-franklin w-1/3 mt-10">
+            <div className="font-libre-franklin md:w-1/3 mt-10 text-xl">
               Gitcoin ID Passport is an identity aggregator of the top identity providers in the web3 space into one
               transportable identity that proves your personhood.
             </div>
 
-            <div className="mb-10 mt-10">
+            <div className="mb-10 mt-10 md:w-1/4">
               <button
                 data-testid="connectWalletButton"
-                className="bg-gray-100 text-violet-500 rounded-lg py-4 px-20"
+                className="bg-gray-100 text-violet-500 rounded-lg py-4 px-20 min-w-full"
                 onClick={handleConnection}
               >
                 <p className="text-base">{address ? `Disconnect from ${label || ''}` : 'Get Started'}</p>
