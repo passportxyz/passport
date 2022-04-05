0c97f4fa9e42365775d38dfb1e5d4cdd5c0cd56e
David Hayes <david@focusedlabs.io>
2022-04-05 17:25:26 -0500

    add unit test stubs; remove rule silencing from index.ts

--- iam/src/__tests__/index.test.ts
+++ iam/src/__tests__/index.test.ts
@@ -41,39 +41,11 @@ describe("POST /challenge", function () {
     // expect(response.status).toEqual(200);
     expect(response.body).toEqual(expectedResponse);
   });
+
+  // TODO new unit test --> if credential.error then return 400
+  // TODO new unit test --> if !(await verifyCredential(DIDKit, credential)) then return 400
+  // TODO new unit test --> if utils.getAddress fails, then ???
+  // TODO new unit test --> if issueMerkleCredential fails, then ???
 });
 
-//✅ MerkleProof verifiable contains the passed in username (test)
-
-// ✅ Credential has verifiably been issued by did:key:z6Mkmhp2sE9s4AxFrKUXQjcNxbDV7WTM8xdh1FDNmNDtogdw
-
-/*
-{
-    "@context": [
-        "https://www.w3.org/2018/credentials/v1"
-    ],
-    "type": [
-        "VerifiableCredential"
-    ],
-    "credentialSubject": {
-        "id": "did:ethr:0xd23e91f4252d57aF484CE64BD02F3c693a9F83D9#Simple",
-        "root": "SZYY9hw6+iBFH8YI0GYalosmpOfGDgVnBcMpjLcsnqQ=",
-        "@context": [
-            {
-                "root": "https://schema.org/Text"
-            }
-        ]
-    },
-    "issuer": "did:key:z6Mkmhp2sE9s4AxFrKUXQjcNxbDV7WTM8xdh1FDNmNDtogdw",
-    "issuanceDate": "2022-04-05T19:27:40.358Z",
-    "proof": {
-        "type": "Ed25519Signature2018",
-        "proofPurpose": "assertionMethod",
-        "verificationMethod": "did:key:z6Mkmhp2sE9s4AxFrKUXQjcNxbDV7WTM8xdh1FDNmNDtogdw#z6Mkmhp2sE9s4AxFrKUXQjcNxbDV7WTM8xdh1FDNmNDtogdw",
-        "created": "2022-04-05T19:27:40.358Z",
-        "jws": "eyJhbGciOiJFZERTQSIsImNyaXQiOlsiYjY0Il0sImI2NCI6ZmFsc2V9..NvVCewNj8GzgUPdmelVqi5U4VJo6P6tWgu11gvW99s2wmsfZjBarkm7zNi2rPVIKJBceOo09YXGnQuknWa_SCQ"
-    },
-    "expirationDate": "2022-05-05T19:27:40.358Z"
-}
-
-*/
+// TODO test POST /verify endpoint
