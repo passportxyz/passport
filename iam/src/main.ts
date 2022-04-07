d8d891540f3f4fbd7758919f52ba51cfefab81e8
Shavina Chau <shavina.chau@gmail.com>
2022-04-07 08:42:25 -0500

    fix(iam): set cors in initial setup instead of only in main file

--- iam/src/main.ts
+++ iam/src/main.ts
@@ -1,14 +1,8 @@
 // ---- Main App from index
 import { app } from "./index";
 
-// ---- Production plugins
-import cors from "cors";
-
 // default port to listen on
 const port = 65535;
-
-// set cors to accept calls from anywhere
-app.use(cors());
 
 // start the Express server
 app.listen(port, () => {
