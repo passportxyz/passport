8db1aaf1fe47894e2201ad01daf8e8c02c17f495
Shavina Chau <shavina.chau@gmail.com>
2022-04-07 13:53:59 -0500

    refactor(app): add react context to app
    - change esModuleInterop flag to true in tsconfig to address react undefined in tests
    - moved verification and issuance from Home into Passport
    - update Home and Passport tests to add context providers
    - set route redirects on connectedWallets instead of loggedIn
    
    [#18]

---
+++ app/src/__tests__/App.test.tsx
@@ -0,0 +1,8 @@
+// if you have not connected your wallet before, show home page
+// if you have connected your wallet before, redirect to dashboard
+/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
+import React from "react";
+
+it("placeholder", () => {
+  expect(true).toEqual(true);
+});
