095d327e731680548c7854199322267d8f091502
Shavina Chau <shavina.chau@gmail.com>
2022-04-12 16:41:46 -0500

    test(identity): refactor axios and didkit mock usage in credentials test
    - export values used in axios mock
    - export mock helpers in axios and didkit mocks
    - mock generateMerkle() in credential test
    - extract THIRTY_DAYS_TO_SECONDS constant in credentials implementation

--- identity/__mocks__/didkit.js
+++ identity/__mocks__/didkit.js
@@ -1,22 +1,39 @@
+const keyToDID = jest.fn(() => Promise.resolve("did:key:PUBLIC_KEY"));
+
+const keyToVerificationMethod = jest.fn(() => Promise.resolve("did:key:PUBLIC_KEY#PUBLIC_KEY"));
+
+const issueCredential = jest.fn((credential) =>
+  Promise.resolve(
+    JSON.stringify({
+      ...JSON.parse(credential),
+      proof: {},
+    })
+  )
+);
+const verifyCredential = jest.fn(() =>
+  Promise.resolve(
+    JSON.stringify({
+      checks: [],
+      warnings: [],
+      errors: [],
+    })
+  )
+);
+
+const clearDidkitMocks = () => {
+  keyToDID.mockClear();
+  keyToVerificationMethod.mockClear();
+  issueCredential.mockClear();
+  verifyCredential.mockClear();
+};
+
 // ---- Generate & Verify methods
 module.exports = {
-  keyToDID: jest.fn(() => Promise.resolve("did:key:PUBLIC_KEY")),
-  keyToVerificationMethod: jest.fn(() => Promise.resolve("did:key:PUBLIC_KEY#PUBLIC_KEY")),
-  issueCredential: jest.fn((credential) =>
-    Promise.resolve(
-      JSON.stringify({
-        ...JSON.parse(credential),
-        proof: {},
-      })
-    )
-  ),
-  verifyCredential: jest.fn(() =>
-    Promise.resolve(
-      JSON.stringify({
-        checks: [],
-        warnings: [],
-        errors: [],
-      })
-    )
-  ),
+  keyToDID,
+  keyToVerificationMethod,
+  issueCredential,
+  verifyCredential,
+
+  /* Mock helpers */
+  clearDidkitMocks,
 };
