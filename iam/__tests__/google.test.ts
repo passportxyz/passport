c7aa9cadf086aae1ebcb396177c492e9003c31c4
Shavina Chau <shavina.chau@gmail.com>
2022-04-12 09:58:22 -0500

    test(iam): add error handling to google provider
    re [#15]

--- iam/__tests__/google.test.ts
+++ iam/__tests__/google.test.ts
@@ -1,34 +1,84 @@
-
 // ---- Test subject
 import { RequestPayload } from "@dpopp/types";
 import { GoogleProvider } from "../src/providers/google";
 
-jest.mock('google-auth-library');
+jest.mock("google-auth-library");
 
 import { OAuth2Client } from "google-auth-library";
 
-const OAuth2ClientMock = jest
-  .spyOn(OAuth2Client.prototype, 'verifyIdToken')
-  .mockImplementation(() => {
+const MOCK_EMAIL = "testEmail";
+const MOCK_EMAIL_VERIFIED = true;
+const MOCK_TOKEN_ID = "testToken";
 
-    return {
-        getPayload: jest.fn(() => ({
-            "email": "test",
-            "email_verified": "test"
-        }))
-    }
-  }); 
+const OAuth2ClientMock = jest.spyOn(OAuth2Client.prototype, "verifyIdToken").mockImplementation(() => {
+  return {
+    getPayload: jest.fn(() => ({
+      email: MOCK_EMAIL,
+      email_verified: MOCK_EMAIL_VERIFIED,
+    })),
+  };
+});
 
 describe("Attempt verification", function () {
-    it("handles valid verification attempt", async () => {
-        const google = new GoogleProvider();
+  it("handles valid verification attempt", async () => {
+    const google = new GoogleProvider();
 
-        google.verify({
-            proofs: {
-                tokenId: "test"
-            }
-        } as unknown as RequestPayload)
+    const verifiedPayload = await google.verify({
+      proofs: {
+        tokenId: MOCK_TOKEN_ID,
+      },
+    } as unknown as RequestPayload);
 
-        expect(OAuth2ClientMock).toBeCalledWith({idToken: "test", audience: process.env.GOOGLE_CLIENT_ID})
+    expect(OAuth2ClientMock).toBeCalledWith({ idToken: MOCK_TOKEN_ID, audience: process.env.GOOGLE_CLIENT_ID });
+    expect(verifiedPayload).toEqual({
+      valid: true,
+      record: {
+        email: MOCK_EMAIL,
+      },
     });
+  });
+
+  it("should return invalid payload when email is not verified", async () => {
+    jest.spyOn(OAuth2Client.prototype, "verifyIdToken").mockImplementation(() => {
+      return {
+        getPayload: jest.fn(() => ({
+          email: MOCK_EMAIL,
+          email_verified: false,
+        })),
+      };
+    });
+
+    const google = new GoogleProvider();
+
+    const verifiedPayload = await google.verify({
+      proofs: {
+        tokenId: MOCK_TOKEN_ID,
+      },
+    } as unknown as RequestPayload);
+
+    expect(verifiedPayload).toEqual({
+      valid: false,
+      record: {
+        email: MOCK_EMAIL,
+      },
+    });
+  });
+
+  it("should return invalid payload when verifyIdToken throws exception", async () => {
+    jest.spyOn(OAuth2Client.prototype, "verifyIdToken").mockImplementation(() => {
+      throw Error("bad >:(");
+    });
+
+    const google = new GoogleProvider();
+
+    const verifiedPayload = await google.verify({
+      proofs: {
+        tokenId: MOCK_TOKEN_ID,
+      },
+    } as unknown as RequestPayload);
+
+    expect(verifiedPayload).toEqual({
+      valid: false,
+    });
+  });
 });
