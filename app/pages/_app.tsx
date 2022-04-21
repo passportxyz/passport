eb9b1e3fe44f5e5eb76d24143753422e1117a390
David Hayes <david@focusedlabs.io>
2022-04-21 15:54:51 -0400

    refactor(app): add testing dependencies back in with next
    + removes @types/react in favor @types/react-dom
      - was causing conflicts with type resolution in transitive node_modules dependencies
    + add ChakraProvider lines back into _app.tsx to ensure it doesn't cause any compilation errors

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,16 +1,12 @@
+import { AppProps } from "next/app";
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     // <UserContext.Provider value={stateMemo}>
     <ChakraProvider>
-      <div className="font-librefranklin font-miriam-libre min-h-default min-h-max bg-violet-700 text-gray-100">
-        <div className="container mx-auto px-5 py-2">
-          <Component {...pageProps} />
-        </div>
-      </div>
+      <Component {...pageProps} />
     </ChakraProvider>
     // </UserContext.Provider>
   );
