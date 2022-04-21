f571490f20c3254a92b7f4e16e18ffd1d93d0211
David Hayes <david@focusedlabs.io>
2022-04-21 09:23:26 -0400

    refactor(app): add nextjs starter and rename old app

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,23 +1,10 @@
-const colors = require("tailwindcss/colors");
-
 module.exports = {
-  content: ["./src/**/*.{ts,tsx}"],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
-    extend: {
-      colors: {
-        purple: {
-          darkpurple: "#0B0228",
-        },
-      },
-    },
-    fontFamily: {
-      miriamlibre: ["miriam libre"],
-      librefranklin: ["Libre Franklin"],
-      body: ['"Libre Franklin"'],
-    },
-    minHeight: {
-      default: "100vh",
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
