90309f3a9d5a1ce93e1bd283a3a68b7f7305d66d
farque65 <fahim.farque@gmail.com>
2022-05-04 18:17:52 -0400

    feat(app): implement ceramic for passport data storage
    [#36]

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -6,17 +6,22 @@ import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { UserContextProvider } from "../context/userContext";
 
+// --- Ceramic Tools
+import { Provider } from "@self.id/framework";
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <UserContextProvider>
-      <ChakraProvider>
-        <div className="font-miriam-libre min-h-max min-h-default bg-violet-700 font-librefranklin text-gray-100">
-          <div className="container mx-auto px-5 py-2" suppressHydrationWarning>
-            {typeof window === "undefined" ? null : <Component {...pageProps} />}
+    <Provider client={{ ceramic: "testnet-clay" }}>
+      <UserContextProvider>
+        <ChakraProvider>
+          <div className="font-miriam-libre min-h-max min-h-default bg-violet-700 font-librefranklin text-gray-100">
+            <div className="container mx-auto px-5 py-2" suppressHydrationWarning>
+              {typeof window === "undefined" ? null : <Component {...pageProps} />}
+            </div>
           </div>
-        </div>
-      </ChakraProvider>
-    </UserContextProvider>
+        </ChakraProvider>
+      </UserContextProvider>
+    </Provider>
   );
 }
 
