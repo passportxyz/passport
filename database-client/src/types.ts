a1ecc3cf3cbed96e22be957900f9c26f46b0d21e
farque65 <fahim.farque@gmail.com>
2022-05-04 17:06:52 -0400

    feat(database-client): implements ceramic functions and tests
    - updates dataStorageBase abstract class
    
    [#36]

--- database-client/src/types.ts
+++ database-client/src/types.ts
@@ -1,8 +1,11 @@
 import { Passport, Stamp, DID } from "@dpopp/types";
 
 // Class used as a base for each DataStorage Type
+// Implementations should enforce 1 Passport <-> 1 user
+//  and it is assumed which Passport/user to act on when
+//  calling createPassport, getPassport, addStamp
 export abstract class DataStorageBase {
   abstract createPassport(): Promise<DID>;
-  abstract getPassport(did: DID): Promise<Passport | undefined>;
-  abstract addStamp(did: DID, stamp: Stamp): Promise<void>;
+  abstract getPassport(): Promise<Passport | undefined>;
+  abstract addStamp(stamp: Stamp): Promise<void>;
 }
