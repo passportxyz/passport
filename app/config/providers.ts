3dacf6596661d382dd1914f148ad9bc03e9f976b
farque65 <fahim.farque@gmail.com>
2022-05-20 17:16:33 -0400

    style(app): updates login page style
    - adds logos to /config/provider.ts
    
    [#80]

--- app/config/providers.ts
+++ app/config/providers.ts
@@ -1,7 +1,7 @@
 import { PROVIDER_ID } from "@dpopp/types";
 
 export type ProviderSpec = {
-  // icon: ??? // TODO
+  icon: string | undefined;
   name: string;
   description: string;
 };
@@ -12,30 +12,37 @@ export type Providers = {
 
 export const STAMP_PROVIDERS: Readonly<Providers> = {
   Google: {
+    icon: "./assets/googleStampIcon.svg",
     name: "Google",
     description: "Google Authentication",
   },
   Simple: {
+    icon: undefined,
     name: "Simple",
     description: "Simple Username",
   },
   Ens: {
+    icon: "./assets/ensStampIcon.svg",
     name: "Ens",
     description: "Ens name",
   },
   Poh: {
+    icon: "./assets/pohStampIcon.svg",
     name: "POH",
     description: "Proof of Humanity",
   },
   Twitter: {
+    icon: "./assets/twitterStampIcon.svg",
     name: "Twitter",
     description: "Twitter name",
   },
   POAP: {
+    icon: "./assets/poapStampIcon.svg",
     name: "POAP",
     description: "POAP Verification",
   },
   Facebook: {
+    icon: "./assets/facebookStampIcon.svg",
     name: "Facebook",
     description: "Facebook name",
   },
