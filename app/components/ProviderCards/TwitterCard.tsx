74958edb5f666748be8693adc4fefcf896dd6d75
farque65 <fahim.farque@gmail.com>
2022-06-13 15:52:30 -0400

    fix(app): adds env variable to sample env

--- app/components/ProviderCards/TwitterCard.tsx
+++ app/components/ProviderCards/TwitterCard.tsx
@@ -112,7 +112,6 @@ export default function TwitterCard(): JSX.Element {
         })
         .catch((e) => {
           datadogLogs.logger.error("Verification Error", { error: e, provider: providerId });
-          datadogRum.addError(e, { provider: providerId });
           throw e;
         })
         .finally(() => {
