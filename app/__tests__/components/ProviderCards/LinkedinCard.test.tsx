0f7d2e89153772a8bc42eefc672f2d37e746d9e3
Rask467 <conner.jensen@hey.com>
2022-07-06 07:09:52 -0600

    test(app): update linkedin card to use ceramic context

--- app/__tests__/components/ProviderCards/LinkedinCard.test.tsx
+++ app/__tests__/components/ProviderCards/LinkedinCard.test.tsx
@@ -1,71 +1,46 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { LinkedinCard } from "../../../components/ProviderCards";
 
-import { UserContext, UserContextState } from "../../../context/userContext";
-import { mockAddress, mockWallet } from "../../../__test-fixtures__/onboardHookValues";
+import { UserContextState } from "../../../context/userContext";
 import { STAMP_PROVIDERS } from "../../../config/providers";
 import { linkedinStampFixture } from "../../../__test-fixtures__/databaseStorageFixtures";
+import {
+  makeTestCeramicContext,
+  makeTestUserContext,
+  renderWithContext,
+} from "../../../__test-fixtures__/contextTestHelpers";
+import { CeramicContextState } from "../../../context/ceramicContext";
 
 jest.mock("../../../utils/onboard.ts");
 
-const mockHandleConnection = jest.fn();
-const mockCreatePassport = jest.fn();
-const handleAddStamp = jest.fn().mockResolvedValue(undefined);
-const mockUserContext: UserContextState = {
-  userDid: undefined,
-  loggedIn: true,
-  passport: {
-    issuanceDate: new Date(),
-    expiryDate: new Date(),
-    stamps: [],
-  },
-  isLoadingPassport: false,
-  allProvidersState: {
-    Linkedin: {
-      providerSpec: STAMP_PROVIDERS.Linkedin,
-      stamp: undefined,
-    },
-  },
-  handleAddStamp: handleAddStamp,
-  handleCreatePassport: mockCreatePassport,
-  handleConnection: mockHandleConnection,
-  address: mockAddress,
-  wallet: mockWallet,
-  signer: undefined,
-  walletLabel: mockWallet.label,
-};
+const mockUserContext: UserContextState = makeTestUserContext();
+const mockCeramicContext: CeramicContextState = makeTestCeramicContext();
 
-describe("when user has not verfied with LinkedinProvider", () => {
+describe("when user has not verified with LinkedinProvider", () => {
   it("should display a linkedin verification button", () => {
-    render(
-      <UserContext.Provider value={mockUserContext}>
-        <LinkedinCard />
-      </UserContext.Provider>
-    );
+    renderWithContext(mockUserContext, mockCeramicContext, <LinkedinCard />);
 
-    const linkedinVerifyButton = screen.queryByTestId("button-verify-linkedin");
+    const verifyLinkedinButton = screen.queryByTestId("button-verify-linkedin");
 
-    expect(linkedinVerifyButton).toBeInTheDocument();
+    expect(verifyLinkedinButton).toBeInTheDocument();
   });
 });
 
 describe("when user has verified with LinkedinProvider", () => {
   it("should display that linkedin is verified", () => {
-    render(
-      <UserContext.Provider
-        value={{
-          ...mockUserContext,
-          allProvidersState: {
-            Linkedin: {
-              providerSpec: STAMP_PROVIDERS.Linkedin,
-              stamp: linkedinStampFixture,
-            },
+    renderWithContext(
+      mockUserContext,
+      {
+        ...mockCeramicContext,
+        allProvidersState: {
+          Linkedin: {
+            providerSpec: STAMP_PROVIDERS.Linkedin,
+            stamp: linkedinStampFixture,
           },
-        }}
-      >
-        <LinkedinCard />
-      </UserContext.Provider>
+        },
+      },
+      <LinkedinCard />
     );
 
     const linkedinVerified = screen.queryByText(/Verified/);
