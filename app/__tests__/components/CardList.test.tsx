a4e6a531276a46d7576b80f8ab016fb31a5faf91
Shavina Chau <shavina.chau@gmail.com>
2022-07-12 09:54:19 -0500

    test(app): fix UserContext and CeramicContext mock values in CardList.test.tsx

--- app/__tests__/components/CardList.test.tsx
+++ app/__tests__/components/CardList.test.tsx
@@ -1,64 +1,19 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 
-import { UserContext, UserContextState } from "../../context/userContext";
+import { UserContextState } from "../../context/userContext";
 import { CardList, CardListProps } from "../../components/CardList";
-import { STAMP_PROVIDERS } from "../../config/providers";
+import {
+  makeTestCeramicContext,
+  makeTestUserContext,
+  renderWithContext,
+} from "../../__test-fixtures__/contextTestHelpers";
+import { CeramicContextState } from "../../context/ceramicContext";
 
 jest.mock("../../utils/onboard.ts");
 
-// Mock isLoadingPassport in each describes beforeEach
-const mockUserContext: UserContextState = {
-  address: undefined,
-  handleAddStamp: jest.fn(),
-  handleConnection: jest.fn(),
-  handleCreatePassport: jest.fn(),
-  signer: undefined,
-  wallet: null,
-  walletLabel: undefined,
-  userDid: undefined,
-  loggedIn: true,
-  passport: {
-    issuanceDate: new Date(),
-    expiryDate: new Date(),
-    stamps: [],
-  },
-  isLoadingPassport: false,
-  allProvidersState: {
-    Google: {
-      providerSpec: STAMP_PROVIDERS.Google,
-      stamp: undefined,
-    },
-    Ens: {
-      providerSpec: STAMP_PROVIDERS.Ens,
-      stamp: undefined,
-    },
-    Poh: {
-      providerSpec: STAMP_PROVIDERS.Poh,
-      stamp: undefined,
-    },
-    Twitter: {
-      providerSpec: STAMP_PROVIDERS.Twitter,
-      stamp: undefined,
-    },
-    POAP: {
-      providerSpec: STAMP_PROVIDERS.POAP,
-      stamp: undefined,
-    },
-    Facebook: {
-      providerSpec: STAMP_PROVIDERS.Facebook,
-      stamp: undefined,
-    },
-    Brightid: {
-      providerSpec: STAMP_PROVIDERS.Brightid,
-      stamp: undefined,
-    },
-    Github: {
-      providerSpec: STAMP_PROVIDERS.Github,
-      stamp: undefined,
-    },
-  },
-};
+const mockUserContext: UserContextState = makeTestUserContext();
+const mockCeramicContext: CeramicContextState = makeTestCeramicContext();
 
 let cardListProps: CardListProps = {};
 
@@ -67,12 +22,15 @@ describe("<CardList />", () => {
     cardListProps = {};
   });
 
+  it("renders provider cards when loading state is not defined", () => {
+    renderWithContext(mockUserContext, mockCeramicContext, <CardList {...cardListProps} />);
+
+    expect(screen.queryByTestId("loading-card")).not.toBeInTheDocument();
+  });
+
   it("renders provider cards when not loading", () => {
-    render(
-      <UserContext.Provider value={mockUserContext}>
-        <CardList {...cardListProps} />
-      </UserContext.Provider>
-    );
+    cardListProps.isLoading = false;
+    renderWithContext(mockUserContext, mockCeramicContext, <CardList {...cardListProps} />);
 
     expect(screen.queryByTestId("loading-card")).not.toBeInTheDocument();
   });
@@ -80,11 +38,7 @@ describe("<CardList />", () => {
   it("renders LoadingCards when loading the passport", () => {
     cardListProps.isLoading = true;
 
-    render(
-      <UserContext.Provider value={mockUserContext}>
-        <CardList {...cardListProps} />
-      </UserContext.Provider>
-    );
+    renderWithContext(mockUserContext, mockCeramicContext, <CardList {...cardListProps} />);
 
     expect(screen.getAllByTestId("loading-card"));
   });
