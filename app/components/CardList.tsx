133eb42691e722bd7300d4523c4be42e9601d9f9
farque65 <fahim.farque@gmail.com>
2022-08-24 14:14:32 -0400

    feat(app): removes signer card from view

--- app/components/CardList.tsx
+++ app/components/CardList.tsx
@@ -43,7 +43,6 @@ export const CardList = ({ isLoading }: CardListProps): JSX.Element => {
             <EnsCard />
             <PohCard />
             <DiscordCard />
-            <SignerCard />
           </>
         )}
       </div>
