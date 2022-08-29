527783c6693ef02ec3e18f52e50343bbeb323aa6
Aminah Burch <hello@aminah.io>
2022-08-29 13:12:06 +0300

    chore(iam): removed next/script import

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -4,7 +4,6 @@ import React, { useEffect } from "react";
 // --- Next Methods
 import { AppProps } from "next/app";
 import Head from "next/head";
-import Script from "next/script";
 
 import "../styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
