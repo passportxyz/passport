048fc82100ce9a7f23995d96aeba0a7a62319d35
Graham Dixon <graham@gdixon.co.uk>
2022-09-02 15:52:40 +0100

    feat(app): implements sidebar for Linkedin platform

--- app/components/PlatformCards/index.tsx
+++ app/components/PlatformCards/index.tsx
@@ -6,7 +6,7 @@ export { default as PoapCard } from "./PoapCard";
 export { default as FacebookPlatform } from "./FacebookPlatform";
 export { default as BrightidCard } from "./BrightidCard";
 export { default as GithubPlatform } from "./GithubPlatform";
-export { default as LinkedinCard } from "./LinkedinCard";
+export { default as LinkedinPlatform } from "./LinkedinPlatform";
 export { default as DiscordCard } from "./DiscordCard";
 export { default as SignerCard } from "./SignerCard";
 export { default as GitPOAPCard } from "./GitPOAPCard";
