555a6ba6035923926a5a2e2f92f09771e4cfe3cd
Graham Dixon <graham@gdixon.co.uk>
2022-09-05 15:01:05 +0100

    fix(app): allows platformId or providerId on done toast

--- app/components/DoneToastContent.tsx
+++ app/components/DoneToastContent.tsx
@@ -1,20 +1,26 @@
 // --- React Methods
 import React from "react";
 
-import { PLATFORM_ID } from "@gitcoin/passport-types";
+import { PLATFORM_ID, PROVIDER_ID } from "@gitcoin/passport-types";
 
 export type CustomToastProps = {
-  platformId: PLATFORM_ID;
+  platformId?: PLATFORM_ID;
+  providerId?: PROVIDER_ID;
   result: any;
   message?: boolean | string;
 };
 
 // This content overrides Chakra UI Toast style in render function
-export const DoneToastContent = ({ platformId, result, message = false }: CustomToastProps): JSX.Element => {
+export const DoneToastContent = ({
+  platformId,
+  providerId,
+  result,
+  message = false,
+}: CustomToastProps): JSX.Element => {
   return (
     <div
       className="rounded-md bg-blue-darkblue text-white"
-      data-testid={`toast-done-${platformId && platformId.toLowerCase()}`}
+      data-testid={`toast-done-${(platformId && platformId.toLowerCase()) || (providerId && providerId.toLowerCase())}`}
     >
       <div className="flex p-4">
         <div className="mr-2">
@@ -24,7 +30,7 @@ export const DoneToastContent = ({ platformId, result, message = false }: Custom
         </div>
         <div className="flex-grow">
           <h2 className="title-font mb-2 text-lg font-bold">Done!</h2>
-          <p>{message || `Your ${platformId} stamp has been verified.`}</p>
+          <p>{message || `Your ${platformId || providerId} stamp has been verified.`}</p>
         </div>
         <div>
           <button className="sticky top-0" onClick={result.onClose}>
