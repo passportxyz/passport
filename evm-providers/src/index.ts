c50ccf741ad494dbb480fdad7f827874f71fe881
schultztimothy <schultz.timothy52@gmail.com>
2022-09-15 07:15:08 -0600

    feat(evm-providers): renamed package to evm-provider

---
+++ evm-providers/src/index.ts
@@ -0,0 +1,4 @@
+import { IdentityProviders } from "./utils/identityProviders";
+import { ENSIdentityProvider } from "./providers/ens";
+
+// export const providers = new IdentityProviders([ENSIdentityProvider]);
