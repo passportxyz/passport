8ad857bf668d0f5b476e786a377c86d3b8bb3b3b
schultztimothy <schultz.timothy52@gmail.com>
2022-09-19 13:03:00 -0600

    feat(evm-providers): return signer or provider

--- evm-providers/src/utils/signer.ts
+++ evm-providers/src/utils/signer.ts
@@ -7,6 +7,7 @@ import { verifyCredential } from "@gitcoin/passport-identity/dist/commonjs/src/c
 
 // ----- Verify signed message with ethers
 import { utils } from "ethers";
+import { JsonRpcProvider, JsonRpcSigner, StaticJsonRpcProvider } from "@ethersproject/providers";
 
 // get the address associated with the signer in the payload
 export const getAddress = async ({ address, signer, issuer }: RequestPayload): Promise<string> => {
@@ -23,4 +24,29 @@ export const getAddress = async ({ address, signer, issuer }: RequestPayload): P
 
   // proof was missing/invalid return controller address from the payload
   return address;
+};
+
+// set the network rpc url based on env
+const RPC_URL = process.env.RPC_URL;
+
+export const getRPCProvider = async (
+  payload: RequestPayload
+): Promise<{ provider: JsonRpcSigner | JsonRpcProvider; address: string }> => {
+  // return signer if provided
+  if (payload.jsonRpcSigner) {
+    const signer = payload.jsonRpcSigner;
+    const address = await signer.getAddress();
+    return {
+      provider: signer,
+      address,
+    };
+  }
+
+  // define a provider using the rpc url
+  const provider: StaticJsonRpcProvider = new StaticJsonRpcProvider(RPC_URL);
+  const address = await getAddress(payload);
+  return {
+    provider,
+    address,
+  };
 };
