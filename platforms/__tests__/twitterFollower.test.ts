0b8ec8dc7bf6ebcd97d6d0a15f6672e678db457b
schultztimothy <schultz.timothy52@gmail.com>
2022-10-20 16:46:38 -0600

    feat(platforms): remove unused dependencies

--- platforms/__tests__/twitterFollower.test.ts
+++ platforms/__tests__/twitterFollower.test.ts
@@ -1,11 +1,3 @@
-// ---- Test subject
-import {
-  TwitterFollowerGT100Provider,
-  TwitterFollowerGT500Provider,
-  TwitterFollowerGTE1000Provider,
-  TwitterFollowerGT5000Provider,
-} from "@gitcoin/passport-iam/src/providers/twitterFollower";
-
 describe("Attempt verification", function () {
   it("it should auth with twitter", async () => {
     expect(true).toBe(true);
