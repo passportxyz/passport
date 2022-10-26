a03c9f2aa91a67d8f3c5654d87c3e800e8d914ea
schultztimothy <schultz.timothy52@gmail.com>
2022-10-26 16:09:16 -0600

    feat(platforms): export ENS Provider

---
+++ platforms/src/index.ts
@@ -0,0 +1,13 @@
+// Twitter Platform
+export { TwitterPlatform } from "./Twitter/App-Bindings";
+export { default as TwitterAuthProvider } from "./Twitter/Providers/TwitterAuthProvider";
+export {
+  TwitterFollowerGT100Provider,
+  TwitterFollowerGT500Provider,
+  TwitterFollowerGTE1000Provider,
+  TwitterFollowerGT5000Provider,
+} from "./Twitter/Providers/TwitterFollowerProvider";
+export { TwitterTweetGT10Provider } from "./Twitter/Providers/TwitterTweetsProvider";
+export { TwitterPlatformDetails, TwitterProviderConfig } from "./Twitter/Providers-config";
+
+export * as Ens from "./Ens";
