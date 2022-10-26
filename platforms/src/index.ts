6db76da6c15da4f0542654184cce482204f40a4f
Fahim <fahim.farque@gmail.com>
2022-10-26 08:32:21 -0400

    feat(platforms): changes /Twitter/Providers to uppercase (#654)

--- platforms/src/index.ts
+++ platforms/src/index.ts
@@ -1,11 +1,11 @@
 // Twitter Platform
 export { TwitterPlatform } from "./Twitter/App-Bindings";
-export { default as TwitterAuthProvider } from "./Twitter/providers/TwitterAuthProvider";
+export { default as TwitterAuthProvider } from "./Twitter/Providers/TwitterAuthProvider";
 export {
   TwitterFollowerGT100Provider,
   TwitterFollowerGT500Provider,
   TwitterFollowerGTE1000Provider,
   TwitterFollowerGT5000Provider,
-} from "./Twitter/providers/TwitterFollowerProvider";
-export { TwitterTweetGT10Provider } from "./Twitter/providers/TwitterTweetsProvider";
+} from "./Twitter/Providers/TwitterFollowerProvider";
+export { TwitterTweetGT10Provider } from "./Twitter/Providers/TwitterTweetsProvider";
 export { TwitterPlatformDetails, TwitterProviderConfig } from "./Twitter/Providers-config";
