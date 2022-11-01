9ff415c5a751e4f65ae5324b8a26f3e42d50b685
schultztimothy <schultz.timothy52@gmail.com>
2022-10-31 18:08:37 -0600

    feat(platforms): add remaining aspects of scaffolded platform

---
+++ platforms/src/Facebook/index.ts
@@ -0,0 +1,5 @@
+export { FacebookProvider } from "./Providers/facebook";
+export { FacebookFriendsProvider } from "./Providers/facebookFriends";
+export { FacebookProfilePictureProvider } from "./Providers/facebookProfilePicture";
+
+export { FacebookPlatformDetails, FacebookProviderConfig } from "./Providers-config";
--- platforms/src/index.ts
+++ platforms/src/index.ts
@@ -1,3 +1,3 @@
 export * as Twitter from "./Twitter";
-
 export * as Ens from "./Ens";
+export * as Facebook from "./Facebook";
