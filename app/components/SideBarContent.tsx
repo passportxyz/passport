eff84ac36cc474e0fbad8133ca0d5096e536a64e
schultztimothy <schultz.timothy52@gmail.com>
2022-11-07 13:15:15 -0700

    feat(app): testing indicator color

--- app/components/SideBarContent.tsx
+++ app/components/SideBarContent.tsx
@@ -109,6 +109,7 @@ export const SideBarContent = ({
                                   : `text-gray-400`
                               }`}
                               key={`${provider.title}${i}`}
+                              data-testid={`indicator-${provider.name}`}
                             >
                               <div className="relative top-[-0.3em] text-sm text-gray-400">{provider.title}</div>
                             </li>
