23c523c4661267c0a57964f95b1d58e80804daa9
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2022-11-08 16:32:48 +0000

    feat(platforms): make twitter provider comply with new provider structure

--- platforms/src/Twitter/App-Bindings.ts
+++ platforms/src/Twitter/App-Bindings.ts
@@ -21,10 +21,35 @@ export class TwitterPlatform implements Platform {
   }
 
   async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
-    return {};
+    const authUrl: string = await this.getOAuthUrl(appContext.state);
+    const width = 600;
+    const height = 800;
+    const left = appContext.screen.width / 2 - width / 2;
+    const top = appContext.screen.height / 2 - height / 2;
+
+    // Pass data to the page via props
+    appContext.window.open(
+      authUrl,
+      "_blank",
+      "toolbar=no, location=no, directories=no, status=no, menubar=no, resizable=no, copyhistory=no, width=" +
+        width +
+        ", height=" +
+        height +
+        ", top=" +
+        top +
+        ", left=" +
+        left
+    );
+
+    return appContext.waitForRedirect().then((data) => {
+      return {
+        code: data.code,
+        sessionKey: data.state,
+        signature: data.signature,
+      }
+    });
   }
 
-  
   async getOAuthUrl(state: string): Promise<string> {
     // Fetch data from external API
     const res = (await axios.post(
