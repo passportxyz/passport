6099af77e6025c6e76d333c81c95c67db5aea507
schultztimothy <schultz.timothy52@gmail.com>
2022-11-10 16:53:08 -0700

    refactor(platforms): extend base platform for facebook

--- platforms/src/Facebook/App-Bindings.ts
+++ platforms/src/Facebook/App-Bindings.ts
@@ -1,17 +1,18 @@
-/* eslint-disable */
-import { Platform, AccessTokenResult, AppContext, ProviderPayload } from "../types";
-export class FacebookPlatform implements Platform {
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import { ProviderPayload } from "../types";
+import { Platform } from "../utils/platform";
+export class FacebookPlatform extends Platform {
   path: string;
   platformId = "Facebook";
 
   //@ts-ignore 'appContext' is defined but never used
-  async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
+  async getProviderPayload(): Promise<ProviderPayload> {
     //@ts-ignore 'appContext' Unexpected empty arrow function
     const result = new Promise<ProviderPayload>((resolve) => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore assuming FB.init was already called; see facebookSdkScript in app/pages/_app.tsx
       // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-      FB.login(function (response) {
+      FB.login(function (response: { authResponse: { accessToken: string }; status: string }) {
         if (response.status === "connected") {
           resolve({ accessToken: response.authResponse.accessToken });
         } else {
@@ -19,25 +20,6 @@ export class FacebookPlatform implements Platform {
         }
       });
     });
-
-    return result;
-  }
-
-  async getProviderProof(): Promise<AccessTokenResult> {
-    // TODO: remove this method
-    // TODO: Shouldn't need all of these ignores, should be just this //@ts-ignore assuming FB.init was already called; see facebookSdkScript in pages/index.tsx once tsconfigs are normalized
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore assuming FB.init was already called; see facebookSdkScript in app/pages/_app.tsx
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    const result = (await FB.login(function (response) {
-      return new Promise((resolve, reject) => {
-        if (response.authenticated) {
-          resolve({ accessToken: response.accessToken });
-        } else {
-          reject({ authenticated: false });
-        }
-      });
-    })) as AccessTokenResult;
 
     return result;
   }
