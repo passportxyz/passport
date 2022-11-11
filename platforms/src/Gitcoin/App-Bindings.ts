dc4a4c895f134889fa78da0b4580ad0968392499
schultztimothy <schultz.timothy52@gmail.com>
2022-11-10 17:16:32 -0700

    refactor(platforms): extend base platform for gitcoin

--- platforms/src/Gitcoin/App-Bindings.ts
+++ platforms/src/Gitcoin/App-Bindings.ts
@@ -1,43 +1,22 @@
-import { AppContext, Platform, PlatformOptions, ProviderPayload } from "../types";
+import { PlatformOptions, ProviderPayload } from "../types";
+import { Platform } from "../utils/platform";
 
-/* eslint-disable */
-export class GitcoinPlatform implements Platform {
+export class GitcoinPlatform extends Platform {
   platformId = "Gitcoin";
   path = "github";
   clientId: string = null;
   redirectUri: string = null;
 
   constructor(options: PlatformOptions = {}) {
+    super();
     this.clientId = options.clientId as string;
     this.redirectUri = options.redirectUri as string;
   }
 
-  async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
-    const authUrl: string = await this.getOAuthUrl(appContext.state);
-    const width = 600;
-    const height = 800;
-    const left = appContext.screen.width / 2 - width / 2;
-    const top = appContext.screen.height / 2 - height / 2;
-
-    // Pass data to the page via props
-    appContext.window.open(
-      authUrl,
-      "_blank",
-      "toolbar=no, location=no, directories=no, status=no, menubar=no, resizable=no, copyhistory=no, width=" +
-        width +
-        ", height=" +
-        height +
-        ", top=" +
-        top +
-        ", left=" +
-        left
-    );
-
-    return appContext.waitForRedirect();
-  }
-
   async getOAuthUrl(state: string): Promise<string> {
-    const githubUrl = `https://github.com/login/oauth/authorize?client_id=${this.clientId}&redirect_uri=${this.redirectUri}&state=${state}`;
+    const githubUrl = await Promise.resolve(
+      `https://github.com/login/oauth/authorize?client_id=${this.clientId}&redirect_uri=${this.redirectUri}&state=${state}`
+    );
     return githubUrl;
   }
 }
