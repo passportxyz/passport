899741053e1c4867443335d4acccb03657d5c616
schultztimothy <schultz.timothy52@gmail.com>
2022-11-11 11:52:16 -0700

    refactor(platforms): extend base platform for gitpoap

--- platforms/src/GitPOAP/App-Bindings.ts
+++ platforms/src/GitPOAP/App-Bindings.ts
@@ -1,17 +1,13 @@
 /* eslint-disable */
-import { AppContext, Platform, PlatformOptions, ProviderPayload } from "../types";
+import { AppContext, Platform, ProviderPayload } from "../types";
 
 export class GitPOAPPlatform implements Platform {
-  async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
-    return {};
-  }
-
-  
-  getOAuthUrl(state: string): Promise<string> {
-    throw new Error("Method not implemented.");
-  }
   platformId = "GitPOAP";
   path = "GitPOAP";
   clientId: string = null;
   redirectUri: string = null;
+
+  async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
+    return {};
+  }
 }
