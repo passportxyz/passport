322b922f2c682e6e6e296faafeb35a18cbe9ecbf
schultztimothy <schultz.timothy52@gmail.com>
2022-11-11 12:16:04 -0700

    refactor(platforms): extend base platform for gtc

--- platforms/src/GTC/App-Bindings.ts
+++ platforms/src/GTC/App-Bindings.ts
@@ -1,17 +1,14 @@
 /* eslint-disable */
-import { AppContext, Platform, PlatformOptions, ProviderPayload } from "../types";
+import { AppContext, ProviderPayload } from "../types";
+import { Platform } from "../utils/platform";
 
-export class GTCPlatform implements Platform {
-  async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
-    return {};
-  }
-
-  getOAuthUrl(state: string): Promise<string> {
-    throw new Error("Method not implemented.");
-  }
-
+export class GTCPlatform extends Platform {
   platformId = "GTC";
   path = "GTC";
   clientId: string = null;
   redirectUri: string = null;
+
+  async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
+    return {};
+  }
 }
