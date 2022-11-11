431ce93b89c11a732f6bae043e73e9bf219cafa0
schultztimothy <schultz.timothy52@gmail.com>
2022-11-11 16:41:55 -0700

    refactor(platforms): es lint and lint

--- platforms/src/GTC/App-Bindings.ts
+++ platforms/src/GTC/App-Bindings.ts
@@ -1,14 +1,12 @@
-/* eslint-disable */
 import { AppContext, ProviderPayload } from "../types";
 import { Platform } from "../utils/platform";
 
 export class GTCPlatform extends Platform {
   platformId = "GTC";
   path = "GTC";
-  clientId: string = null;
-  redirectUri: string = null;
 
   async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
-    return {};
+    const result = await Promise.resolve({});
+    return result;
   }
 }
