bebbb991fc5a6fc37fd248a6f6a0cff00c1cef6f
nutrina <nutrina9@gmail.com>
2022-11-14 11:07:01 +0200

    fix(platforms): remove console.log statement

--- database-client/src/ceramicClient.ts
+++ database-client/src/ceramicClient.ts
@@ -110,12 +110,11 @@ export class CeramicDatabase implements DataStorageBase {
       // `stamps` is stored as ceramic URLs - must load actual VC data from URL
       const stampsToLoad = passport?.stamps.map(async (_stamp, idx) => {
         const streamUrl = `${this.apiHost}/api/v0/streams/${streamIDs[idx].substring(10)}`;
-        console.log("streamUrl", streamUrl);
+        this.logger.log(`get stamp from streamUrl: ${streamUrl}`);
 
         try {
           const { provider, credential } = _stamp;
           const loadedCred = (await axios.get(streamUrl)) as { data: { state: { content: VerifiableCredential } } };
-          console.log("Credential: ", loadedCred.data.state.content);
           return {
             provider,
             credential: loadedCred.data.state.content,
