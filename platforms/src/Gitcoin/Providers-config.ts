b839f0182be157dbd8860f9b6d7dfde91b9a8062
Aminah Burch <hello@aminah.io>
2022-11-30 10:43:12 +0300

    fix(platforms): updates labels to 'at least'

--- platforms/src/Gitcoin/Providers-config.ts
+++ platforms/src/Gitcoin/Providers-config.ts
@@ -12,33 +12,33 @@ export const GitcoinProviderConfig: PlatformGroupSpec[] = [
   {
     platformGroup: "Contributed to...",
     providers: [
-      { title: "more than 1 Grant", name: "GitcoinContributorStatistics#numGrantsContributeToGte#1" },
-      { title: "more than 10 Grants", name: "GitcoinContributorStatistics#numGrantsContributeToGte#10" },
-      { title: "more than 25 Grants", name: "GitcoinContributorStatistics#numGrantsContributeToGte#25" },
-      { title: "more than 100 Grants", name: "GitcoinContributorStatistics#numGrantsContributeToGte#100" },
+      { title: "at least 1 Grant", name: "GitcoinContributorStatistics#numGrantsContributeToGte#1" },
+      { title: "at least 10 Grants", name: "GitcoinContributorStatistics#numGrantsContributeToGte#10" },
+      { title: "at least 25 Grants", name: "GitcoinContributorStatistics#numGrantsContributeToGte#25" },
+      { title: "at least 100 Grants", name: "GitcoinContributorStatistics#numGrantsContributeToGte#100" },
     ],
   },
   {
     platformGroup: "Contributed ($)...",
     providers: [
-      { title: "more than $10", name: "GitcoinContributorStatistics#totalContributionAmountGte#10" },
-      { title: "more than $100", name: "GitcoinContributorStatistics#totalContributionAmountGte#100" },
-      { title: "more than $1000", name: "GitcoinContributorStatistics#totalContributionAmountGte#1000" },
+      { title: "at least $10", name: "GitcoinContributorStatistics#totalContributionAmountGte#10" },
+      { title: "at least $100", name: "GitcoinContributorStatistics#totalContributionAmountGte#100" },
+      { title: "at least $1000", name: "GitcoinContributorStatistics#totalContributionAmountGte#1000" },
     ],
   },
   {
     platformGroup: "Contributed in...",
     providers: [
       { title: "GR14", name: "GitcoinContributorStatistics#numGr14ContributionsGte#1" },
-      { title: "more than 1 Round", name: "GitcoinContributorStatistics#numRoundsContributedToGte#1" },
+      { title: "at least 1 Round", name: "GitcoinContributorStatistics#numRoundsContributedToGte#1" },
     ],
   },
   {
     platformGroup: "Owner of...",
-    providers: [{ title: " more than 1 Grant", name: "GitcoinGranteeStatistics#numOwnedGrants#1" }],
+    providers: [{ title: " at least 1 Grant", name: "GitcoinGranteeStatistics#numOwnedGrants#1" }],
   },
   {
-    platformGroup: "Grants have more than...",
+    platformGroup: "Grants have at least...",
     providers: [
       { title: "10 Contributors", name: "GitcoinGranteeStatistics#numGrantContributors#10" },
       { title: "25 Contributors", name: "GitcoinGranteeStatistics#numGrantContributors#25" },
@@ -48,16 +48,16 @@ export const GitcoinProviderConfig: PlatformGroupSpec[] = [
   {
     platformGroup: "Grants have received...",
     providers: [
-      { title: "more than $100", name: "GitcoinGranteeStatistics#totalContributionAmount#100" },
-      { title: "more than $1000", name: "GitcoinGranteeStatistics#totalContributionAmount#1000" },
-      { title: "more than $10000", name: "GitcoinGranteeStatistics#totalContributionAmount#10000" },
+      { title: "at least $100", name: "GitcoinGranteeStatistics#totalContributionAmount#100" },
+      { title: "at least $1000", name: "GitcoinGranteeStatistics#totalContributionAmount#1000" },
+      { title: "at least $10000", name: "GitcoinGranteeStatistics#totalContributionAmount#10000" },
     ],
   },
   {
     platformGroup: "Eco/Cause Rounds",
     providers: [
       {
-        title: "owner of more than 1 Grant in Eco/Cause Rounds",
+        title: "owner of at least 1 Grant in Eco/Cause Rounds",
         name: "GitcoinGranteeStatistics#numGrantsInEcoAndCauseRound#1",
       },
     ],
