8deee031cfdc4bc68d9f93ae71b957dd71c494ba
schultztimothy <schultz.timothy52@gmail.com>
2023-01-04 13:23:04 -0700

    feat(app): test that only evm providers are called

--- app/__tests__/utils/utils.test.ts
+++ app/__tests__/utils/utils.test.ts
@@ -2,7 +2,7 @@ import { fetchPossibleEVMStamps } from "../../signer/utils";
 import { providers } from "@gitcoin/passport-platforms";
 import { ProviderContext, RequestPayload, VerifiedPayload } from "@gitcoin/passport-types";
 import { VALID_ENS_VERIFICATION, VALID_LENS_VERIFICATION } from "../../__test-fixtures__/verifiableCredentialResults";
-import { Ens, Lens } from "@gitcoin/passport-platforms";
+import { Ens, Lens, Github } from "@gitcoin/passport-platforms";
 
 const mockedAllPlatforms = new Map();
 mockedAllPlatforms.set("Ens", {
@@ -15,8 +15,16 @@ mockedAllPlatforms.set("Lens", {
   platFormGroupSpec: Lens.LensProviderConfig,
 });
 
+mockedAllPlatforms.set("Github", {
+  platform: new Github.GithubPlatform({
+    clientId: process.env.NEXT_PUBLIC_PASSPORT_GITHUB_CLIENT_ID,
+    redirectUri: process.env.NEXT_PUBLIC_PASSPORT_GITHUB_CALLBACK,
+  }),
+  platFormGroupSpec: Github.GithubProviderConfig,
+});
+
 describe("fetchPossibleEVMStamps", () => {
-  it("should return valid platforms", async () => {
+  beforeEach(() => {
     jest
       .spyOn(providers._providers["Ens"], "verify")
       .mockImplementation(async (payload: RequestPayload): Promise<VerifiedPayload> => {
@@ -29,10 +37,21 @@ describe("fetchPossibleEVMStamps", () => {
       .mockImplementation(async (payload: RequestPayload): Promise<VerifiedPayload> => {
         return VALID_LENS_VERIFICATION.payload;
       });
+  });
+  it("should return valid platforms", async () => {
     const result = await fetchPossibleEVMStamps("0x123", mockedAllPlatforms);
 
     expect(result.length).toBe(1);
 
     expect(result[0].platformProps.platform.path).toBe("Ens");
+  });
+  it("should not include a non evm platform", async () => {
+    const github = jest
+      .spyOn(providers._providers["Github"], "verify")
+      .mockImplementation(async (payload: RequestPayload): Promise<VerifiedPayload> => {
+        return {} as VerifiedPayload;
+      });
+    const result = await fetchPossibleEVMStamps("0x123", mockedAllPlatforms);
+    expect(github).not.toHaveBeenCalled();
   });
 });
