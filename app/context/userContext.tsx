e06490ca96fa7c31d8970f552de9a175a29f4866
nutrina <nutrina9@gmail.com>
2023-01-13 07:10:18 +0200

    feat(app): make passport network agnostic, passports are always on "mainnet"
    - creating did with network id 1, regardless of the network to which the user connected
    - do not force use to switch to mainnet

--- app/context/userContext.tsx
+++ app/context/userContext.tsx
@@ -16,6 +16,10 @@ import { useViewerConnection } from "@self.id/framework";
 import { datadogLogs } from "@datadog/browser-logs";
 import { datadogRum } from "@datadog/browser-rum";
 
+import { DIDSession } from "did-session";
+import { EthereumWebAuth } from "@didtools/pkh-ethereum";
+import { AccountId } from "caip";
+
 export interface UserContextState {
   loggedIn: boolean;
   handleConnection: () => void;
@@ -110,7 +114,7 @@ export const UserContextProvider = ({ children }: { children: any }) => {
     // check that passportLogin isnt mid-way through
     if (wallet && !loggingIn) {
       // ensure that passport is connected to mainnet
-      const hasCorrectChainId = await ensureMainnet();
+      const hasCorrectChainId = process.env.NEXT_PUBLIC_FF_MULTICHAIN_SIGNATURE === "on" ? true : await ensureMainnet();
       // mark that we're attempting to login
       setLoggingIn(true);
       // with loaded chainId
@@ -128,9 +132,13 @@ export const UserContextProvider = ({ children }: { children: any }) => {
           const sessionStr = localStorage.getItem(sessionKey);
 
           // @ts-ignore
-          let selfId = await ceramicConnect(ethAuthProvider, sessionStr);
+          // When sessionStr is null, this will create a new selfId. We want to avoid this, becasue we want to make sure
+          // that chainId 1 is in the did
+          let selfId = !!sessionStr ? await ceramicConnect(ethAuthProvider, sessionStr) : null;
 
           if (
+            // @ts-ignore
+            !selfId ||
             // @ts-ignore
             !selfId?.client?.session ||
             // @ts-ignore
@@ -138,9 +146,30 @@ export const UserContextProvider = ({ children }: { children: any }) => {
             // @ts-ignore
             selfId?.client?.session?.expireInSecs < 3600
           ) {
-            // If the session loaded is not valid, or if it is expired or close to expire, we create
-            // a new connection to ceramic
-            selfId = await ceramicConnect(ethAuthProvider);
+            if (process.env.NEXT_PUBLIC_FF_MULTICHAIN_SIGNATURE === "on") {
+              // If the session loaded is not valid, or if it is expired or close to expire, we create
+              // a new session
+              // Also we enforce the "1" chainId, as we always want to use mainnet dids, in order to avoid confusion
+              // as to where a passport / stamp has been stored
+              const authMethod = await EthereumWebAuth.getAuthMethod(
+                wallet.provider,
+                new AccountId({
+                  chainId: "eip155:1",
+                  address: address,
+                })
+              );
+              const session = await DIDSession.authorize(authMethod, {
+                expiresInSecs: 24 * 3600,
+                resources: ["ceramic://*"],
+              });
+              const newSessionStr = session.serialize();
+
+              selfId = await ceramicConnect(ethAuthProvider, newSessionStr);
+            } else {
+              // If the session loaded is not valid, or if it is expired or close to expire, we create
+              // a new connection to ceramic
+              selfId = await ceramicConnect(ethAuthProvider);
+            }
           }
 
           // Store the session in localstorage
