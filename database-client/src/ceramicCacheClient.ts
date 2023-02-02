101ba61c8a93ee8359e529e48f8a85f50d4966f5
schultztimothy <schultz.timothy52@gmail.com>
2023-02-01 17:32:55 -0700

    feat(app, database-client): initiate cacheClient within context

--- database-client/src/ceramicCacheClient.ts
+++ database-client/src/ceramicCacheClient.ts
@@ -1,8 +1,19 @@
 import { PassportLoadResponse, Stamp } from "@gitcoin/passport-types";
 import axios from "axios";
+import { Logger } from "./ceramicClient";
 import { DataStorageBase } from "./types";
 
-export class CeramicCacheClient implements DataStorageBase {
+export class CeramicCacheDatabase implements DataStorageBase {
+  ceramicCacheUrl: string;
+  address: string;
+  logger: Logger;
+
+  constructor(ceramicCacheUrl: string, address: string, logger?: Logger) {
+    this.ceramicCacheUrl = ceramicCacheUrl;
+    this.address = address;
+    this.logger = logger;
+  }
+
   async createPassport(): Promise<string> {
     throw new Error("Method not implemented.");
   }
