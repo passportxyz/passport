e6eac7d0f734cbde1b359876158a218e4c90bc8b
Michael <michaelgreen06@gmail.com>
2023-04-11 15:44:32 -0500

    feat: add intercom widget to all pages

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -20,10 +20,55 @@ import TagManager from "react-gtm-module";
 
 const FacebookAppId = process.env.NEXT_PUBLIC_PASSPORT_FACEBOOK_APP_ID || "";
 const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || "";
+const INTERCOM_APP_ID = process.env.NEXT_PUBLIC_INTERCOM_APP_ID || "";
 
 function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     TagManager.initialize({ gtmId: `${GTM_ID}` });
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // @ts-ignore
+      window.intercomSettings = {
+        api_base: "https://api-iam.intercom.io",
+        app_id: INTERCOM_APP_ID,
+      };
+      //@ts-ignore eslint-disable-next-line no-use-before-define
+      (function () {
+        var w = window;
+        var ic = w.Intercom;
+        if (typeof ic === "function") {
+          ic("reattach_activator");
+          ic("update", w.intercomSettings);
+        } else {
+          var d = document;
+          var i = function () {
+            i.c(arguments);
+          };
+          i.q = [];
+          i.c = function (args) {
+            i.q.push(args);
+          };
+          w.Intercom = i;
+          var l = function () {
+            var s = d.createElement("script");
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://widget.intercom.io/widget/xaafeyri";
+            var x = d.getElementsByTagName("script")[0];
+            x.parentNode.insertBefore(s, x);
+          };
+          if (document.readyState === "complete") {
+            l();
+          } else if (w.attachEvent) {
+            w.attachEvent("onload", l);
+          } else {
+            w.addEventListener("load", l, false);
+          }
+        }
+      })();
+    }
   }, []);
 
   if (typeof window !== "undefined") {
