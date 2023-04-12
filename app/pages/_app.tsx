9cd3bb9dcbe497e068a439bbd8377f6a8adef186
michaelgreen06 <michaelgreen06@gmail.com>
2023-04-12 15:30:32 -0500

    feat:adds intercom integration (#1083)
    * feat:adds intercom integration
    
    * feat: add intercom widget to all pages
    
    * fix: adding type any to window
    
    * feat(app,intercom): fixing typescript complaints
    
    ---------
    
    Co-authored-by: Michael Green <michaelgreen@Michaels-MacBook-Pro.local>
    Co-authored-by: nutrina <nutrina9@gmail.com>

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -20,10 +20,68 @@ import TagManager from "react-gtm-module";
 
 const FacebookAppId = process.env.NEXT_PUBLIC_PASSPORT_FACEBOOK_APP_ID || "";
 const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID || "";
+const INTERCOM_APP_ID = process.env.NEXT_PUBLIC_INTERCOM_APP_ID || "";
+
+// Type definition for the window object
+declare global {
+  interface Window {
+    intercomSettings?: {
+      api_base: string;
+      app_id: string;
+    };
+    Intercom: any;
+  }
+}
 
 function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     TagManager.initialize({ gtmId: `${GTM_ID}` });
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.intercomSettings = {
+        api_base: "https://api-iam.intercom.io",
+        app_id: INTERCOM_APP_ID,
+      };
+      (function () {
+        var w: any = window;
+        var ic = w.Intercom;
+        if (typeof ic === "function") {
+          ic("reattach_activator");
+          ic("update", w.intercomSettings);
+        } else {
+          var d = document;
+          var i = function () {
+            // @ts-ignore
+            i.c(arguments);
+          };
+          // @ts-ignore
+          i.q = [];
+          // @ts-ignore
+          i.c = function (args) {
+            // @ts-ignore
+            i.q.push(args);
+          };
+          w.Intercom = i;
+          var l = function () {
+            var s = d.createElement("script");
+            s.type = "text/javascript";
+            s.async = true;
+            s.src = "https://widget.intercom.io/widget/" + INTERCOM_APP_ID;
+            var x = d.getElementsByTagName("script")[0];
+            x.parentNode?.insertBefore(s, x);
+          };
+          if (document.readyState === "complete") {
+            l();
+          } else if (w.attachEvent) {
+            w.attachEvent("onload", l);
+          } else {
+            w.addEventListener("load", l, false);
+          }
+        }
+      })();
+    }
   }, []);
 
   if (typeof window !== "undefined") {
