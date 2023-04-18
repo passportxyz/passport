23d448f699d2c6ee104352f29883cced3278ff7f
schultztimothy <schultz.timothy52@gmail.com>
2023-04-18 16:04:41 -0600

    feat(app): add InitialWelcome and WelcomeWrapper component

---
+++ app/components/InitialWelcome.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { WelcomeWrapper } from "./WelcomeWrapper";
+import { useNavigate } from "react-router-dom";
+
+const welcomeSteps = [
+  {
+    header: "Welcome to Gitcoin Passport!",
+    subHeader: "Privacy-First Verification",
+    body: "The non-doxxing identity aggregator that combines multiple sybil prevention mechanisms to prove your unique humanity and build your reputation.",
+    imgSrc: "",
+  },
+  {
+    header: "Introducing Passport Scoring",
+    subHeader: "Get Your Unique Humanity Score",
+    body: "Think of your score as a trampoline that launches you to your destination. As your score increases, communities more easily trust that you are a unique human.",
+    imgSrc: "",
+  },
+  {
+    header: "Get Started",
+    subHeader: "One-Click Verification",
+    body: "You can now verify most web3 stamps with one-click verification! Verifying Web2 apps will come after this step, and we encourage you to collect as many stamps as possible.",
+    imgSrc: "",
+  },
+];
+
+const stepIndicator = (step: number) => {
+  const steps = welcomeSteps.map((_, i) => {
+    const active = i === step ? "border-4 border-muted h-4 w-4" : "w-2.5 h-2.5 mt-0.5";
+    return <div key={i} className={`mx-4 rounded-full bg-accent ${active}`} />;
+  });
+  return <div className="ml-3 flex justify-center">{steps}</div>;
+};
+
+export const InitialWelcome = ({ onBoardFinished }: { onBoardFinished: () => void }) => {
+  const [step, setStep] = useState(0);
+  const navigate = useNavigate();
+
+  return (
+    <WelcomeWrapper content={welcomeSteps[step]}>
+      <div className="flex w-full flex-col">
+        <div className="mb-5 flex w-full items-center justify-center">
+          Step {step + 1} of {welcomeSteps.length} {stepIndicator(step)}
+        </div>
+        <div className="flex w-full justify-end">
+          {step === 2 && (
+            <button
+              className="secondary-btn mr-2  w-1/2 rounded-sm py-2 px-6"
+              onClick={() => {
+                setStep(0);
+                navigate("/dashboard");
+              }}
+            >
+              Skip For Now
+            </button>
+          )}
+          <button
+            className="ml-2 w-1/2 rounded-sm bg-accent py-2 px-6"
+            onClick={() => {
+              if (step + 1 === welcomeSteps.length) {
+                onBoardFinished();
+              } else {
+                setStep(step + 1);
+              }
+            }}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </WelcomeWrapper>
+  );
+};
---
+++ app/components/WelcomeWrapper.tsx
@@ -0,0 +1,25 @@
+export type Content = {
+  header: string;
+  subHeader: string;
+  body: string;
+  imgSrc: string;
+};
+
+export type WelcomeWrapperProps = {
+  content: Content;
+  children: React.ReactNode;
+};
+
+export const WelcomeWrapper = ({ content, children }: WelcomeWrapperProps) => {
+  return (
+    <>
+      <div className="top-[113px] mt-10 text-3xl">{content.header}</div>
+      <div className="top-[209px] mt-10 h-[240px] w-[295px] border border-accent-2 bg-background lg:h-[333.56px] lg:w-[410px]"></div>
+      <p className="top-[113px] mt-10 text-2xl text-muted">{content.subHeader}</p>
+      <p className="mt-2 mb-10 w-[343px] text-gray-300 lg:w-[410px]">{content.body}</p>
+      <div className="absolute bottom-10 mb-auto flex w-[295px] items-center justify-between md:relative md:mt-16 lg:w-[410px]">
+        {children}
+      </div>
+    </>
+  );
+};
