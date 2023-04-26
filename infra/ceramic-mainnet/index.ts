ade2f02973e0c77015f7e715ee6b6627bc4e5e56
nutrina <nutrina9@gmail.com>
2023-04-26 14:34:29 +0300

    feat(ceramic): the ceramic node instance is conditioned, alterntively it creates the redirect to the new ceramic node in the lb

--- infra/ceramic-mainnet/index.ts
+++ infra/ceramic-mainnet/index.ts
@@ -13,6 +13,10 @@ import * as awsx from "@pulumi/awsx";
 let route53Zone = `${process.env["ROUTE_53_ZONE"]}`;
 let domain = `ceramic.${process.env["DOMAIN"]}`;
 let rpcUrl = `${process.env["RPC_URL"]}`;
+let ceramicCommand = null;
+let frontendUrlEcs = null;
+let clusterId = null;
+const createCluster = false;
 const serviceAccount = aws.elb.getServiceAccount({});
 
 //////////////////////////////////////////////////////////////
@@ -228,230 +232,264 @@ const privateMountTarget_1 = new aws.efs.MountTarget(`dpopp-ipfs-data-privateMou
 const publicMountTarget_2 = new aws.efs.MountTarget(`dpopp-ipfs-data-privateMountTarget-2`, {
   fileSystemId: efs.id,
   subnetId: vpcPrivateSubnetId2Str,
-  securityGroups: [sg.id]
+  securityGroups: [sg.id],
 });
 
-//////////////////////////////////////////////////////////////
-// Set up ALB and ECS cluster
-//////////////////////////////////////////////////////////////
+if (createCluster) {
+  //////////////////////////////////////////////////////////////
+  // Set up ALB and ECS cluster
+  //////////////////////////////////////////////////////////////
 
-const cluster = new awsx.ecs.Cluster("gitcoin-ceramic", { vpc });
-export const clusterId = cluster.id;
+  const cluster = new awsx.ecs.Cluster("gitcoin-ceramic", { vpc });
+  clusterId = cluster.id;
 
-const alb = new awsx.lb.ApplicationLoadBalancer(`gitcoin-ceramic`, {
-  vpc,
-  accessLogs: {
-    bucket: accessLogsBucket.bucket,
-    enabled: true,
-  },
-  idleTimeout: 120
-});
-
-// Listen to HTTP traffic on port 80 and redirect to 443
-const httpListener = alb.createListener("gitcoin-ceramic-http", {
-  port: 80,
-  protocol: "HTTP",
-  defaultAction: {
-    type: "redirect",
-    redirect: {
-      protocol: "HTTPS",
-      port: "443",
-      statusCode: "HTTP_301",
+  const alb = new awsx.lb.ApplicationLoadBalancer(`gitcoin-ceramic`, {
+    vpc,
+    accessLogs: {
+      bucket: accessLogsBucket.bucket,
+      enabled: true,
     },
-  },
-});
-export const frontendUrlEcs = pulumi.interpolate`http://${httpListener.endpoint.hostname}/`;
+    idleTimeout: 120,
+  });
 
-const target = alb.createTargetGroup("gitcoin-dpopp-ceramic", {
-  vpc,
-  port: 80,
-  healthCheck: { path: "/api/v0/node/healthcheck", unhealthyThreshold: 3, port: "80", interval: 60, timeout: 30, healthyThreshold: 2 },
-});
-
-// Listen to traffic on port 443 & route it through the Ceramic target group
-const httpsListener = target.createListener("gitcoin-ceramic-https", {
-  port: 443,
-  certificateArn: certificateValidation.certificateArn,
-});
-
-// Target group for the IPFS container
-const ceramicTarget = alb.createTargetGroup("gitcoin-dpopp-swarm", {
-  vpc,
-  port: 4001,
-  protocol: "HTTP",
-  healthCheck: { path: "/", unhealthyThreshold: 5, port: "8011", interval: 60, timeout: 30 },
-});
-
-const ceramicListener = ceramicTarget.createListener("gitcoin-dpopp-swarm", {
-  protocol: "HTTP",
-  port: 4001,
-});
-
-const ipfsTarget = alb.createTargetGroup("gitcoin-dpopp-ipfs", {
-  vpc,
-  port: 5001,
-  protocol: "HTTP",
-  healthCheck: { path: "/", unhealthyThreshold: 5, port: "8011", interval: 60, timeout: 30 },
-});
-
-const ipfsListener = ipfsTarget.createListener("gitcoin-dpopp-ipfs", {
-  protocol: "HTTP",
-  port: 5001,
-});
-
-const ipfsHealthcheckTarget = alb.createTargetGroup("dpopp-ipfs-healthcheck", {
-  vpc,
-  port: 8011,
-  protocol: "HTTP",
-  healthCheck: { path: "/", unhealthyThreshold: 5, port: "8011", interval: 60, timeout: 30 },
-});
-
-const ipfsHealthcheckListener = ipfsHealthcheckTarget.createListener("ipfs-healthcheck", {
-  protocol: "HTTP",
-  port: 8011,
-});
-
-const ipfsWS = alb.createTargetGroup("dpopp-ipfs-ws", {
-  vpc,
-  port: 8081,
-  protocol: "HTTP",
-  healthCheck: { path: "/", unhealthyThreshold: 5, port: "8011", interval: 60, timeout: 30 },
-});
-
-const ifpsWSListener = ipfsWS.createListener("ipfs-ws", {
-  protocol: "HTTP",
-  port: 8081,
-});
-
-// Create a DNS record for the load balancer
-const www = new aws.route53.Record("www", {
-  zoneId: route53Zone,
-  name: domain,
-  type: "A",
-  aliases: [
-    {
-      name: httpsListener.endpoint.hostname,
-      zoneId: httpsListener.loadBalancer.loadBalancer.zoneId,
-      evaluateTargetHealth: true,
+  // Listen to HTTP traffic on port 80 and redirect to 443
+  const httpListener = alb.createListener("gitcoin-ceramic-http", {
+    port: 80,
+    protocol: "HTTP",
+    defaultAction: {
+      type: "redirect",
+      redirect: {
+        protocol: "HTTPS",
+        port: "443",
+        statusCode: "HTTP_301",
+      },
     },
-  ],
-});
+  });
+  frontendUrlEcs = pulumi.interpolate`http://${httpListener.endpoint.hostname}/`;
 
-function makeCmd(inputbucketName: pulumi.Input<string>, inputIpfsUrl: pulumi.Input<string>): pulumi.Output<string[]> {
-  let bucketName = pulumi.output(inputbucketName);
-  let ipfsUrl = pulumi.output(inputIpfsUrl);
-  return pulumi.all([bucketName, ipfsUrl]).apply((t: [string, string]) => {
-    const bucketName = t[0];
-    const ipfsUrl = t[1];
-    return [
-      "--port",
-      "80",
-      "--hostname",
-      "0.0.0.0",
-      "--network",
-      "elp",
-      "--ipfs-api",
-      ipfsUrl,
-      "--log-to-files",
-      "--cors-allowed-origins",
-      ".*",
-      "--state-store-s3-bucket",
-      bucketName,
-      "--ethereum-rpc",
-      rpcUrl,
-      "--verbose",
-    ];
+  const target = alb.createTargetGroup("gitcoin-dpopp-ceramic", {
+    vpc,
+    port: 80,
+    healthCheck: {
+      path: "/api/v0/node/healthcheck",
+      unhealthyThreshold: 3,
+      port: "80",
+      interval: 60,
+      timeout: 30,
+      healthyThreshold: 2,
+    },
+  });
+
+  // Listen to traffic on port 443 & route it through the Ceramic target group
+  const httpsListener = target.createListener("gitcoin-ceramic-https", {
+    port: 443,
+    certificateArn: certificateValidation.certificateArn,
+  });
+
+  // Target group for the IPFS container
+  const ceramicTarget = alb.createTargetGroup("gitcoin-dpopp-swarm", {
+    vpc,
+    port: 4001,
+    protocol: "HTTP",
+    healthCheck: { path: "/", unhealthyThreshold: 5, port: "8011", interval: 60, timeout: 30 },
+  });
+
+  const ceramicListener = ceramicTarget.createListener("gitcoin-dpopp-swarm", {
+    protocol: "HTTP",
+    port: 4001,
+  });
+
+  const ipfsTarget = alb.createTargetGroup("gitcoin-dpopp-ipfs", {
+    vpc,
+    port: 5001,
+    protocol: "HTTP",
+    healthCheck: { path: "/", unhealthyThreshold: 5, port: "8011", interval: 60, timeout: 30 },
+  });
+
+  const ipfsListener = ipfsTarget.createListener("gitcoin-dpopp-ipfs", {
+    protocol: "HTTP",
+    port: 5001,
+  });
+
+  const ipfsHealthcheckTarget = alb.createTargetGroup("dpopp-ipfs-healthcheck", {
+    vpc,
+    port: 8011,
+    protocol: "HTTP",
+    healthCheck: { path: "/", unhealthyThreshold: 5, port: "8011", interval: 60, timeout: 30 },
+  });
+
+  const ipfsHealthcheckListener = ipfsHealthcheckTarget.createListener("ipfs-healthcheck", {
+    protocol: "HTTP",
+    port: 8011,
+  });
+
+  const ipfsWS = alb.createTargetGroup("dpopp-ipfs-ws", {
+    vpc,
+    port: 8081,
+    protocol: "HTTP",
+    healthCheck: { path: "/", unhealthyThreshold: 5, port: "8011", interval: 60, timeout: 30 },
+  });
+
+  const ifpsWSListener = ipfsWS.createListener("ipfs-ws", {
+    protocol: "HTTP",
+    port: 8081,
+  });
+
+  // Create a DNS record for the load balancer
+  const www = new aws.route53.Record("www", {
+    zoneId: route53Zone,
+    name: domain,
+    type: "A",
+    aliases: [
+      {
+        name: httpsListener.endpoint.hostname,
+        zoneId: httpsListener.loadBalancer.loadBalancer.zoneId,
+        evaluateTargetHealth: true,
+      },
+    ],
+  });
+
+  function makeCmd(inputbucketName: pulumi.Input<string>, inputIpfsUrl: pulumi.Input<string>): pulumi.Output<string[]> {
+    let bucketName = pulumi.output(inputbucketName);
+    let ipfsUrl = pulumi.output(inputIpfsUrl);
+    return pulumi.all([bucketName, ipfsUrl]).apply((t: [string, string]) => {
+      const bucketName = t[0];
+      const ipfsUrl = t[1];
+      return [
+        "--port",
+        "80",
+        "--hostname",
+        "0.0.0.0",
+        "--network",
+        "elp",
+        "--ipfs-api",
+        ipfsUrl,
+        "--log-to-files",
+        "--cors-allowed-origins",
+        ".*",
+        "--state-store-s3-bucket",
+        bucketName,
+        "--ethereum-rpc",
+        rpcUrl,
+        "--verbose",
+      ];
+    });
+  }
+
+  ceramicCommand = makeCmd(ceramicStateBucketName, pulumi.interpolate`http://${httpListener.endpoint.hostname}:5001`);
+
+  const service = new awsx.ecs.FargateService("dpopp-ceramic", {
+    cluster,
+    desiredCount: 1,
+    subnets: vpc.privateSubnetIds,
+    enableExecuteCommand: true,
+    taskDefinitionArgs: {
+      containers: {
+        ceramic: {
+          image: "ceramicnetwork/js-ceramic:daf32d0470a0c378a52cb4884020526c6718ec44",
+          memory: 8192,
+          cpu: 4096,
+          portMappings: [httpsListener],
+          links: [],
+          command: ceramicCommand,
+          ulimits: [
+            {
+              name: "nofile",
+              hardLimit: 1000000,
+              softLimit: 1000000,
+            },
+          ],
+          environment: [
+            { name: "NODE_ENV", value: "production" },
+            { name: "AWS_ACCESS_KEY_ID", value: usrS3Key },
+            { name: "AWS_SECRET_ACCESS_KEY", value: usrS3Secret },
+            { name: "NODE_OPTIONS", value: "--max-old-space-size=7168" },
+            { name: "CERAMIC_STREAM_CACHE_LIMIT", value: "20000" },
+            { name: "CERAMIC_DISABLE_PUBSUB_UPDATES", value: "false" },
+          ],
+        },
+      },
+    },
+  });
+
+  const serviceIPFS = new awsx.ecs.FargateService("dpopp-ipfs", {
+    cluster,
+    desiredCount: 1,
+    subnets: vpc.privateSubnetIds,
+    enableExecuteCommand: true,
+    taskDefinitionArgs: {
+      containers: {
+        ipfs: {
+          image: "ceramicnetwork/go-ipfs-daemon:15f69c080be97fc884552a7fbdca5778f492e47d",
+          memory: 12288,
+          cpu: 4096,
+          portMappings: [ceramicListener, ipfsListener, ipfsHealthcheckListener, ifpsWSListener],
+          links: [],
+          ulimits: [
+            {
+              name: "nofile",
+              hardLimit: 1000000,
+              softLimit: 1000000,
+            },
+          ],
+          environment: [
+            { name: "IPFS_ENABLE_S3", value: "true" },
+            { name: "IPFS_S3_REGION", value: "us-east-1" },
+            { name: "IPFS_S3_BUCKET_NAME", value: ipfsBucketName },
+            { name: "IPFS_S3_ROOT_DIRECTORY", value: "root" },
+            { name: "IPFS_S3_ACCESS_KEY_ID", value: usrS3Key },
+            { name: "IPFS_S3_SECRET_ACCESS_KEY", value: usrS3Secret },
+            { name: "IPFS_S3_KEY_TRANSFORM", value: "next-to-last/2" },
+            { name: "GOLOG_LOG_LEVEL", value: "info" },
+            { name: "GODEBUG", value: "gctrace=1" },
+          ],
+          mountPoints: [
+            {
+              sourceVolume: "dpopp-ipfs-data-volume",
+              containerPath: "/data/ipfs",
+            },
+          ],
+        },
+      },
+      volumes: [
+        {
+          name: `dpopp-ipfs-data-volume`,
+          efsVolumeConfiguration: {
+            fileSystemId: efs.id,
+            transitEncryption: "ENABLED",
+          },
+        },
+      ],
+    },
+  });
+} else {
+  const alb = new awsx.lb.ApplicationLoadBalancer(`gitcoin-ceramic`, {
+    vpc,
+    accessLogs: {
+      bucket: accessLogsBucket.bucket,
+      enabled: true,
+    },
+    idleTimeout: 120,
+  });
+
+  // Listen to HTTP traffic on port 80 and redirect to 443
+  const httpListener = alb.createListener("gitcoin-ceramic-http", {
+    port: 443,
+    protocol: "HTTPS",
+    defaultAction: {
+      type: "redirect",
+      redirect: {
+        host: "ceramic-gitcoin.hirenodes.io",
+        protocol: "HTTPS",
+        port: "443",
+        statusCode: "HTTP_301",
+      },
+    },
   });
 }
 
-export const ceramicCommand = makeCmd(
-  ceramicStateBucketName,
-  pulumi.interpolate`http://${httpListener.endpoint.hostname}:5001`
-);
-
-const service = new awsx.ecs.FargateService("dpopp-ceramic", {
-  cluster,
-  desiredCount: 1,
-  subnets: vpc.privateSubnetIds,
-  enableExecuteCommand: true,
-  taskDefinitionArgs: {
-    containers: {
-      ceramic: {
-        image: "ceramicnetwork/js-ceramic:daf32d0470a0c378a52cb4884020526c6718ec44",
-        memory: 8192,
-        cpu: 4096,
-        portMappings: [httpsListener],
-        links: [],
-        command: ceramicCommand,
-        "ulimits": [
-          {
-              "name": "nofile",
-              "hardLimit": 1000000,
-              "softLimit": 1000000
-          }
-        ],
-        environment: [
-          { name: "NODE_ENV", value: "production" },
-          { name: "AWS_ACCESS_KEY_ID", value: usrS3Key },
-          { name: "AWS_SECRET_ACCESS_KEY", value: usrS3Secret },
-          { name: "NODE_OPTIONS", value: "--max-old-space-size=7168" },
-          { name: "CERAMIC_STREAM_CACHE_LIMIT", value: "20000" },
-          { name: "CERAMIC_DISABLE_PUBSUB_UPDATES", value: "false" },
-        ],
-      },
-    },
-  },
-});
-
-const serviceIPFS = new awsx.ecs.FargateService("dpopp-ipfs", {
-  cluster,
-  desiredCount: 1,
-  subnets: vpc.privateSubnetIds,
-  enableExecuteCommand: true,
-  taskDefinitionArgs: {
-    containers: {
-      ipfs: {
-        image: "ceramicnetwork/go-ipfs-daemon:15f69c080be97fc884552a7fbdca5778f492e47d", 
-        memory: 12288,
-        cpu: 4096,
-        portMappings: [ceramicListener, ipfsListener, ipfsHealthcheckListener, ifpsWSListener],
-        links: [],
-        "ulimits": [
-          {
-              "name": "nofile",
-              "hardLimit": 1000000,
-              "softLimit": 1000000
-          }
-        ],
-        environment: [
-          { name: "IPFS_ENABLE_S3", value: "true" },
-          { name: "IPFS_S3_REGION", value: "us-east-1" },
-          { name: "IPFS_S3_BUCKET_NAME", value: ipfsBucketName },
-          { name: "IPFS_S3_ROOT_DIRECTORY", value: "root" },
-          { name: "IPFS_S3_ACCESS_KEY_ID", value: usrS3Key },
-          { name: "IPFS_S3_SECRET_ACCESS_KEY", value: usrS3Secret },
-          { name: "IPFS_S3_KEY_TRANSFORM", value: "next-to-last/2" },
-          { name: "GOLOG_LOG_LEVEL", value: "info" },
-          { name: "GODEBUG", value: "gctrace=1" },
-        ],
-        mountPoints: [
-          {
-            sourceVolume: "dpopp-ipfs-data-volume",
-            containerPath: "/data/ipfs",
-          },
-        ],
-      },
-    },
-    volumes: [
-      {
-        name: `dpopp-ipfs-data-volume`,
-        efsVolumeConfiguration: {
-          fileSystemId: efs.id,
-          transitEncryption: "ENABLED",
-        },
-      },
-    ],
-  },
-});
-
 export const ceramicUrl = pulumi.interpolate`https://${domain}`;
+
+export { ceramicCommand };
+export { frontendUrlEcs };
+export { clusterId };
