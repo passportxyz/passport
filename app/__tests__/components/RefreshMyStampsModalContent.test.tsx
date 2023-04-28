102bac857b23f9b957e377421ca599622bcc3759
Lucian Hymer <lucian@gitcoin.co>
2023-04-28 11:02:54 -0700

    updated tests

--- app/__tests__/components/RefreshMyStampsModalContent.test.tsx
+++ app/__tests__/components/RefreshMyStampsModalContent.test.tsx
@@ -8,7 +8,7 @@ import {
 } from "../../__test-fixtures__/contextTestHelpers";
 
 import { useNavigate } from "react-router-dom";
-import { PossibleEVMProvider } from "../../signer/utils";
+import { ValidatedPlatform } from "../../signer/utils";
 
 import { fetchVerifiableCredential } from "@gitcoin/passport-identity/dist/commonjs/src/credentials";
 import { reduceStampResponse } from "../../utils/helpers";
@@ -46,31 +46,28 @@ jest.mock("../../components/RefreshMyStampsModalContentCardList.tsx", () => ({
   RefreshMyStampsModalContentCardList: () => <div>RefreshMyStampsModalContentCardList</div>,
 }));
 
-const fetchedPossibleEVMStamps: PossibleEVMProvider[] = [
+const validPlatforms: ValidatedPlatform[] = [
   {
-    validatedPlatformGroups: [
-      [
-        {
-          payload: {
-            valid: true,
-            record: {
-              address: "0xc79abb54e4824cdb65c71f2eeb2d7f2db5da1fb8",
-              hasGTE30DaysSinceFirstTxnOnTheMainnet: "true",
-            },
+    groups: [
+      {
+        name: "Eth",
+        providers: [
+          {
+            name: "FirstEthTxnProvider",
+            title: "First Eth Txn",
           },
-          providerType: "FirstEthTxnProvider",
-        },
-      ],
+        ],
+      },
     ],
     platformProps: {
       platform: {} as any,
       platFormGroupSpec: [
         {
-          platformGroup: "Possessions",
+          platformGroup: "Eth",
           providers: [
             {
-              title: "At least 1 ETH",
-              name: "ethPossessionsGte#1",
+              name: "FirstEthTxnProvider",
+              title: "First Eth Txn",
             },
           ],
         },
@@ -80,25 +77,26 @@ const fetchedPossibleEVMStamps: PossibleEVMProvider[] = [
 ];
 
 describe("RefreshMyStampsModalContent", () => {
-  it("renders the Stamps Found title when fetchedPossibleEVMStamps are provided", () => {
+  it("renders the Stamps Found title when validPlatforms are provided", () => {
     renderWithContext(
       mockUserContext,
       mockCeramicContext,
       <RefreshMyStampsModalContent
         onClose={mockOnClose}
-        fetchedPossibleEVMStamps={[]}
+        validPlatforms={validPlatforms}
         resetStampsAndProgressState={mockResetStampsAndProgressState}
       />
     );
-    expect(screen.getByText(/No Eligible Web3 Stamps Found/i)).toBeInTheDocument();
+    expect(screen.getByText(/Stamps Found/i)).toBeInTheDocument();
   });
+
   it("calls handleRefreshSelectedStamps when the 'Confirm Stamps' button is clicked", async () => {
     renderWithContext(
       mockUserContext,
       mockCeramicContext,
       <RefreshMyStampsModalContent
         onClose={mockOnClose}
-        fetchedPossibleEVMStamps={fetchedPossibleEVMStamps}
+        validPlatforms={validPlatforms}
         resetStampsAndProgressState={mockResetStampsAndProgressState}
       />
     );
@@ -109,7 +107,7 @@ describe("RefreshMyStampsModalContent", () => {
         iamUrl,
         {
           type: "EVMBulkVerify",
-          types: ["ethPossessionsGte#1"],
+          types: ["FirstEthTxnProvider"],
           version: "0.0.0",
           address: "0xmyAddress",
           proofs: {},
@@ -126,7 +124,7 @@ describe("RefreshMyStampsModalContent", () => {
       mockCeramicContext,
       <RefreshMyStampsModalContent
         onClose={mockOnClose}
-        fetchedPossibleEVMStamps={fetchedPossibleEVMStamps}
+        validPlatforms={validPlatforms}
         resetStampsAndProgressState={mockResetStampsAndProgressState}
       />
     );
--- app/__tests__/components/RefreshMyStampsSelector.test.tsx
+++ app/__tests__/components/RefreshMyStampsSelector.test.tsx
@@ -1,37 +1,36 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import { RefreshMyStampsSelector } from "../../components/RefreshMyStampsSelector";
-import { PlatformGroupSpec, PlatformSpec, PROVIDER_ID } from "@gitcoin/passport-platforms/src/types";
+import { PROVIDER_ID } from "@gitcoin/passport-platforms/src/types";
+import { ValidatedProviderGroup } from "../../signer/utils";
 
 const mockSetSelectedProviders = jest.fn();
 
-const currentProviders: PlatformGroupSpec[] = [
+const validPlatformGroups: ValidatedProviderGroup[] = [
   {
-    platformGroup: "Platform Group",
+    name: "Platform Group",
     providers: [
       {
-        title: "Provider Title",
-        name: "Ens" as PROVIDER_ID,
-        icon: "provider-icon.png",
-        description: "Provider description",
+        name: "FirstEthTxnProvider",
+        title: "First Eth Txn",
       },
     ],
   },
 ];
 
-const selectedProviders: PROVIDER_ID[] = ["Ens"];
+const selectedProviders: PROVIDER_ID[] = ["FirstEthTxnProvider"];
 
 describe("RefreshMyStampsSelector", () => {
   it("renders the component and handles checkbox change", () => {
     render(
       <RefreshMyStampsSelector
-        currentProviders={currentProviders}
+        validPlatformGroups={validPlatformGroups}
         selectedProviders={selectedProviders}
         setSelectedProviders={mockSetSelectedProviders}
         platformChecked={true}
       />
     );
 
-    const checkboxElement = screen.getByTestId(`checkbox-${currentProviders[0].providers[0].name}`);
+    const checkboxElement = screen.getByTestId(`checkbox-${validPlatformGroups[0].providers[0].name}`);
     expect(checkboxElement).toBeInTheDocument();
 
     fireEvent.click(checkboxElement);
@@ -41,28 +40,28 @@ describe("RefreshMyStampsSelector", () => {
   it("should mark checkbox as checked if selectedProviders are passed as props", () => {
     render(
       <RefreshMyStampsSelector
-        currentProviders={currentProviders}
+        validPlatformGroups={validPlatformGroups}
         selectedProviders={selectedProviders}
         setSelectedProviders={mockSetSelectedProviders}
         platformChecked={true}
       />
     );
 
-    const checkboxElement = screen.getByTestId(`checkbox-${currentProviders[0].providers[0].name}`);
+    const checkboxElement = screen.getByTestId(`checkbox-${validPlatformGroups[0].providers[0].name}`);
     expect(checkboxElement).toHaveAttribute("data-checked");
   });
 
   it("should disable checkboxes if platformChecked is false", () => {
     render(
       <RefreshMyStampsSelector
-        currentProviders={currentProviders}
+        validPlatformGroups={validPlatformGroups}
         selectedProviders={selectedProviders}
         setSelectedProviders={mockSetSelectedProviders}
         platformChecked={false}
       />
     );
 
-    const checkboxElement = screen.getByTestId(`checkbox-${currentProviders[0].providers[0].name}`);
+    const checkboxElement = screen.getByTestId(`checkbox-${validPlatformGroups[0].providers[0].name}`);
     expect(checkboxElement).toHaveAttribute("disabled");
   });
 });
