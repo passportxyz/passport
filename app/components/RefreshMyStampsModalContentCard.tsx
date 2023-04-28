272fc5920c2ce7c893d5af51a46f5ef21ebc23a8
Chibuotu Amadi <chibuotu@gmail.com>
2023-04-28 01:31:38 +0100

    feat(1-click): hide credentials on disabled stamp

--- app/components/RefreshMyStampsModalContentCard.tsx
+++ app/components/RefreshMyStampsModalContentCard.tsx
@@ -1,5 +1,5 @@
 // --- React hooks
-import { useMemo } from "react";
+import { useMemo, useState, useRef } from "react";
 
 // --- UI components
 import { Switch, Accordion, AccordionItem, AccordionButton, AccordionPanel, AccordionIcon } from "@chakra-ui/react";
@@ -23,6 +23,9 @@ export const RefreshMyStampsModalContentCard = ({
   selectedProviders,
   setSelectedProviders,
 }: RefreshMyStampsModalCardProps): JSX.Element => {
+  const [disableExpand, setDisableExpand] = useState(false);
+
+  const accordionButton = useRef<HTMLButtonElement>(null);
   const platformProviders = useMemo(
     () => platformGroup.map((group) => group.providers?.map((provider) => provider.name)).flat(),
     [platformGroup]
@@ -36,22 +39,26 @@ export const RefreshMyStampsModalContentCard = ({
   return (
     <div>
       <Accordion allowToggle>
-        <AccordionItem className="py-2 first:border-t-accent-2 first:border-b-accent-2">
-          <AccordionButton
-            className="items-center justify-between text-white focus:shadow-none"
-            _focus={{ outline: "none" }}
-          >
-            <div className="flex items-center justify-start">
-              <img src={currentPlatform?.icon} alt={currentPlatform?.name} className="mr-5 h-11 w-11" />
-              <p className="text-left">{currentPlatform?.name}</p>
+        <AccordionItem className="py-2 first:border-t-accent-2 first:border-b-accent-2" isDisabled={disableExpand}>
+          <div className="grid grid-cols-10 items-center justify-between text-white focus:shadow-none">
+            <div className="col-span-8 items-center justify-start">
+              <AccordionButton _focus={{ outline: "none" }} ref={accordionButton}>
+                <img src={currentPlatform?.icon} alt={currentPlatform?.name} className="mr-5 h-11 w-11" />
+                <p className="text-left">{currentPlatform?.name}</p>
+              </AccordionButton>
             </div>
-            <div className="grid grid-cols-2 items-center">
+            <div className="grid grid-cols-2 items-center justify-end gap-8">
               <Switch
                 data-testid={`switch-${currentPlatform?.name}`}
                 value={`${currentPlatform?.name}`}
                 colorScheme="purple"
                 isChecked={checked}
                 onChange={(e) => {
+                  if (!disableExpand) {
+                    // collapse before disabling accordion
+                    accordionButton.current?.click();
+                  }
+                  setDisableExpand(!disableExpand);
                   if (e.target.checked) {
                     setSelectedProviders((selectedProviders || []).concat(platformProviders));
                   } else {
@@ -61,9 +68,16 @@ export const RefreshMyStampsModalContentCard = ({
                   }
                 }}
               />
-              <AccordionIcon marginLeft="8px" fontSize="28px" />
+              <AccordionIcon
+                className="cursor-pointer"
+                marginLeft="8px"
+                fontSize="28px"
+                onClick={() => {
+                  accordionButton.current?.click();
+                }}
+              />
             </div>
-          </AccordionButton>
+          </div>
           <AccordionPanel borderTop="1px solid #083A40" marginTop="8px" paddingLeft="0" paddingRight="0">
             <RefreshMyStampsSelector
               currentPlatform={currentPlatform}
