d789a1bb53a34c0792c30b6fb35f249d295e5b20
Tim Schultz <schultztimothy@users.noreply.github.com>
2023-05-01 17:12:16 -0600

    fix(platforms): add path to staking app-bindings and return checkSum … (#1215)
    * fix(platforms): add path to staking app-bindings and return checkSum address from getAddress
    
    * fix(platforms): checkSum address in individual provider

--- platforms/src/GtcStaking/App-Bindings.ts
+++ platforms/src/GtcStaking/App-Bindings.ts
@@ -3,7 +3,7 @@ import { AppContext, Platform, PlatformOptions, ProviderPayload } from "../types
 
 export class GTCStakingPlatform implements Platform {
   platformId = "GtcStaking";
-  path = "";
+  path = "GtcStaking";
   isEVM = true;
   clientId: string = null;
   redirectUri: string = null;
