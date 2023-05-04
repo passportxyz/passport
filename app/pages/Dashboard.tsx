29d8f04535db1838332084a3277e2e0d45168965
Lucian Hymer <lucian@gitcoin.co>
2023-05-04 15:52:36 -0700

    fix(app): positioning tweaks for subheader

--- app/pages/Dashboard.tsx
+++ app/pages/Dashboard.tsx
@@ -230,47 +230,42 @@ export default function Dashboard() {
             </Tooltip>
           </div>
         </div>
-        {isLiveAlloScoreEnabled && (
-          <div
-            className={`col-span-1 col-end-[-${
-              isOnChainSyncEnabled ? "3" : "2"
-            }] flex min-w-fit items-center justify-self-end`}
-          >
-            <div className={`pr-2 ${passportSubmissionState === "APP_REQUEST_PENDING" ? "visible" : "invisible"}`}>
-              <Spinner
-                className="my-[2px]"
-                thickness="2px"
-                speed="0.65s"
-                emptyColor="darkGray"
-                color="gray"
-                size="md"
-              />
-            </div>
-            <div className="flex flex-col items-center">
-              <div className="flex text-2xl">
-                {/* TODO add color to theme */}
-                <span className={`${score == 1 ? "text-accent-3" : "text-[#FFE28A]"}`}>{rawScore.toFixed(2)}</span>
-                <Tooltip>
-                  Your Unique Humanity Score is based out of 100 and measures how unique you are. The current passing
-                  score threshold is 15.
-                </Tooltip>
+        <div className="col-span-2 col-end-[-1] flex justify-self-end">
+          {isLiveAlloScoreEnabled && (
+            <div className={"flex min-w-fit items-center"}>
+              <div className={`pr-2 ${passportSubmissionState === "APP_REQUEST_PENDING" ? "visible" : "invisible"}`}>
+                <Spinner
+                  className="my-[2px]"
+                  thickness="2px"
+                  speed="0.65s"
+                  emptyColor="darkGray"
+                  color="gray"
+                  size="md"
+                />
               </div>
-              <div className="flex whitespace-nowrap text-sm">{scoreDescription}</div>
+              <div className="flex flex-col items-center">
+                <div className="flex text-2xl">
+                  {/* TODO add color to theme */}
+                  <span className={`${score == 1 ? "text-accent-3" : "text-[#FFE28A]"}`}>{rawScore.toFixed(2)}</span>
+                  <Tooltip>
+                    Your Unique Humanity Score is based out of 100 and measures how unique you are. The current passing
+                    score threshold is 15.
+                  </Tooltip>
+                </div>
+                <div className="flex whitespace-nowrap text-sm">{scoreDescription}</div>
+              </div>
             </div>
-          </div>
-        )}
+          )}
 
-        {isOnChainSyncEnabled && (
-          <div className="col-span-1 col-end-[-2] justify-self-end">
-            <SyncToChainButton />
-          </div>
-        )}
-
-        <div className="col-span-1 col-end-[-1] justify-self-end">
+          {isOnChainSyncEnabled && (
+            <div className="mx-4">
+              <SyncToChainButton />
+            </div>
+          )}
           {passport ? (
             <button
               data-testid="button-passport-json-mobile"
-              className="h-10 w-10 rounded-md border border-accent-2 border-gray-300"
+              className="h-10 w-10 rounded-md border border-muted"
               onClick={onOpen}
             >
               {`</>`}
@@ -278,7 +273,7 @@ export default function Dashboard() {
           ) : (
             <div
               data-testid="loading-spinner-passport"
-              className="flex flex-row items-center rounded-md border-2 border-gray-300 py-2 px-4"
+              className="flex flex-row items-center rounded-md border-2 border-muted py-2 px-4"
             >
               <Spinner
                 className="my-[2px]"
