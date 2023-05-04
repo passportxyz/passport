6110e9c302b4b9089a5809d0abf6b795d5b946bf
Lucian Hymer <lucian@gitcoin.co>
2023-05-04 16:03:50 -0700

    fixed line break when on chain disabled

--- app/pages/Dashboard.tsx
+++ app/pages/Dashboard.tsx
@@ -230,7 +230,7 @@ export default function Dashboard() {
             </Tooltip>
           </div>
         </div>
-        <div className="col-start-[-3] col-end-[-1] flex justify-self-end">
+        <div className={`col-start-[-${isOnChainSyncEnabled ? 3 : 2}] col-end-[-1] flex justify-self-end`}>
           {isLiveAlloScoreEnabled && (
             <div className={"flex min-w-fit items-center"}>
               <div className={`pr-2 ${passportSubmissionState === "APP_REQUEST_PENDING" ? "visible" : "invisible"}`}>
