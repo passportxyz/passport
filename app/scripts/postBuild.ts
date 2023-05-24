e57646207cb655b9104715c0a8f09c3a0c6e7e82
Lucian Hymer <lucian.hymer@gmail.com>
2023-05-24 15:40:54 -0700

    feat(app): added script to generate platforms.json

--- app/scripts/postBuild.ts
+++ app/scripts/postBuild.ts
@@ -2,12 +2,53 @@ import dotenv from "dotenv";
 import { writeFileSync } from "fs";
 import { join } from "path";
 
-import { platforms } from "@gitcoin/passport-platforms";
+import { PlatformGroupSpec, platforms } from "@gitcoin/passport-platforms";
 
 dotenv.config();
 
-const platformData: any = [];
-console.log("POST BUILD");
+type GroupData = {
+  name: string;
+  stamps: string[];
+};
 
-// Parse platforms and write to a JSON file in the public directory
-// writeFileSync(join(__dirname, "public", "providers.json"), JSON.stringify(platformData));
+type PlatformData = {
+  name: string;
+  icon: string;
+  description: string;
+  connectMessage: string;
+  groups: GroupData[];
+};
+
+const formatPlatformGroups = (providerConfig: PlatformGroupSpec[]) =>
+  providerConfig.reduce(
+    (groups: GroupData[], group: PlatformGroupSpec) => [
+      ...groups,
+      {
+        name: group.platformGroup,
+        stamps: group.providers.map(({ name }) => name),
+      },
+    ],
+    [] as GroupData[]
+  );
+
+const platformsData = Object.entries(platforms).reduce((data, [id, platform]) => {
+  const { name, icon, description, connectMessage } = platform.PlatformDetails;
+  const groups = formatPlatformGroups(platform.ProviderConfig);
+
+  return [
+    ...data,
+    {
+      id,
+      name,
+      icon,
+      description,
+      connectMessage,
+      groups,
+    },
+  ];
+}, [] as PlatformData[]);
+
+const outPath = join(__dirname, "..", "public", "platforms.json");
+console.log(`Saving platform info to JSON file at ${outPath}`);
+
+writeFileSync(outPath, JSON.stringify(platformsData));
