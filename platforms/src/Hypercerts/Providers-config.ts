1aac61dba3411410d0a942ad94e16e3715150923
Tim Schultz <schultztimothy@users.noreply.github.com>
2023-05-25 11:41:26 -0600

    1236 update hypercert permissions (#1307)
    * chore(platforms): update hypercert copy
    
    * feat(platforms): check that hypercerts are greater than 15 days and that they have more than one
    
    * chore(platforms): update hypercert eligibility

--- platforms/src/Hypercerts/Providers-config.ts
+++ platforms/src/Hypercerts/Providers-config.ts
@@ -4,11 +4,14 @@ export const HypercertsPlatformDetails: PlatformSpec = {
   icon: "./assets/hypercertsStampIcon.svg",
   platform: "Hypercerts",
   name: "Hypercerts",
-  description: "Connect your wallet to verify that you hold a Hypercert",
+  description: "Connect your wallet to verify that you hold Hypercerts",
   connectMessage: "Connect Wallet",
   isEVM: true,
 };
 
 export const HypercertsProviderConfig: PlatformGroupSpec[] = [
-  { platformGroup: "Account Name", providers: [{ title: "Holds at least 1 Hypercert", name: "Hypercerts" }] },
+  {
+    platformGroup: "Account Name",
+    providers: [{ title: "Held at least two Hypercerts for more than 15 days", name: "Hypercerts" }],
+  },
 ];
