aa34ad72e3ede38e3224c65df6e751e93e9c8029
Tim Schultz <schultztimothy@users.noreply.github.com>
2023-05-25 11:41:26 -0600

    1236 update hypercert permissions (#1307)
    * chore(platforms): update hypercert copy
    
    * feat(platforms): check that hypercerts are greater than 15 days and that they have more than one
    
    * chore(platforms): update hypercert eligibility

--- platforms/src/Hypercerts/Providers/__tests__/Hypercerts.test.ts
+++ platforms/src/Hypercerts/Providers/__tests__/Hypercerts.test.ts
@@ -9,6 +9,9 @@ const mockedAxiosPost = jest.spyOn(axios, "post");
 const MOCK_ADDRESS = "0xcF314CE817E25b4F784bC1f24c9A79A525fEC50f";
 const MOCK_ADDRESS_LOWER = MOCK_ADDRESS.toLowerCase();
 
+// Mock current date
+jest.spyOn(Date, "now").mockImplementation(() => 1677350400000); // This corresponds to "2023-01-25T00:00:00.000Z"
+
 // Mock Claim Tokens Response
 const mockClaimTokensResponse = {
   data: {
@@ -23,6 +26,18 @@ const mockClaimTokensResponse = {
             id: "1",
             creation: "1632189184",
             uri: "https://hypercerts.com/token/1",
+            totalUnits: "1000",
+          },
+        },
+        {
+          id: "2",
+          owner: MOCK_ADDRESS_LOWER,
+          tokenID: "2",
+          units: "100",
+          claim: {
+            id: "2",
+            creation: "1632189185",
+            uri: "https://hypercerts.com/token/2",
             totalUnits: "1000",
           },
         },
@@ -90,6 +105,73 @@ describe("Hypercerts Provider", () => {
     expect(res).toMatchObject({
       valid: false,
       error: ["Self Staking Gold Provider verifyStake Error"],
+    });
+
+    expect(mockedAxiosPost).toBeCalledTimes(1);
+  });
+
+  it("should handle claims older than 15 days", async () => {
+    const hypercertsProvider = new HypercertsProvider();
+    const verifiedPayload = await hypercertsProvider.verify({
+      address: MOCK_ADDRESS,
+    } as RequestPayload);
+
+    expect(verifiedPayload).toEqual({
+      valid: true,
+      record: {
+        address: MOCK_ADDRESS_LOWER,
+      },
+    });
+
+    expect(mockedAxiosPost).toBeCalledTimes(1);
+  });
+
+  it("should handle claims less than 15 days old", async () => {
+    mockedAxiosPost.mockResolvedValueOnce({
+      data: {
+        data: {
+          claimTokens: [
+            {
+              id: "1",
+              owner: MOCK_ADDRESS_LOWER,
+              tokenID: "1",
+              units: "100",
+              claim: {
+                id: "1",
+                // This timestamp corresponds to "2023-01-20T00:00:00.000Z" (less than 15 days ago)
+                creation: "1676889600",
+                uri: "https://hypercerts.com/token/1",
+                totalUnits: "1000",
+              },
+            },
+            {
+              id: "2",
+              owner: MOCK_ADDRESS_LOWER,
+              tokenID: "2",
+              units: "100",
+              claim: {
+                id: "2",
+                // Valid TS
+                creation: "1632189185",
+                uri: "https://hypercerts.com/token/2",
+                totalUnits: "1000",
+              },
+            },
+          ],
+        },
+      },
+    });
+
+    const hypercertsProvider = new HypercertsProvider();
+    const verifiedPayload = await hypercertsProvider.verify({
+      address: MOCK_ADDRESS,
+    } as RequestPayload);
+
+    expect(verifiedPayload).toEqual({
+      valid: false,
+      record: {
+        address: MOCK_ADDRESS_LOWER,
+      },
     });
 
     expect(mockedAxiosPost).toBeCalledTimes(1);
