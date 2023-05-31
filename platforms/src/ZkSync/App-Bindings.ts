564aa010dc4d24a0dc94c14bbc03e6a6a8fd5c37
Michael Green <michaelgreen@Michaels-MacBook-Air.local>
2023-05-31 14:33:39 -0600

    fix(app): add banner for zk-sync stamp indicating only transfers are recognized

--- platforms/src/ZkSync/App-Bindings.ts
+++ platforms/src/ZkSync/App-Bindings.ts
@@ -5,6 +5,11 @@ export class ZkSyncPlatform extends Platform {
   path = "ZkSync";
   isEVM = true;
 
+  banner = {
+    heading:
+      "Currently, only  We look for a transaction with a finalized status and an op.from field that matches the your address.",
+  };
+
   async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
     const result = await Promise.resolve({});
     return result;
