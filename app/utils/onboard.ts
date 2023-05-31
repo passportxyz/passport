8b7d8985bb3a2ee3d2fbb237ac27c63558a70a6a
Michael Green <michaelgreen@Michaels-MacBook-Air.local>
2023-05-31 14:38:42 -0600

    fix(app,dependencies): deleted unnecessary packages. removed inected wallets opt2

--- app/utils/onboard.ts
+++ app/utils/onboard.ts
@@ -2,79 +2,13 @@ import { init } from "@web3-onboard/react";
 import injectedModule from "@web3-onboard/injected-wallets";
 import ledgerModule from "@web3-onboard/ledger";
 import walletConnectModule from "@web3-onboard/walletconnect";
-import walletLinkModule from "@web3-onboard/walletlink";
 
 // RPC urls
 const MAINNET_RPC_URL = process.env.NEXT_PUBLIC_PASSPORT_MAINNET_RPC_URL as string;
 
-// // Injected wallet opt1
+// // Injected wallet - shows all available injected wallets
 
 const injected = injectedModule();
-
-// Injected wallet opt2
-// const injected = injectedModule({
-//   // display all wallets even if they are unavailable
-//   displayUnavailable: true,
-//   // but only show wallets if they are available
-//   filter: {
-//     [ProviderLabel.AlphaWallet]: "unavailable",
-//     [ProviderLabel.ApexWallet]: "unavailable",
-//     [ProviderLabel.AToken]: "unavailable",
-//     [ProviderLabel.BifrostWallet]: "unavailable",
-//     [ProviderLabel.Binance]: "unavailable",
-//     [ProviderLabel.Bitski]: "unavailable",
-//     [ProviderLabel.BlockWallet]: "unavailable",
-//     [ProviderLabel.Brave]: "unavailable",
-//     [ProviderLabel.Dcent]: "unavailable",
-//     [ProviderLabel.Exodus]: "unavailable",
-//     [ProviderLabel.Frame]: "unavailable",
-//     [ProviderLabel.Frontier]: "unavailable",
-//     [ProviderLabel.HuobiWallet]: "unavailable",
-//     [ProviderLabel.HyperPay]: "unavailable",
-//     [ProviderLabel.ImToken]: "unavailable",
-//     [ProviderLabel.InfinityWallet]: "unavailable",
-//     [ProviderLabel.Liquality]: "unavailable",
-//     [ProviderLabel.MeetOne]: "unavailable",
-//     [ProviderLabel.MyKey]: "unavailable",
-//     [ProviderLabel.Opera]: "unavailable",
-//     [ProviderLabel.OwnBit]: "unavailable",
-//     [ProviderLabel.Status]: "unavailable",
-//     [ProviderLabel.Trust]: "unavailable",
-//     [ProviderLabel.TokenPocket]: "unavailable",
-//     [ProviderLabel.TP]: "unavailable",
-//     [ProviderLabel.WalletIo]: "unavailable",
-//     [ProviderLabel.XDEFI]: "unavailable",
-//     [ProviderLabel.OneInch]: "unavailable",
-//     [ProviderLabel.Tokenary]: "unavailable",
-//     [ProviderLabel.Tally]: "unavailable",
-//     [ProviderLabel.Rabby]: "unavailable",
-//     [ProviderLabel.MathWallet]: "unavailable",
-//     [ProviderLabel.GameStop]: "unavailable",
-//     [ProviderLabel.BitKeep]: "unavailable",
-//     [ProviderLabel.Sequence]: "unavailable",
-//     [ProviderLabel.Core]: "unavailable",
-//     [ProviderLabel.Enkrypt]: "unavailable",
-//     [ProviderLabel.Zeal]: "unavailable",
-//     [ProviderLabel.Phantom]: "unavailable",
-//     [ProviderLabel.Zerion]: "unavailable",
-//     [ProviderLabel.Rainbow]: "unavailable",
-//     [ProviderLabel.SafePal]: "unavailable",
-//     [ProviderLabel.DeFiWallet]: "unavailable",
-//     [ProviderLabel.Safeheron]: "unavailable",
-//     [ProviderLabel.Talisman]: "unavailable",
-//   },
-//   // do a manual sort of injected wallets so that MetaMask and Coinbase are ordered first
-//   sort: (wallets) => {
-//     const coinbase = wallets.find(({ label }) => label === ProviderLabel.Coinbase);
-//     const okxwallet = wallets.find(({ label }) => label === ProviderLabel.OKXWallet);
-
-//     return (
-//       [coinbase, okxwallet, ...wallets]
-//         // remove undefined values
-//         .filter((wallet) => wallet)
-//     );
-//   },
-// });
 
 // web3Onboard modules
 
