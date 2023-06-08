c8daa7c32188ddacfb82a52ba034e659b4fd28e9
nutrina <nutrina9@gmail.com>
2023-06-08 22:25:31 +0300

    1309 score onchain (#1338)
    * feat(types): add eas passport types
    
    * feat(iam): request to fetch score from scorer service
    
    * feat(iam): encode new passport structure
    
    * feat(iam): update eas endpoint to sign updated passport data
    
    * feat(app): update frontend to account for new passport structure
    
    * feat(iam): update sample env variables
    
    * fix(iam): correct env variables and fee parsing
    
    * feat(iam): testing the new EAS implementation for bringing stampms & score on-chain
    
    * chore(iam): successful /eas test
    
    * chore(iam): test formatMultiAttestationRequest
    
    * chore(iam): lint and resolve fialing tests
    
    * chore(app, iam): remove recipient
    
    * chore(iam, app): removing unsued variables
    
    * fix(iam): ensure all recipients are equivalent before signing
    
    * chore(iam): use static api key and renamed fetchPassportScore
    
    * feat(iam): store score as bignumber represenatation fo decimal
    
    * chore(iam): update score schema
    
    * chore(infra, iam): ensure environment variables are configured for deployments
    
    * chore(iam): add default value to test ci
    
    ---------
    
    Co-authored-by: schultztimothy <schultz.timothy52@gmail.com>

---
+++ iam/src/utils/scorerService.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { Score } from "./easSchema";
+
+const scorerApiGetScore = `${process.env.SCORER_ENDPOINT}/registry/score/${process.env.ALLO_SCORER_ID}`;
+// Use public endpoint and static api key to fetch score
+export async function fetchPassportScore(address: string): Promise<Score> {
+  try {
+    const response: {
+      data: {
+        status: string;
+        evidence: {
+          rawScore: string;
+        };
+      };
+    } = await axios.get(`${scorerApiGetScore}/${address}`, {
+      headers: {
+        "X-API-Key": process.env.SCORER_API_KEY,
+      },
+    });
+
+    const { data } = response;
+
+    if (data.status !== "DONE") {
+      throw new Error(`Score not ready yet. Status: ${data.status}`);
+    }
+
+    const score: Score = {
+      score: Number(data.evidence.rawScore),
+      scorer_id: Number(process.env.ALLO_SCORER_ID),
+    };
+
+    return score;
+  } catch (e) {
+    throw new Error("Error fetching score");
+  }
+}
