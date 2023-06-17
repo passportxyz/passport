4daed4f8494aa460bd4066fd96ea823885ff6fe2
Chibuotu Amadi <chibuotu@gmail.com>
2023-06-17 01:23:18 +0100

    feat(app, platforms): integrate holonym stamp (#1348)
    * feat(iam, app, platforms): add holonym stamp
    
    * Update Holonym stamp description
    
    * chore: clean HolonymGovIdProvider
    
    * chore: fix failing test
    
    * chore(platforms): update gov_id_sr_address for holonym
    
    ---------
    
    Co-authored-by: calebtuttle <1calebtuttle@gmail.com>

---
+++ platforms/src/Holonym/Providers-config.ts
@@ -0,0 +1,21 @@
+import { PlatformSpec, PlatformGroupSpec, Provider } from "../types";
+import { HolonymGovIdProvider } from "./Providers/holonymGovIdProvider";
+
+export const PlatformDetails: PlatformSpec = {
+  icon: "./assets/holonymStampIcon.svg",
+  platform: "Holonym",
+  name: "Holonym",
+  description:
+    "To verify your Holo, mint your Holo at app.holonym.id and then prove uniqueness at app.holonym.id/prove/uniqueness",
+  connectMessage: "Connect Account",
+  isEVM: true,
+};
+
+export const ProviderConfig: PlatformGroupSpec[] = [
+  {
+    platformGroup: "Government ID",
+    providers: [{ title: "Proven uniqueness using Holonym with government ID", name: "HolonymGovIdProvider" }],
+  },
+];
+
+export const providers: Provider[] = [new HolonymGovIdProvider()];
