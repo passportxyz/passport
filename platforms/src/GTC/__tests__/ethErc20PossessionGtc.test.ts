053c4372a32453839893ece1ae3488c155aed5cc
Lucian <lucian@gitcoin.co>
2023-06-19 14:40:27 -0700

    1371 backend alchemy efficiency (#1380)
    * fixing nft endpoint
    
    * refactor(iam): remove redundant Alchemy calls and use more efficient NFT method
    
    * typo
    
    * refactor(iam): working primarily with string & BigNumber when checking posessions
    
    ---------
    
    Co-authored-by: Gerald Iakobinyi-Pich <nutrina9@gmail.com>

--- platforms/src/GTC/__tests__/ethErc20PossessionGtc.test.ts
+++ platforms/src/GTC/__tests__/ethErc20PossessionGtc.test.ts
@@ -5,11 +5,7 @@ import { RequestPayload } from "@gitcoin/passport-types";
 import { EthErc20PossessionProvider } from "../../ETH/Providers/ethErc20Possession";
 
 // ----- Ethers library
-import { formatUnits } from "@ethersproject/units";
-
-jest.mock("@ethersproject/units", () => ({
-  formatUnits: jest.fn(),
-}));
+import * as units from "@ethersproject/units";
 
 const mockBalanceOf = jest.fn();
 jest.mock("ethers", () => {
@@ -31,24 +27,28 @@ describe("Attempt verification", function () {
   beforeEach(() => {
     jest.clearAllMocks();
     mockBalanceOf.mockResolvedValue(MOCK_BALANCE);
-    (formatUnits as jest.Mock).mockImplementation((num: string, power: number) => {
-      return parseFloat(num) / Math.pow(10, power);
-    });
   });
 
   it("should return valid response", async () => {
+    const parseUnits = jest.spyOn(units, "parseUnits");
+
     const gtcPossessions = new EthErc20PossessionProvider({
-      threshold: 100,
+      threshold: "100",
       recordAttribute: "gtcPossessionsGte",
       contractAddress: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f",
     });
 
-    const verifiedPayload = await gtcPossessions.verify({
-      address: MOCK_ADDRESS_LOWER,
-    } as unknown as RequestPayload);
+    const verifiedPayload = await gtcPossessions.verify(
+      {
+        address: MOCK_ADDRESS_LOWER,
+      } as unknown as RequestPayload,
+      {}
+    );
 
     expect(mockBalanceOf).toBeCalledWith(MOCK_ADDRESS_LOWER);
-    expect(formatUnits).toBeCalledWith(MOCK_BALANCE, 18);
+    // Check that parseUnits is called for threshold and balance
+    expect(parseUnits).toBeCalledWith("100", 18);
+    expect(parseUnits).toBeCalledWith(MOCK_BALANCE, 0);
     expect(verifiedPayload).toEqual({
       valid: true,
       record: {
@@ -61,15 +61,18 @@ describe("Attempt verification", function () {
   it("should return false for an improper address", async () => {
     mockBalanceOf.mockRejectedValueOnce("0");
     const gtcPossessions = new EthErc20PossessionProvider({
-      threshold: 100,
+      threshold: "100",
       recordAttribute: "gtcPossessionsGte",
       contractAddress: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f",
       error: "GTC Possessions >= 100 Provider verify Error",
     });
 
-    const verifiedPayload = await gtcPossessions.verify({
-      address: MOCK_FAKE_ADDRESS,
-    } as RequestPayload);
+    const verifiedPayload = await gtcPossessions.verify(
+      {
+        address: MOCK_FAKE_ADDRESS,
+      } as RequestPayload,
+      {}
+    );
 
     expect(mockBalanceOf).toBeCalledWith(MOCK_FAKE_ADDRESS);
     expect(verifiedPayload).toEqual({
@@ -81,15 +84,18 @@ describe("Attempt verification", function () {
   it("should return error response when balanceOf call throws an error", async () => {
     mockBalanceOf.mockRejectedValueOnce(new Error("some error"));
     const gtcPossessions = new EthErc20PossessionProvider({
-      threshold: 100,
+      threshold: "100",
       recordAttribute: "gtcPossessionsGte",
       contractAddress: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f",
       error: "GTC Possessions >= 100 Provider verify Error",
     });
 
-    const verifiedPayload = await gtcPossessions.verify({
-      address: MOCK_ADDRESS_LOWER,
-    } as unknown as RequestPayload);
+    const verifiedPayload = await gtcPossessions.verify(
+      {
+        address: MOCK_ADDRESS_LOWER,
+      } as unknown as RequestPayload,
+      {}
+    );
 
     expect(mockBalanceOf).toBeCalledWith(MOCK_ADDRESS_LOWER);
     expect(verifiedPayload).toEqual({
@@ -103,22 +109,22 @@ describe("Check valid cases for GTC Balances", function () {
   beforeEach(() => {
     jest.clearAllMocks();
     mockBalanceOf.mockResolvedValue(MOCK_BALANCE);
-    (formatUnits as jest.Mock).mockImplementation((num: string, power: number) => {
-      return parseFloat(num) / Math.pow(10, power);
-    });
   });
 
   it("Expected Greater than 10 GTC and GTC Balance is 15", async () => {
     mockBalanceOf.mockResolvedValueOnce("15000000000000000000");
     const gtcPossessions = new EthErc20PossessionProvider({
-      threshold: 10,
+      threshold: "10",
       recordAttribute: "gtcPossessionsGte",
       contractAddress: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f",
     });
 
-    const verifiedPayload = await gtcPossessions.verify({
-      address: MOCK_ADDRESS_LOWER,
-    } as unknown as RequestPayload);
+    const verifiedPayload = await gtcPossessions.verify(
+      {
+        address: MOCK_ADDRESS_LOWER,
+      } as unknown as RequestPayload,
+      {}
+    );
 
     expect(verifiedPayload).toEqual({
       valid: true,
@@ -131,14 +137,17 @@ describe("Check valid cases for GTC Balances", function () {
   it("Expected Greater than 100 GTC and GTC Balance is 150", async () => {
     mockBalanceOf.mockResolvedValueOnce("150000000000000000000");
     const gtcPossessions = new EthErc20PossessionProvider({
-      threshold: 100,
+      threshold: "100",
       recordAttribute: "gtcPossessionsGte",
       contractAddress: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f",
     });
 
-    const verifiedPayload = await gtcPossessions.verify({
-      address: MOCK_ADDRESS_LOWER,
-    } as RequestPayload);
+    const verifiedPayload = await gtcPossessions.verify(
+      {
+        address: MOCK_ADDRESS_LOWER,
+      } as RequestPayload,
+      {}
+    );
 
     expect(verifiedPayload).toEqual({
       valid: true,
@@ -154,21 +163,21 @@ describe("Check invalid cases for GTC Balances", function () {
   beforeEach(() => {
     jest.clearAllMocks();
     mockBalanceOf.mockResolvedValue(MOCK_BALANCE);
-    (formatUnits as jest.Mock).mockImplementation((num: string, power: number) => {
-      return parseFloat(num) / Math.pow(10, power);
-    });
   });
   it("Expected Greater than 10 GTC and GTC Balance is 7", async () => {
     mockBalanceOf.mockResolvedValueOnce("7000000000000000000");
     const gtcPossessions = new EthErc20PossessionProvider({
-      threshold: 10,
+      threshold: "10",
       recordAttribute: "gtcPossessionsGte",
       contractAddress: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f",
     });
 
-    const verifiedPayload = await gtcPossessions.verify({
-      address: MOCK_ADDRESS,
-    } as RequestPayload);
+    const verifiedPayload = await gtcPossessions.verify(
+      {
+        address: MOCK_ADDRESS,
+      } as RequestPayload,
+      {}
+    );
 
     expect(verifiedPayload).toEqual({
       valid: false,
@@ -178,14 +187,17 @@ describe("Check invalid cases for GTC Balances", function () {
   it("Expected Greater than 100 GTC and GTC Balance is 75", async () => {
     mockBalanceOf.mockResolvedValueOnce("75000000000000000000");
     const gtcPossessions = new EthErc20PossessionProvider({
-      threshold: 100,
+      threshold: "100",
       recordAttribute: "gtcPossessionsGte",
       contractAddress: "0xde30da39c46104798bb5aa3fe8b9e0e1f348163f",
     });
 
-    const verifiedPayload = await gtcPossessions.verify({
-      address: MOCK_ADDRESS_LOWER,
-    } as RequestPayload);
+    const verifiedPayload = await gtcPossessions.verify(
+      {
+        address: MOCK_ADDRESS_LOWER,
+      } as RequestPayload,
+      {}
+    );
 
     expect(verifiedPayload).toEqual({
       valid: false,
