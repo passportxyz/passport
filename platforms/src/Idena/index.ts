ab50ff34be82c892c9174b0f33ba9ea18a5fa29b
Lucian <lucian@gitcoin.co>
2023-06-21 14:44:01 -0700

    feat(platforms): adding Idena stamps (#1388)
    * feat(iam, app, platforms): adds idena stamps
    
    * refactor(platforms): switched idena over to new cache and config
    
    * Add tests (#1356)
    
    * feat(platforms): added an Idena README
    
    
    ---------
    
    Co-authored-by: mbidenaio <mb@idena.io>

---
+++ platforms/src/Idena/index.ts
@@ -0,0 +1,11 @@
+// Idena Platform
+export { IdenaPlatform } from "./App-Bindings";
+export { IdenaAge5Provider, IdenaAge10Provider } from "./Providers/IdenaAgeProvider";
+export { IdenaStake1kProvider, IdenaStake10kProvider, IdenaStake100kProvider } from "./Providers/IdenaStakeProvider";
+export {
+  IdenaStateNewbieProvider,
+  IdenaStateVerifiedProvider,
+  IdenaStateHumanProvider,
+} from "./Providers/IdenaStateProvider";
+
+export { PlatformDetails, ProviderConfig, providers } from "./Providers-config";
