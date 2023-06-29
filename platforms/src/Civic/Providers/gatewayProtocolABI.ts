6d77e0654fcab2f55d16ef3015d54ac8d9120b05
dankelleher <dankelleher@yahoo.com>
2023-06-29 16:22:32 +0200

    feat: civic pass stamp changes
    - enforcing one stamp per pass
    (this was previously configuration)
    - removing the pass and chain information from the record,
    to ensure that a stamp for a given pass always hashes to the same value
    - add stamp expiry based on the maximum pass expiry of a
    given pass on all chains

--- platforms/src/Civic/Providers/gatewayProtocolABI.ts
+++ platforms/src/Civic/Providers/gatewayProtocolABI.ts
@@ -23,4 +23,72 @@ export const GATEWAY_PROTOCOL_ABI = [
     stateMutability: "view",
     type: "function",
   },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "network",
+        type: "uint256",
+      },
+      {
+        internalType: "bool",
+        name: "onlyActive",
+        type: "bool",
+      },
+    ],
+    name: "getTokenIdsByOwnerAndNetwork",
+    outputs: [
+      {
+        internalType: "uint256[]",
+        name: "",
+        type: "uint256[]",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256",
+      },
+    ],
+    name: "getToken",
+    outputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "uint8",
+        name: "state",
+        type: "uint8",
+      },
+      {
+        internalType: "string",
+        name: "identity",
+        type: "string",
+      },
+      {
+        internalType: "uint256",
+        name: "expiration",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "bitmask",
+        type: "uint256",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
 ];
