1148c679ba86299a2b48c36a5cf824ccc355c311
Lucian Hymer <lucian.hymer@gmail.com>
2023-08-09 09:37:35 -0700

    feat(platforms): added logging for unhandled errors

--- platforms/src/utils/providers.ts
+++ platforms/src/utils/providers.ts
@@ -4,18 +4,18 @@ import type { RequestPayload, VerifiedPayload, ProviderContext } from "@gitcoin/
 
 const UPDATED_PROVIDERS = ["twitterAccountAgeGte#180", "twitterAccountAgeGte#365", "twitterAccountAgeGte#730"];
 
-function inDevEnvironment() {
-  // TODO
-  return true;
-}
-
-function reportUnhandledError(e: unknown) {
-  if (inDevEnvironment()) {
-    console.error("Unhandled error", e);
-    throw e;
+function reportUnhandledError(type: string, address: string, e: unknown) {
+  if (process.env.EXIT_ON_UNHANDLED_ERROR === "true") {
+    // To be used when running locally to ensure that unhandled errors are fixed
+    console.error(`Unhandled error for type ${type}`, e);
+    process.exit(1);
   } else {
-    // TODO log to datadog and set up alert
-    console.error("Unhandled error", e);
+    let errorMessage = "unable to parse, not derived from Error";
+    if (e instanceof Error) {
+      // Don't log the message (or first line of stack) as it may contain PII
+      errorMessage = `${e.name} ${e.stack.replace(/^.*\n *(?=at)/m, "")}`;
+    }
+    console.error(`UNHANDLED ERROR: for type ${type} and address ${address} -`, errorMessage);
   }
 }
 
@@ -47,8 +47,20 @@ export class Providers {
 
     // if a provider is available - use it to verify the payload
     if (provider) {
-      // return the verification response
-      return await provider.verify(payload, context);
+      try {
+        // return the verification response
+        return await provider.verify(payload, context);
+      } catch (e) {
+        if (e instanceof Error) {
+          const message = `Unable to verify provider: ${e.stack.replace(/\n\s*(?= )/, "").replace(/\n.*$/gm, "")}`;
+
+          return {
+            valid: false,
+            error: [message],
+          };
+        }
+        throw e;
+      }
     }
 
     // unable to verify without provider
@@ -65,20 +77,24 @@ export class Providers {
 
     if (provider) {
       try {
-        return await provider.verify(payload, context);
+        const result = await provider.verify(payload, context);
+        if (!result.valid && (!result.errors || result.errors.length === 0)) {
+          reportUnhandledError(type, payload.address, new Error("No reason provided for invalid payload"));
+        }
+        return result;
       } catch (e) {
         if (e instanceof ProviderExternalVerificationError || e instanceof ProviderInternalVerificationError) {
           return {
             valid: false,
+            // TODO change to "errors" once everything is updated
             error: [e.message],
           };
         } else {
-          reportUnhandledError(e);
+          reportUnhandledError(type, payload.address, e);
 
-          let message =
-            "There was an unexpected error during verification, this has been reported to the Gitcoin team.";
+          let message = "There was an unexpected error during verification.";
 
-          if (e instanceof Error) message += ` ${e.name}`;
+          if (e instanceof Error) message += ` ${e.stack.replace(/\n\s*(?= )/, "").replace(/\n.*$/gm, "")}`;
 
           return {
             valid: false,
