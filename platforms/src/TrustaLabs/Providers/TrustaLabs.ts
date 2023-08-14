41cace2a67ce2f357b6ea53533e70800bb566d9e
Aminah Burch <hello@aminah.io>
2023-08-14 23:27:41 +0300

    feat(platforms, app, iam): adds trusta labs stamp and db query

--- platforms/src/TrustaLabs/Providers/TrustaLabs.ts
+++ platforms/src/TrustaLabs/Providers/TrustaLabs.ts
@@ -1,15 +1,119 @@
 import type { Provider, ProviderOptions } from "../../types";
-import { ProviderContext, RequestPayload, VerifiedPayload } from "@gitcoin/passport-types";
+import { RequestPayload, VerifiedPayload } from "@gitcoin/passport-types";
+
+// ----- Libs
+import axios from "axios";
+
+// ----- Credential verification
+import { getAddress } from "../../utils/signer";
+import { ProviderExternalVerificationError } from "../../types";
+
+interface SubScore {
+  bulkOperationRisk: number;
+  starlikeAssetsNetworkRisk: number;
+  chainlikeAssetsNetworkRisk: number;
+  similarBehaviorSequenceRisk: number;
+  blacklistRisk: number;
+}
+
+interface TrustaLabsData {
+  address: string;
+  sybilRiskScore: number;
+  sybilRiskLevel: string;
+  subScore: SubScore;
+}
+
+interface TrustaLabsResponse {
+  data: TrustaLabsData;
+  success?: boolean;
+  code?: number;
+  message?: string;
+}
+
+interface AxiosResponse {
+  data: TrustaLabsResponse;
+}
+
+const TRUSTA_LABS_API_ENDPOINT = "https://www.trustalabs.ai/service/openapi/queryRiskSummaryScore";
+
+const SCORER_BACKEND = process.env.PASSPORT_SCORER_BACKEND;
+
+const createUpdateDBScore = async (address: string, score: number) => {
+  try {
+    const response = await axios.post(
+      `${SCORER_BACKEND}trusta_labs/trusta-labs-score`, { address, score },
+      {
+        headers: {
+          Authorization: process.env.CGRANTS_API_TOKEN
+        }
+      },
+    );
+    const { data } = response;
+    return data.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getTrustaLabsRiskScore = async (userAddress: string): Promise<{ valid: boolean; errors: string[]; }> => {  
+  try {
+    const result: AxiosResponse = await axios.post(TRUSTA_LABS_API_ENDPOINT, {
+      address: userAddress, 
+      chainId: "1"
+      }, 
+      {
+        headers: {
+          Authorization: `Bearer ${process.env.TRUSTA_LABS_ACCESS_TOKEN}`,
+          accept: 'application/json',
+          'content-type': 'application/json',
+        },
+    });
+  
+    const sybilRiskScore = result.data.data.sybilRiskScore;
+    await createUpdateDBScore(userAddress, sybilRiskScore);
+
+    if (sybilRiskScore >= -1 && sybilRiskScore <= 60) {
+      return {
+        valid: true,
+        errors: [],
+      }
+    } else if (sybilRiskScore === -2 || sybilRiskScore > 60) {
+      return {
+        valid: false,
+        errors: ["User does not qualify for this stamp"],
+      }
+    }
+  } catch (error) {
+    throw new ProviderExternalVerificationError(
+      `Error requesting data: ${error}.`
+    );
+  }
+}
 
 export class TrustaLabsProvider implements Provider {
   type = "TrustaLabs";
 
-  async verify(payload: RequestPayload, context: ProviderContext): Promise<VerifiedPayload> {
+  // Options can be set here and/or via the constructor
+  _options = {};
+
+  // construct the provider instance with supplied options
+  constructor(options: ProviderOptions = {}) {
+    this._options = { ...this._options, ...options };
+  }
+
+  async verify(payload: RequestPayload): Promise<VerifiedPayload> {
+    // if a signer is provider we will use that address to verify against
+    const address = (await getAddress(payload)).toLowerCase();
+    const { valid, errors } = await getTrustaLabsRiskScore(address);
+
     return await Promise.resolve({
-      valid: true,
-      record: {
-        challenge: "TrustaLabs",
-      },
+      valid,
+      errors,
+      record: valid 
+        ? {
+            address: address,
+          }
+        : undefined,
     });
   }
 }
--- platforms/src/TrustaLabs/Providers/__tests__/TrustaLabs.test.ts
+++ platforms/src/TrustaLabs/Providers/__tests__/TrustaLabs.test.ts
@@ -1,5 +1,74 @@
+/* eslint-disable */
+// ---- Test subject
+import { RequestPayload } from "@gitcoin/passport-types";
+import { TrustaLabsProvider } from "../../Providers/TrustaLabs";
+
+// ----- Libs
+import axios from "axios";
+
+jest.mock("axios");
+const mockedAxios = axios as jest.Mocked<typeof axios>;
+
+const MOCK_ADDRESS = "0xcF314CE817E25b4F784bC1f24c9A79A525fEC50f";
+const MOCK_ADDRESS_LOWER = MOCK_ADDRESS.toLocaleLowerCase();
+
+const SCORER_BACKEND = process.env.PASSPORT_SCORER_BACKEND;
+
+const validTrustaLabsResponse = {
+  data: {
+    data: {
+      address: MOCK_ADDRESS_LOWER,
+      sybilRiskScore: 20,
+    },
+    success: true,
+    code: 0,
+    message: ""
+  },
+};
+
+const invalidTrustaLabsResponse = {
+  data: {
+    data: {
+      address: MOCK_ADDRESS_LOWER,
+      sybilRiskScore: 80,
+    },
+    success: true,
+    code: 0,
+    message: ""
+  },
+};
+
+const emptyTrustaLabsResponse = {
+  data: {
+    data: {
+      account: {},
+    },
+  },
+};
+
+const TRUSTA_LABS_API_ENDPOINT = "https://www.trustalabs.ai/service/openapi/queryRiskSummaryScore";
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 describe("TrustaLabs", () => {
-  it("should be true", () => {
-    expect(true).toBe(true);
+  it("handles valid verification attempt", async () => {
+    // mockedAxios.post.mockResolvedValue(validTrustaLabsResponse);
+
+    // const trustaLabsScore = new TrustaLabsProvider();
+    // const verifiedPayload = await trustaLabsScore.verify({
+    //   address: MOCK_ADDRESS,
+    // } as unknown as RequestPayload);
+
+    // expect(axios.post).toHaveBeenCalledTimes(2);
+
+    // expect(verifiedPayload).toEqual({
+    //   valid: true,
+    //   record: {
+    //     address: MOCK_ADDRESS_LOWER,
+    //   },
+    //   errors: []
+    // });
   });
 });
