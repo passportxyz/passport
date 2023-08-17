43340f629ffd50d328882caa891a3c20f06b12fa
Lucian Hymer <lucian.hymer@gmail.com>
2023-08-17 15:20:48 -0700

    lint fix

--- platforms/src/TrustaLabs/App-Bindings.tsx
+++ platforms/src/TrustaLabs/App-Bindings.tsx
@@ -3,7 +3,7 @@ import { Platform } from "../utils/platform";
 import React from "react";
 
 const Link = ({ href, children }: { href: string; children: React.ReactNode }) => (
-  <a href={href} target="_blank" className="text-color-1 cursor-pointer underline">
+  <a href={href} target="_blank" className="text-color-1 cursor-pointer underline" rel="noreferrer">
     {children}
   </a>
 );
