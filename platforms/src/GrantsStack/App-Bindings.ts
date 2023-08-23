73362df0707c3fa6821280b2303469706191e75d
nutrina <nutrina9@gmail.com>
2023-08-23 12:18:24 +0300

    feat(app): update Gitcoin & GrantsStack stamps notes (#1631)

--- platforms/src/GrantsStack/App-Bindings.ts
+++ platforms/src/GrantsStack/App-Bindings.ts
@@ -9,7 +9,7 @@ export class GrantsStackPlatform extends Platform {
 
   banner = {
     heading:
-      "Note: Only Alpha and Beta rounds run by Gitcoin are included. For the Alpha program, only donations larger than $1 are counted. For the Beta program, only matching-eligible contributions are counted.",
+      "Note: Only Alpha and Beta rounds run by Gitcoin are included. For the Alpha program, only donations larger than $1 are counted. For the Beta program, only matching-eligible contributions are counted. Donations during Grants Round 18 (August '23) will be added by the end of September.",
   };
 
   async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
