18a50c09b3fd8e54cfa7fee7c4488fb0ddd068a7
Aminah Burch <hello@aminah.io>
2023-08-25 19:14:58 +0300

    feat(iam): removes types and other helpers to utils file

--- iam/src/scripts/lensWeb3Script.ts
+++ iam/src/scripts/lensWeb3Script.ts
@@ -1,324 +1,65 @@
 /* eslint-disable */
-import dotenv from "dotenv";
-dotenv.config({ path: "../../.env"});
-import { verifyTypes }  from "../index";
-import { RequestPayload } from "@gitcoin/passport-types";
+import * as dotenv from "dotenv";
+import { writeFile as _writeFile } from "fs";
+import * as fs from "fs";
+import { promisify } from "util";
 import { stakingSubgraph } from "@gitcoin/passport-platforms/src/GtcStaking/Providers/GtcStaking";
-import { BigNumber } from "ethers";
+import { Stamp } from "@gitcoin/passport-types";
+import { PROVIDER_ID, RequestPayload } from "@gitcoin/passport-types";
 import axios from "axios";
+import csv from "csv-parser";
+import { BigNumber } from "ethers";
+import {
+  UserStakes,
+  Output,
+  GITCOIN_PASSPORT_WEIGHTS,
+  web3Payloads,
+  VerificationResults,
+  hexToDecimal,
+} from "./utils";
 
-// are idena and civic EVM platforms--they don't have isEVM = true
+dotenv.config({ path: "../../.env" });
+import { verifyTypes } from "../index";
+import { AxiosError } from "axios";
 
-// process.env.IAM_JWK;
-// process.env.ATTESTATION_SIGNER_PRIVATE_KEY;
-// process.env.ALLO_SCORER_ID;
-// process.env.SCORER_ENDPOINT;
-// process.env.SCORER_API_KEY;
-// process.env.EAS_GITCOIN_STAMP_SCHEMA;
-
-const SCORER_ID = process.env.ALLO_SCORER_ID || "";
-const API_KEY = process.env.SCORER_API_KEY || "";
-
-type UserStakes = {
-  selfStake: BigNumber;
-  communityStake: BigNumber;
-};
-
-const GITCOIN_PASSPORT_WEIGHTS = {
-  "Brightid": "0.689",
-  "CivicCaptchaPass": "1",
-  "CivicLivenessPass": "2.25",
-  "CivicUniquenessPass": "2.25",
-  "CommunityStakingBronze": "1.27",
-  "CommunityStakingGold": "1.27",
-  "CommunityStakingSilver": "1.27",
-  "Ens": "2.2",
-  "EthGasProvider": "2.4",
-  "EthGTEOneTxnProvider": "1.27",
-  "ethPossessionsGte#1": "1.79",
-  "ethPossessionsGte#10": "1.27",
-  "ethPossessionsGte#32": "1.27",
-  "FirstEthTxnProvider": "1.16",
-  "GitcoinContributorStatistics#numGr14ContributionsGte#1": "1.41",
-  "GitcoinContributorStatistics#numGrantsContributeToGte#1": "1.57",
-  "GitcoinContributorStatistics#numGrantsContributeToGte#10": "2.30",
-  "GitcoinContributorStatistics#numGrantsContributeToGte#100": "0.52",
-  "GitcoinContributorStatistics#numGrantsContributeToGte#25": "1.48",
-  "GitcoinContributorStatistics#numRoundsContributedToGte#1": "1.57",
-  "GitcoinContributorStatistics#totalContributionAmountGte#10": "1.53",
-  "GitcoinContributorStatistics#totalContributionAmountGte#100": "1.37",
-  "GitcoinContributorStatistics#totalContributionAmountGte#1000": "1.18",
-  "GnosisSafe": "2.65",
-  "GuildAdmin": "0.689",
-  "GuildMember": "0.689",
-  "GuildPassportMember": "0.689",
-  "HolonymGovIdProvider": "4",
-  "Hypercerts": "0.689",
-  "IdenaAge#10": "1.48",
-  "IdenaAge#5": "1.48",
-  "IdenaStake#100k": "1.41",
-  "IdenaStake#10k": "1.16",
-  "IdenaStake#1k": "0.9",
-  "IdenaState#Human": "1.61",
-  "IdenaState#Newbie": "0.51",
-  "IdenaState#Verified": "1.35",
-  "Lens": "2.45",
-  "NFT": "0.69",
-  "PHIActivityGold": "1.16",
-  "PHIActivitySilver": "1.67",
-  "Poh": "1.21",
-  "SelfStakingBronze": "1.21",
-  "SelfStakingGold": "1.21",
-  "SelfStakingSilver": "1.21",
-  "SnapshotProposalsProvider": "2.82",
-  "SnapshotVotesProvider": "1.41",
-  "ZkSync": "0.400",
-  "ZkSyncEra": "0.400",
-  "CyberProfilePremium": "1.21",
-  "CyberProfilePaid": "1.21",
-  "CyberProfileOrgMember": "1.21",
-  "GrantsStack3Projects": "1.07",
-  "GrantsStack5Projects": "1.07",
-  "GrantsStack7Projects": "1.07",
-  "GrantsStack2Programs": "1.07",
-  "GrantsStack4Programs": "1.07",
-  "GrantsStack6Programs": "1.07",
-  "TrustaLabs": "1.54",
+function logSize(direction: string, data: any) {
+  if (data) {
+    const sizeInBytes = Buffer.from(JSON.stringify(data)).length;
+    const sizeInKilobytes = (sizeInBytes / 1024).toFixed(2);
+    console.log(`Size of ${direction} payload: ${sizeInKilobytes} KB`);
+  }
 }
 
-const TYPE = [
-  "Ens",
-  "Poh",
-  "Brightid",
-  "Gitcoin",
-  "Signer",
-  "Snapshot",
-  "ETH",
-  "GtcStaking",
-  "NFT",
-  "ZkSync",
-  "Lens",
-  "GnosisSafe",
-  "GuildXYZ",
-  "Hypercerts",
-  "PHI",
-  "Holonym",
-  "Civic",
-  "CyberConnect",
-  "GrantsStack",
-  "TrustaLabs"
-]
+axios.interceptors.request.use(config => {
+  logSize("request", config.data);
+  return config;
+}, error => {
+  return Promise.reject(error);
+});
 
-const PROVIDER_ID = [
-  "Signer",
-  "Ens",
-  "Poh",
-  "Brightid",
-  "GitcoinContributorStatistics#numGrantsContributeToGte#1",
-  "GitcoinContributorStatistics#numGrantsContributeToGte#10",
-  "GitcoinContributorStatistics#numGrantsContributeToGte#25",
-  "GitcoinContributorStatistics#numGrantsContributeToGte#100",
-  "GitcoinContributorStatistics#totalContributionAmountGte#10",
-  "GitcoinContributorStatistics#totalContributionAmountGte#100",
-  "GitcoinContributorStatistics#totalContributionAmountGte#1000",
-  "GitcoinContributorStatistics#numRoundsContributedToGte#1",
-  "GitcoinContributorStatistics#numGr14ContributionsGte#1",
-  "Snapshot",
-  "SnapshotProposalsProvider",
-  "SnapshotVotesProvider",
-  "ethPossessionsGte#1",
-  "ethPossessionsGte#10",
-  "ethPossessionsGte#32",
-  "FirstEthTxnProvider",
-  "EthGTEOneTxnProvider",
-  "EthGasProvider",
-  "SelfStakingBronze",
-  "SelfStakingSilver",
-  "SelfStakingGold",
-  "CommunityStakingBronze",
-  "CommunityStakingSilver",
-  "CommunityStakingGold",
-  "NFT",
-  "ZkSync",
-  "ZkSyncEra",
-  "Lens",
-  "GnosisSafe",
-  "GuildMember",
-  "GuildAdmin",
-  "GuildPassportMember",
-  "Hypercerts",
-  "CyberProfilePremium",
-  "CyberProfilePaid",
-  "CyberProfileOrgMember",
-  "PHIActivitySilver",
-  "PHIActivityGold",
-  "HolonymGovIdProvider",
-  "CivicCaptchaPass",
-  "CivicUniquenessPass",
-  "CivicLivenessPass",
-  "GrantsStack3Projects",
-  "GrantsStack5Projects",
-  "GrantsStack7Projects",
-  "GrantsStack2Programs",
-  "GrantsStack4Programs",
-  "GrantsStack6Programs",
-  "TrustaLabs"
-]
+axios.interceptors.response.use(response => {
+  logSize("response", response.data);
+  return response;
+}, error => {
+  return Promise.reject(error);
+});
 
-const web3Payloads = [
-  {
-    type: "Ens",
-    types: ["Ens"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "Poh",
-    types: ["Poh"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "Brightid",
-    types: ["Brightid"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "Gitcoin",
-    types: ["GitcoinContributorStatistics#numGrantsContributeToGte#1",
-      "GitcoinContributorStatistics#numGrantsContributeToGte#10",
-      "GitcoinContributorStatistics#numGrantsContributeToGte#25",
-      "GitcoinContributorStatistics#numGrantsContributeToGte#100",
-      "GitcoinContributorStatistics#totalContributionAmountGte#10",
-      "GitcoinContributorStatistics#totalContributionAmountGte#100",
-      "GitcoinContributorStatistics#totalContributionAmountGte#1000",
-      "GitcoinContributorStatistics#numRoundsContributedToGte#1",
-      "GitcoinContributorStatistics#numGr14ContributionsGte#1"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "Snapshot",
-    types: ["SnapshotProposalsProvider",
-      "SnapshotVotesProvider"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "ETH",
-    types: ["ethPossessionsGte#1",
-      "ethPossessionsGte#10",
-      "ethPossessionsGte#32",
-      "FirstEthTxnProvider",
-      "EthGTEOneTxnProvider",
-      "EthGasProvider"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "GtcStaking",
-    types: ["SelfStakingBronze",
-      "SelfStakingSilver",
-      "SelfStakingGold",
-      "CommunityStakingBronze",
-      "CommunityStakingSilver",
-      "CommunityStakingGold"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "NFT",
-    types: ["NFT"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "ZkSync",
-    types: ["ZkSync",
-      "ZkSyncEra"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "Lens",
-    types: ["Lens"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "GnosisSafe",
-    types: ["GnosisSafe"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "GuildXYZ",
-    types: ["GuildMember",
-      "GuildAdmin",
-      "GuildPassportMember"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "Hypercerts",
-    types: ["Hypercerts"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "PHI",
-    types: ["PHIActivitySilver",
-      "PHIActivityGold"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "Holonym",
-    types: ["HolonymGovIdProvider",],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "CyberConnect",
-    types: ["CyberProfilePremium",
-      "CyberProfilePaid",
-      "CyberProfileOrgMember"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "GrantsStack",
-    types: ["GrantsStack3Projects",
-      "GrantsStack5Projects",
-      "GrantsStack7Projects",
-      "GrantsStack2Programs",
-      "GrantsStack4Programs",
-      "GrantsStack6Programs"],
-    address: "",
-    version: "0.0.0",
-  },
-  {
-    type: "TrustaLabs",
-    types: ["TrustaLabs"],
-    address: "",
-    version: "0.0.0",
-  },
-]
-// First check if the address is already in scorer --> isAddressInRegistry()
-// -- if yes
-// ---- return ScorerType
-// REGARDLESS of whether or not an address is in the scorer db --->
-// ---- check GTC staked --> getStake()
-// ---- check if address qualifies for Web3 stamp data points --> check()
-// ------ if qualifies
-// -------- score web3 stamps
-// ------ else
-// -------- give score of 0
+const SCORER_ID = process.env.ALLO_SCORER_ID || "";
+
+const API_KEY = process.env.SCORER_API_KEY || "";
 
 const GET_PASSPORT_ENDPOINT = process.env.SCORER_ENDPOINT + "/registry/stamps/";
 
 const SCORE_PASSPORT_ENDPOINT = process.env.SCORER_ENDPOINT + "/registry/submit-passport";
+
+let CURRENT_ADDRESS_HAS_PASSPORT = false;
+
+export const headerOptions = {
+  headers: {
+    "X-API-Key": API_KEY,
+    "Content-Type": "application/json",
+  }
+}
 
 function getStakeQuery(address: string): string {
   return `
@@ -335,141 +76,196 @@ function getStakeQuery(address: string): string {
   `;
 }
 
-const check = async (payload: RequestPayload): Promise<void> => {
+const check = async (payload: RequestPayload): Promise<VerificationResults[]> => {
   let responses;
   // types filters out the types into an individual type
   const types = (payload.types?.length ? payload.types : [payload.type]).filter((type) => type);
-  // console.log(types);
 
   // verifyTypes returns whether the type is valid
   await verifyTypes(types, payload)
     .then((results) => {
-      responses = results.map(({ verifyResult, type, error, code }) => ({
-        valid: verifyResult.valid,
-        type,
-        error,
-        code,
-      }));
+      responses = results.filter((result) => {
+        if (result.verifyResult.valid === true) {
+          return { valid: result.verifyResult.valid, type: result.type }
+        }
+      });
     })
     .catch(() => "something went wrong");
-    console.log(responses);
+  return responses;
 };
 
-interface Output {
-  ScoreType: string;
-  Score: number;
-  GTC_Staked: number;
-  AdditionalCriteria?: any;
-}
-
-async function isAddressInRegistry(address: string): Promise<boolean> {
-  const response = await axios.get(`${GET_PASSPORT_ENDPOINT}${address}?limit=10`)
-  const addressInRegistry = response.data.items.length > 0 ? true : false;
-  return addressInRegistry;
-}
-
-async function checkGTCStaked(address: string): Promise<UserStakes> {
-  const addressLower = address.toLowerCase();
-  const response = await axios.post(stakingSubgraph, {
-    query: getStakeQuery(addressLower),
-  });
-
-  const selfStake = BigNumber.from(response?.data?.data?.users[0]?.stakes[0]?.stake || "0");
-  const communityStake = BigNumber.from(response?.data?.data?.users[0]?.xstakeAggregates[0]?.total || "0");
-  const userStakes = { selfStake, communityStake }
-  return userStakes;
-}
-
-async function qualifiesForWeb3Stamp(address: string): Promise<boolean> {
-  // TODO: Check if address qualifies for web3 stamp data points.
+const handleCheckForWeb3Stamps = async (address: string): Promise<Promise<VerificationResults[]>[]> => {
   try {
-    const checkedArray =  web3Payloads.map(payload => {
-      payload.address = address;
-      const checked = check(payload)
-      // console.log(checked);
+    return web3Payloads.map(async (payload) => {
+    const updatedPayload = {...payload, address: address};
+      return await check(updatedPayload);
     });
-    
-    return false;
   } catch (e) {
     console.error(e)
   }
 }
 
+async function doesAddressHavePassport(address: string): Promise<boolean> {
+  const response = await axios.get(`${GET_PASSPORT_ENDPOINT}${address}?limit=5`, headerOptions);
+  const addressInRegistry = response.data.items.length > 0 ? true : false;
+  
+  return addressInRegistry;
+}
+
+async function getStamps(address: string): Promise<Stamp[]> {
+  try {
+    const response = await axios.get(`${GET_PASSPORT_ENDPOINT}${address}?limit=1000`, headerOptions);
+    const data: Stamp[] = response.data.items;
+    return data;
+  } catch (e) {
+    throw new Error(e.message) as AxiosError;
+  }
+}
+
+async function checkGTCStaked(address: string): Promise<UserStakes> {
+  try {
+    const addressLower = address.toLowerCase();
+    const response = await axios.post(stakingSubgraph, {
+      query: getStakeQuery(addressLower),
+    });
+  
+    const selfStake = BigNumber.from(response?.data?.data?.users[0]?.stakes[0]?.stake ?? "0");
+    const communityStake = BigNumber.from(response?.data?.data?.users[0]?.xstakeAggregates[0]?.total ?? "0");
+    return { selfStake, communityStake };
+  } catch (e) {
+    throw new Error(e.message) as AxiosError;
+  }
+}
+
 async function scorePassport(address: any): Promise<number> {
-  const options = {
-    data: {
-      address: address,
-      scorer_id: SCORER_ID,
-    },
-    headers: {
-      "X-API-Key": API_KEY,
-      "Content-Type": "application/json",
+  const data = {
+    address: address,
+    scorer_id: SCORER_ID,
+    signature: "",
+    nonce: "",
+  };
+
+  try {
+    const response = await axios.post(SCORE_PASSPORT_ENDPOINT, data, headerOptions);
+
+    return response.data.score;
+  } catch (e) {
+    throw new Error(e.message) as AxiosError;
+  }
+}
+
+async function scoreWeb3Stamps(address: string, data: Promise<VerificationResults[]>[] | []): Promise<number> {
+  // do a check if the web3 stamps are already in their passport. If not, score them
+  let acc = 0;
+  
+  function totalScore(acc: number, item: { name: string; score: string; }) {
+    return acc + parseInt(item.score);
+  }
+
+  if (CURRENT_ADDRESS_HAS_PASSPORT) {
+    const passportWeb3Providers = [];
+    const stamps = await getStamps(address);
+    for (let stamp of stamps) {
+      const { provider } = stamp;
+      for (let web3Provider of GITCOIN_PASSPORT_WEIGHTS) {
+        if (provider !== web3Provider.name) {
+          passportWeb3Providers.push(provider);
+        }
+      }
+    }
+    for (let item of GITCOIN_PASSPORT_WEIGHTS) {
+      passportWeb3Providers.map((provider: PROVIDER_ID) => {
+        if (provider === item.name) {
+          acc = totalScore(acc, item);
+        }
+      });
+    }
+  } else {
+    for (let item of GITCOIN_PASSPORT_WEIGHTS) {
+      data.map((el: any) => {
+        if (el === undefined) {
+          acc = 0;
+        } else if (el.type === item.name && el.valid === true) {
+          acc = totalScore(acc, item);
+        }
+      });
     }
   }
+  
+  return acc;
+};
+
+async function main(address: string): Promise<Output> {
   try {
-    // if isAddressInRegistry() === true, score with endpoint
-    const response = axios.post(SCORE_PASSPORT_ENDPOINT, options)
-    return 42;
+    let output: Output;
+    CURRENT_ADDRESS_HAS_PASSPORT = await doesAddressHavePassport(address);
+    const { selfStake, communityStake} = await checkGTCStaked(address);
+    const gtcStaked = hexToDecimal(selfStake) + hexToDecimal(communityStake);
+    const addressLower = address.toLowerCase();
+
+    if (CURRENT_ADDRESS_HAS_PASSPORT) {
+      const score = await scorePassport(addressLower);
+      const web3Score = await scoreWeb3Stamps(addressLower, []);
+
+      output = {
+        ScoreType: "Passport",
+        Score: score + web3Score,
+        GTCStaked: gtcStaked,
+        AdditionalCriteria: {}
+      };
+    } else if (CURRENT_ADDRESS_HAS_PASSPORT === false) {
+      const results = await handleCheckForWeb3Stamps(addressLower);
+      const score = scoreWeb3Stamps(addressLower, results);
+
+      output = {
+        ScoreType: "Web3",
+        Score: score,
+        GTCStaked: gtcStaked,
+        AdditionalCriteria: {}
+      };
+    }
+    // console.log("output --->", output);
+    
+    return output;
   } catch (e) {
-    console.error(e);
+    throw new Error(e.message) as AxiosError;
   }
 }
 
-async function scoreWeb3Stamps(address: string): Promise<number> {
-  const scores = [];
-  // Score based on web3 stamps for the given address using calculation
-  // and web3 stamp weights
-  // if {type: check.data.valid === true}
-  // --> add score to scores array
-  // reduce the scores array and return the number
-  return 42;
+const csvFilePath = "./LensData.csv";
+
+const startTime = new Date().getTime();
+
+function parseCSV(): Promise<any[]> {
+    return new Promise((resolve, reject) => {
+        const results: any[] = [];
+        fs.createReadStream(csvFilePath)
+            .pipe(csv())
+            .on("data", (data) => results.push(data))
+            .on("end", () => {
+                resolve(results);
+            })
+            .on("error", (error) => reject(error));
+    });
 }
 
-// Promise<Output>
-/**
- * Output:
- * ScoreType: Distinguishing between Passport and web3.
- * Score: The actual numerical score.
- * GTC Staked: Amount of GTC staked by the user.
- * Additional Criteria: Any other pertinent data we deem necessary.
- */
-async function main(address: string): Promise<void> {
-  let output: Output;
-
-  if (await isAddressInRegistry(address)) {
-    const score = await scorePassport(address);
-    const gtcStaked = await checkGTCStaked(address);
-
-    // output = {
-    //     ScoreType: 'Passport',
-    //     Score: score,
-    //     GTC_Staked: gtcStaked,
-    //     AdditionalCriteria: {}
-    // };
-
-    // } else {
-    //     if (await qualifiesForWeb3Stamp(address)) {
-    //         const score = await scoreWeb3Stamps(address);
-    //         const gtcStaked = await checkGTCStaked(address);
-
-    //         output = {
-    //             ScoreType: 'Web3',
-    //             Score: score,
-    //             GTC_Staked: gtcStaked,
-    //             AdditionalCriteria: {}
-    //         };
-    //     } else {
-    //         output = {
-    //             ScoreType: 'Web3',
-    //             Score: 0,
-    //             GTC_Staked: gtcStaked,
-    //             AdditionalCriteria: {}
-    //         };
-    //     }
-  }
-
-  // return output;
+// Convert rows to JSON and write to a file
+async function writeJSONFromCSV(): Promise<void> {
+    try {
+        const jsonData = await parseCSV();
+        fs.writeFileSync("output.json", JSON.stringify(jsonData, null, 2), "utf8");
+        console.log("Data successfully written to output.json");
+    } catch (error) {
+        console.error("Error processing the CSV file:", error);
+    }
 }
 
-// main("someAddress").then(console.log);
-qualifiesForWeb3Stamp("0xe4afbc14c3b52c04c273edcffc1f9568b5259cca")
+writeJSONFromCSV();
+
+
+
+
+
+const endTime = new Date().getTime();
+const timeTaken = endTime - startTime;
+console.log(`Function took ${timeTaken} milliseconds to execute.`);
