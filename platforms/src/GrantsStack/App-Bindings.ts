b3f1cf2e1038f1ddd1b496895feedd77f73c36b5
Lucian Hymer <lucian.hymer@gmail.com>
2023-08-28 11:52:19 -0600

    fix(app): fixed GrantsStack stamp description, only >=$1 dontations are counted

--- platforms/src/GrantsStack/App-Bindings.ts
+++ platforms/src/GrantsStack/App-Bindings.ts
@@ -8,16 +8,14 @@ export class GrantsStackPlatform extends Platform {
   redirectUri: string = null;
 
   banner = {
-    heading:
-      "Note: Only Alpha and Beta rounds run by Gitcoin are included. For the Alpha program, only donations larger than $1 are counted. For the Beta program, only matching-eligible contributions are counted. Donations during Grants Round 18 (August '23) will be added by the end of September.",
+    heading: `Note: Only Alpha and Beta rounds run by Gitcoin are included, and only
+    donations of $1 or larger are counted. For the Beta program, only matching-eligible
+    contributions are counted. Donations during Grants Round 18 (August '23) will be
+    added by the end of September.`,
   };
 
-  async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
+  async getProviderPayload(_appContext: AppContext): Promise<ProviderPayload> {
     const result = await Promise.resolve({});
     return result;
-  }
-
-  getOAuthUrl(state: string): Promise<string> {
-    throw new Error("Method not implemented.");
   }
 }
