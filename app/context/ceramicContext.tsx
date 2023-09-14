0978bd4a88d70dc4e385060a90a9c519c3a73c88
Aminah Burch <hello@aminah.io>
2023-09-14 14:36:37 +0300

    feat(app, iam): removes backfilling and adds configError

--- app/context/ceramicContext.tsx
+++ app/context/ceramicContext.tsx
@@ -448,6 +448,7 @@ export const CeramicContextProvider = ({ children }: { children: any }) => {
   const passportLoadDoesNotExist = async () => {
     try {
       await handleCreatePassport();
+      loadCeramicPassport();
     } catch (e) {
       return false;
     }
@@ -511,6 +512,7 @@ export const CeramicContextProvider = ({ children }: { children: any }) => {
         const { status, passport } = await Promise.race<PassportLoadResponse>([
           returnEmptyPassportAfterTimeout(parseInt(CERAMIC_TIMEOUT_MS)),
           returnEmptyPassportOnCancel(),
+          loadCeramicPassport(),
         ]);
         if (status === "Success" && passport?.stamps.length) {
           initialStamps = passport.stamps;
@@ -539,6 +541,42 @@ export const CeramicContextProvider = ({ children }: { children: any }) => {
     new Promise<PassportLoadResponse>((resolve) =>
       setTimeout(() => resolve({ status: "Success", passport: { stamps: [] } }), timeout)
     );
+
+  // Start also fetching the passport from ceramic.
+  // We only do this to asses the "health" of the Passport & Stamps
+  // In case of erros we will force a reset of the Pasport.
+  // In case of borked stamps, we will not reset those, we'll simply ignore the borked stamps
+  // and the user ca claim other stamps
+  const loadCeramicPassport = async (): Promise<PassportLoadResponse> => {
+    if (ceramicClient) {
+      const ret = await ceramicClient.getPassport();
+      switch (ret.status) {
+        case "Success":
+          // Ok, nothing to do for now
+          break;
+        case "StampCacaoError":
+          // Ok, nothing to do for now. We will ignore borked stamps
+          break;
+        case "PassportCacaoError":
+          // We need to reset the passport to the last stable state
+          datadogRum.addError(
+            "Passport CACAO error -- error thrown on initial fetch. Going to refresh passport with SyncOptions.SYNC_ALWAYS option",
+            { address }
+          );
+          await ceramicClient.refreshPassport();
+          break;
+        case "DoesNotExist":
+          // Ok, nothing to do for now
+          break;
+        case "ExceptionRaised":
+          // Ok, nothing to do for now
+          break;
+      }
+      return ret;
+    }
+    // We just return an error here
+    return { status: "ExceptionRaised", passport: undefined };
+  };
 
   const handleAddStamps = async (stamps: Stamp[]): Promise<void> => {
     try {
