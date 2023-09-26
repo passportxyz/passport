8c5b18bf7b09be9dab7650e8be90b2ccb6bb5e57
schultztimothy <schultz.timothy52@gmail.com>
2023-09-26 07:57:17 -0600

    chore(app): remove ledger

--- app/utils/onboard.ts
+++ app/utils/onboard.ts
@@ -1,6 +1,5 @@
 import { init } from "@web3-onboard/react";
 import injectedModule from "@web3-onboard/injected-wallets";
-import ledgerModule from "@web3-onboard/ledger";
 import walletConnectModule, { WalletConnectOptions } from "@web3-onboard/walletconnect";
 import { chains } from "./chains";
 
@@ -19,16 +18,10 @@ const onBoardExploreUrl =
   (process.env.NEXT_PUBLIC_WEB3_ONBOARD_EXPLORE_URL as string) || "https://passport.gitcoin.co/";
 
 const walletConnect = walletConnectModule(walletConnectOptions);
-// Include ledger capabilities
-const ledger = ledgerModule({
-  walletConnectVersion: 2,
-  enableDebugLogs: false,
-  projectId: walletConnectProjectId,
-});
 
 // Exports onboard-core instance (https://github.com/blocknative/web3-onboard)
 export const initWeb3Onboard = init({
-  wallets: [injected, ledger, walletConnect],
+  wallets: [injected, walletConnect],
   chains: chains.map(({ id, token, label, rpcUrl, icon }) => ({ id, token, label, rpcUrl, icon })),
   appMetadata: {
     name: "Passport",
