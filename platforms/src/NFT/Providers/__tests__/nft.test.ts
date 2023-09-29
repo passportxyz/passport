e9433edd37514261253ef86a3d5141bd213ccf3d
schultztimothy <schultz.timothy52@gmail.com>
2023-09-29 14:57:47 -0600

    chore(platofrms): update nft test to match new error schema

--- platforms/src/NFT/Providers/__tests__/nft.test.ts
+++ platforms/src/NFT/Providers/__tests__/nft.test.ts
@@ -6,6 +6,7 @@ import { RequestPayload } from "@gitcoin/passport-types";
 
 // ----- Libs
 import axios from "axios";
+import { ProviderExternalVerificationError } from "../../../types";
 
 jest.mock("axios");
 
@@ -19,12 +20,59 @@ beforeEach(() => {
 
 describe("Attempt verification", function () {
   it.each([
-    [200, 0, false],
-    [200, 1, true],
-    [200, 200, true],
-    [300, 0, false],
     [400, 1, false],
     [500, 200, false],
+  ])(
+    " - when exeption is thrown with status of %p and totalCount is %p valid es expected to be %p",
+    async (httpStatus, totalCount: number, expectedValid: boolean) => {
+      (axios.get as jest.Mock).mockRejectedValueOnce("bad request");
+      const nftProvider = new NFTProvider();
+      await expect(async () => {
+        return await nftProvider.verify({
+          address: MOCK_ADDRESS,
+        } as unknown as RequestPayload);
+      }).rejects.toThrow(
+        // eslint-disable-next-line quotes
+        new ProviderExternalVerificationError("NFT check error: {}")
+      );
+
+      // Check the request to get the NFTs
+      expect(axios.get).toHaveBeenCalledTimes(1);
+      expect(mockedAxios.get).toBeCalledWith(getNFTEndpoint(), {
+        params: {
+          withMetadata: "false",
+          owner: MOCK_ADDRESS_LOWER,
+          pageSize: 1,
+        },
+      });
+    }
+  );
+  it("should return an error when request is successful but they do not qualify", async () => {
+    (axios.get as jest.Mock).mockImplementation((url) => {
+      return Promise.resolve({
+        status: 200,
+        data: {
+          totalCount: 0,
+          ownedNfts: [],
+        },
+      });
+    });
+
+    const nftProvider = new NFTProvider();
+
+    const nftPayload = await nftProvider.verify({
+      address: MOCK_ADDRESS,
+    } as unknown as RequestPayload);
+
+    expect(nftPayload).toEqual({
+      valid: false,
+      record: undefined,
+      errors: ["You do not have the required amount of NFTs -- Your NFT count: 0."],
+    });
+  });
+  it.each([
+    [200, 1, true],
+    [200, 200, true],
   ])(
     " - when status is %p and totalCount is %p valid es expected to be %p",
     async (httpStatus, totalCount: number, expectedValid: boolean) => {
@@ -39,6 +87,7 @@ describe("Attempt verification", function () {
       });
 
       const nftProvider = new NFTProvider();
+
       const nftPayload = await nftProvider.verify({
         address: MOCK_ADDRESS,
       } as unknown as RequestPayload);
@@ -52,34 +101,14 @@ describe("Attempt verification", function () {
           pageSize: 1,
         },
       });
-
-      if (expectedValid) {
-        expect(nftPayload).toEqual({
-          valid: true,
-          record: {
-            address: MOCK_ADDRESS_LOWER,
-            "NFT#numNFTsGte": "1",
-          },
-        });
-      } else {
-        expect(nftPayload).toEqual({
-          valid: false,
-        });
-      }
+      expect(nftPayload).toEqual({
+        valid: true,
+        record: {
+          address: MOCK_ADDRESS_LOWER,
+          "NFT#numNFTsGte": "1",
+        },
+        errors: [],
+      });
     }
   );
-
-  it("should return invalid payload when unable to get NFTs (exception thrown)", async () => {
-    mockedAxios.get.mockImplementation(async () => {
-      throw "some kind of error";
-    });
-
-    const nftProvider = new NFTProvider();
-    const nftPayload = await nftProvider.verify({
-      address: MOCK_ADDRESS,
-    } as unknown as RequestPayload);
-
-    expect(axios.get).toHaveBeenCalledTimes(1);
-    expect(nftPayload).toMatchObject({ valid: false });
-  });
 });
