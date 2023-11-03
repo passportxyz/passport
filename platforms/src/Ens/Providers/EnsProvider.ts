6b9f7352ad57c69bb9abc0d85ca3925749fd9e19
Lucian <lucian@gitcoin.co>
2023-11-03 15:21:06 -0600

    feat(platforms): restrict ENS stamp to only ENS names using the public resolver (#1852)

--- platforms/src/Ens/Providers/EnsProvider.ts
+++ platforms/src/Ens/Providers/EnsProvider.ts
@@ -5,6 +5,8 @@ import type { RequestPayload, VerifiedPayload } from "@gitcoin/passport-types";
 // ----- Credential verification
 import { getRPCProvider } from "../../utils/signer";
 
+const ENS_PUBLIC_RESOLVER = "0x231b0ee14048e9dccd1d247744d114a4eb5e8e63";
+
 // Export a Ens Provider to carry out Ens name check and return a record object
 export class EnsProvider implements Provider {
   // Give the provider a type so that we can select it with a payload
@@ -27,17 +29,21 @@ export class EnsProvider implements Provider {
     try {
       const provider = getRPCProvider(payload);
       reportedName = await provider.lookupAddress(payload.address);
-      valid = !!reportedName;
-      if (valid) {
-        record = {
-          ens: reportedName,
-        };
+
+      if (reportedName) {
+        const resolver = await provider.getResolver(reportedName);
+        if (resolver?.address?.toLowerCase() === ENS_PUBLIC_RESOLVER) {
+          valid = true;
+          record = {
+            ens: reportedName,
+          };
+        } else {
+          errors.push(
+            "Apologies! Your primary ENS name uses an alternative resolver and is not eligible for the ENS stamp."
+          );
+        }
       } else {
         errors.push("Primary ENS name was not found for given address.");
-      }
-
-      if (!valid && errors.length === 0) {
-        errors.push("We were unable to determine the cause of your error");
       }
 
       return {
