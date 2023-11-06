f6f982e3c0f1ead5fd51cccd9bdc476cd83041b0
Aminah Burch <hello@aminah.io>
2023-11-06 22:46:45 +0300

    feat(platforms): adds cid to find staked only events and updates community logic

--- platforms/src/GtcStaking/Providers/GtcStaking.ts
+++ platforms/src/GtcStaking/Providers/GtcStaking.ts
@@ -5,12 +5,16 @@ import type { ProviderContext, PROVIDER_ID, RequestPayload, VerifiedPayload } fr
 // ----- Libs
 import axios from "axios";
 import { handleProviderAxiosError } from "../../utils/handleProviderAxiosError";
+import BigNumber from "bignumber.js";
+
+// ----- Utils
+import { buildCID } from "../../utils/createCid";
 
 const gtcStakingEndpoint = `${process.env.PASSPORT_SCORER_BACKEND}registry/gtc-stake`;
 const apiKey = process.env.SCORER_API_KEY;
 
 type UserStake = {
-  selfStake: number;
+  selfStake: BigNumber;
   communityStakes: Stake[];
   error?: string;
 };
@@ -46,7 +50,7 @@ export type GtcStakingContext = ProviderContext & {
 
 export type GtcStakingProviderOptions = {
   type: PROVIDER_ID;
-  threshold?: number;
+  threshold?: BigNumber | number;
   dataKey: keyof UserStake;
   communityTypeCount?: number | undefined;
   // Only needed for historic hashes, can be left
@@ -56,7 +60,7 @@ export type GtcStakingProviderOptions = {
 
 export class GtcStakingProvider implements Provider {
   type: PROVIDER_ID;
-  threshold?: number;
+  threshold?: BigNumber | number;
   dataKey: keyof UserStake;
   identifier: string;
   communityTypeCount?: number | undefined;
@@ -92,7 +96,7 @@ export class GtcStakingProvider implements Provider {
 
       const selfStakeAmount = stakeData.selfStake;
       const communityStakes = stakeData.communityStakes;
-      const commStakeCounts = checkCommunityStakes(communityStakes, address);
+      const commStakeCounts = await checkCommunityStakes(communityStakes, address);
 
       if (selfStakeAmount >= this.threshold) valid = true;
 
@@ -109,7 +113,7 @@ export class GtcStakingProvider implements Provider {
         };
       } else if (!valid && selfStakeAmount < this.threshold) {
         errors.push(
-          `Your current GTC self staking amount is ${selfStakeAmount} GTC, which is below the required ${this.threshold} GTC for this stamp.`
+          `Your current GTC self staking amount is ${selfStakeAmount.toString()} GTC, which is below the required ${this.threshold.toString()} GTC for this stamp.`
         );
       } else {
         errors.push(
@@ -128,42 +132,41 @@ export class GtcStakingProvider implements Provider {
   }
 }
 
-function checkCommunityStakes(communityStakes: Stake[], address: string): CommunityStakingCounts {
-  let bcs1gte5 = 0;
-  let ecs2gte10 = 0;
-  const trustCitStakers = new Set();
+async function checkCommunityStakes(communityStakes: Stake[], address: string): Promise<CommunityStakingCounts> {
+  const bcsMap = new Map<string, number>();
+  const ecsMap = new Map<string, number>();
+  const tcMap = new Map<string, number>();
+  const tcSet = new Set();
 
-  communityStakes.forEach((stake: Stake) => {
-    if (
-      stake.event_type === "Xstake" &&
-      stake.staked === true &&
-      parseInt(stake.amount) >= 5 &&
-      (stake.address === address || stake.staker === address)
-    ) {
-      bcs1gte5 += 1;
+  for (let i = 0; i < communityStakes.length; i++) {
+    const stake = communityStakes[i];
+    const cid = await buildCID({ address: stake.address, staker: stake.staker, amount: stake.amount });
+    const currentAmount = new BigNumber(stake.amount);
+
+    if (stake.address === address || stake.staker === address) {
+      if (currentAmount.gte(5) && currentAmount.lt(10)) {
+        bcsMap.set(cid, (bcsMap.get(cid) || 0) + 1);
+      }
+      if (currentAmount.gte(10)) {
+        bcsMap.set(cid, (bcsMap.get(cid) || 0) + 1);
+        ecsMap.set(cid, (ecsMap.get(cid) || 0) + 1);
+      }
     }
 
-    if (
-      stake.event_type === "Xstake" &&
-      stake.staked === true &&
-      parseInt(stake.amount) >= 10 &&
-      (stake.address === address || stake.staker === address)
-    ) {
-      ecs2gte10 += 1;
+    if (stake.address === address && currentAmount.gte(20)) {
+      if (!tcSet.has(stake.staker)) {
+        tcSet.add(stake.staker);
+        bcsMap.set(cid, (bcsMap.get(cid) || 0) + 1);
+        ecsMap.set(cid, (ecsMap.get(cid) || 0) + 1);
+        tcMap.set(cid, (tcMap.get(cid) || 0) + 1);
+      }
     }
+  }
 
-    if (
-      stake.event_type === "Xstake" &&
-      stake.staked &&
-      stake.address === address &&
-      parseInt(stake.amount) >= 20 &&
-      trustCitStakers.size <= 5
-    ) {
-      trustCitStakers.add(stake.staker);
-    }
-  });
-
-  const tc5gte20 = trustCitStakers.size;
+  // Use the maps to find unpaired CIDs.
+  const bcs1gte5 = [...bcsMap].filter(([_, count]) => count === 1).map(([cid, _]) => cid).length;
+  const ecs2gte10 = [...ecsMap].filter(([_, count]) => count === 1).map(([cid, _]) => cid).length;
+  const tc5gte20 = [...tcMap].filter(([_, count]) => count === 1).map(([cid, _]) => cid).length;
 
   return {
     bcs1gte5,
@@ -191,13 +194,13 @@ async function verifyStake(payload: RequestPayload, context: GtcStakingContext):
         stake.event_type === "SelfStake" ? selfStakes.push(stake) : communityStakes.push(stake);
       });
 
-      const maxSelfStake: number | undefined = selfStakes.reduce((acc, curr) => {
+      const selfStake: BigNumber = selfStakes.reduce((acc, curr) => {
         if (curr.staked === true) {
-          return acc + parseInt(curr.amount);
+          return acc.plus(new BigNumber(curr.amount));
+        } else {
+          return acc.minus(new BigNumber(curr.amount));
         }
-      }, 0);
-
-      const selfStake = maxSelfStake;
+      }, new BigNumber(0));
 
       if (!context.gtcStaking) context.gtcStaking = {};
 
