76c60f94f05637106bb8681590b46fa5a06d9d48
Lucian Hymer <lucian@gitcoin.co>
2023-11-13 12:03:47 -0700

    feat(platforms): updated GTC stake provider to use new API response

--- platforms/src/GtcStaking/Providers/communityStaking.ts
+++ platforms/src/GtcStaking/Providers/communityStaking.ts
@@ -1,19 +1,81 @@
-import { GtcStakingProvider, GtcStakingProviderOptions } from "./GtcStaking";
+import { RequestPayload, VerifiedPayload } from "@gitcoin/passport-types";
+import BigNumber from "bignumber.js";
+import { GtcStakingContext, GtcStakingProvider, GtcStakingProviderOptions, Stake } from "./GtcStaking";
 
 class CommunityStakingBaseProvider extends GtcStakingProvider {
-  constructor(options: Omit<GtcStakingProviderOptions, "dataKey">) {
-    super({
-      ...options,
-      dataKey: "communityStakes",
-    });
+  minimumCountCommunityStakes: number;
+
+  constructor(options: GtcStakingProviderOptions & { minimumCountCommunityStakes: number }) {
+    super(options);
+    this.minimumCountCommunityStakes = options.minimumCountCommunityStakes;
+  }
+
+  async verify(payload: RequestPayload, context: GtcStakingContext): Promise<VerifiedPayload> {
+    const address = this.getAddress(payload);
+    const stakeData = await this.getStakes(payload, context);
+    const communityStakes = stakeData.communityStakes;
+
+    const countRelevantStakes = this.getCountRelevantStakes(communityStakes, address);
+
+    if (countRelevantStakes >= this.minimumCountCommunityStakes) {
+      return {
+        valid: true,
+        record: { address },
+      };
+    } else {
+      return {
+        valid: false,
+        errors: [
+          `There are currently ${countRelevantStakes} community stakes of at least ${this.thresholdAmount.toString()} GTC on/by your address, ` +
+            `you need a minimum of ${this.minimumCountCommunityStakes} relevant community stakes to claim this stamp`,
+        ],
+      };
+    }
+  }
+
+  getCountRelevantStakes(communityStakes: Stake[], address: string): number {
+    const stakesOnAddressByOthers: Record<string, BigNumber> = {};
+    const stakesByAddressOnOthers: Record<string, BigNumber> = {};
+
+    for (let i = 0; i < communityStakes.length; i++) {
+      const stake = communityStakes[i];
+      const stakeAmount = new BigNumber(stake.amount);
+
+      if (stake.staker === address && stake.address !== address) {
+        stakesByAddressOnOthers[stake.address] ||= new BigNumber(0);
+        if (stake.staked) {
+          stakesByAddressOnOthers[stake.address] = stakesByAddressOnOthers[stake.address].plus(stakeAmount);
+        } else {
+          stakesByAddressOnOthers[stake.address] = stakesByAddressOnOthers[stake.address].sub(stakeAmount);
+        }
+      } else if (stake.address === address && stake.staker !== address) {
+        stakesOnAddressByOthers[stake.staker] ||= new BigNumber(0);
+        if (stake.staked) {
+          stakesOnAddressByOthers[stake.staker] = stakesOnAddressByOthers[stake.staker].plus(stakeAmount);
+        } else {
+          stakesOnAddressByOthers[stake.staker] = stakesOnAddressByOthers[stake.staker].sub(stakeAmount);
+        }
+      }
+    }
+
+    return [...Object.entries(stakesByAddressOnOthers), ...Object.entries(stakesOnAddressByOthers)].reduce(
+      (count, [_address, amount]) => {
+        if (amount.gte(this.thresholdAmount)) {
+          return count + 1;
+        }
+        return count;
+      },
+      0
+    );
   }
 }
+
 export class BeginnerCommunityStakerProvider extends CommunityStakingBaseProvider {
   constructor() {
     super({
       type: "BeginnerCommunityStaker",
-      identifier: "bcs1gte5",
-      communityTypeCount: 1,
+      thresholdAmount: new BigNumber(5),
+      minimumCountCommunityStakes: 1,
     });
   }
 }
@@ -22,8 +84,8 @@ export class ExperiencedCommunityStakerProvider extends CommunityStakingBaseProv
   constructor() {
     super({
       type: "ExperiencedCommunityStaker",
-      identifier: "ecs2gte10",
-      communityTypeCount: 2,
+      thresholdAmount: new BigNumber(10),
+      minimumCountCommunityStakes: 2,
     });
   }
 }
@@ -32,8 +94,8 @@ export class TrustedCitizenProvider extends CommunityStakingBaseProvider {
   constructor() {
     super({
       type: "TrustedCitizen",
-      identifier: "tc5gte20",
-      communityTypeCount: 5,
+      thresholdAmount: new BigNumber(20),
+      minimumCountCommunityStakes: 5,
     });
   }
 }
