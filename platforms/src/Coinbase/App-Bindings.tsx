145f6952a25a19ee316971fad6b94dfa4dfc171d
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2023-11-24 11:10:59 +0200

    feat(app): UI changes for coinbase stamp

--- platforms/src/Coinbase/App-Bindings.tsx
+++ platforms/src/Coinbase/App-Bindings.tsx
@@ -14,24 +14,6 @@ export class CoinbasePlatform extends Platform {
   clientId: string = null;
   redirectUri: string = null;
 
-  banner = {
-    content: (
-      <div>
-        This stamp verifies that you have a Coinbase account and that you have completed the{" "}
-        <Link href="https://help.coinbase.com/en/coinbase/getting-started/getting-started-with-coinbase/id-doc-verification">
-          Coinbase Onchain Verification
-        </Link>{" "}
-        process. In order to qualify for this stamp, you must complete the Coinbase Oauth process. Once authenticated we
-        will query the{" "}
-        <Link href="https://base.easscan.org/schema/view/0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9">
-          Ethereum Attestation Service
-        </Link>{" "}
-        to verify that you have a valid Verified Account attestation for the connected account. Note, Coinbase allows
-        you to verify up to three accounts, but we will only issue points for a single address.
-      </div>
-    ),
-  };
-
   constructor(options: PlatformOptions = {}) {
     super();
     this.clientId = options.clientId as string;
--- platforms/src/Coinbase/Providers-config.ts
+++ platforms/src/Coinbase/Providers-config.ts
@@ -4,15 +4,16 @@ import { CoinbaseProvider } from "./Providers/coinbase";
 export const PlatformDetails: PlatformSpec = {
   icon: "./assets/coinbaseStampIcon.svg",
   platform: "Coinbase",
-  name: "Coinbase Account & Identity Check",
-  description: "Dual Verification Credential",
+  name: "Coinbase",
+  description: "Confirm Your Coinbase Account & Onchain Identity",
   connectMessage: "Connect Account",
+  website: "https://www.coinbase.com/onchain-verify",
 };
 
 export const ProviderConfig: PlatformGroupSpec[] = [
   {
-    platformGroup: "Account Name",
-    providers: [{ title: "Encrypted", name: "CoinbaseDualVerification" }],
+    platformGroup: "Account & Identity Verification",
+    providers: [{ title: "Your privacy is paramount. We only retain a unique hash to acknowledge your account's verification.", name: "CoinbaseDualVerification" }],
   },
 ];
 
