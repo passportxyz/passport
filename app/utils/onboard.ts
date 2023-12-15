84e98aa51767a2b609dd93876aebb1695d1488c0
schultztimothy <schultz.timothy52@gmail.com>
2023-12-15 08:55:41 -0700

    chore(app): re enable wallet connect

--- app/utils/onboard.ts
+++ app/utils/onboard.ts
@@ -1,6 +1,6 @@
 import { init } from "@web3-onboard/react";
 import injectedModule from "@web3-onboard/injected-wallets";
-// import walletConnectModule, { WalletConnectOptions } from "@web3-onboard/walletconnect";
+import walletConnectModule, { WalletConnectOptions } from "@web3-onboard/walletconnect";
 import { chains } from "./chains";
 
 // Injected wallet - shows all available injected wallets
@@ -8,21 +8,20 @@ import { chains } from "./chains";
 const injected = injectedModule();
 
 // web3Onboard modules
-// const walletConnectProjectId = (process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string) || "default-project-id";
-//
-// const walletConnectOptions: WalletConnectOptions = {
-//   projectId: walletConnectProjectId,
-// };
+const walletConnectProjectId = (process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID as string) || "default-project-id";
+
+const walletConnectOptions: WalletConnectOptions = {
+  projectId: walletConnectProjectId,
+};
 
 const onBoardExploreUrl =
   (process.env.NEXT_PUBLIC_WEB3_ONBOARD_EXPLORE_URL as string) || "https://passport.gitcoin.co/";
 
-// const walletConnect = walletConnectModule(walletConnectOptions);
+const walletConnect = walletConnectModule(walletConnectOptions);
 
 // Exports onboard-core instance (https://github.com/blocknative/web3-onboard)
 export const onboard = init({
-  // wallets: [injected, walletConnect],
-  wallets: [injected],
+  wallets: [injected, walletConnect],
   chains: chains.map(({ id, token, label, rpcUrl, icon }) => ({ id, token, label, rpcUrl, icon })),
   appMetadata: {
     name: "Passport",
