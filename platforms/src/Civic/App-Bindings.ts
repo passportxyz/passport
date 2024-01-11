10658155bc6f4bc55340ae98fe43f1937761c817
Daniel Kelleher <dankelleher@yahoo.com>
2024-01-11 17:11:42 +0100

    feat: civic stamp - sidebar info update (#2029)
    Text and url change to include new chains and referrer

--- platforms/src/Civic/App-Bindings.ts
+++ platforms/src/Civic/App-Bindings.ts
@@ -17,12 +17,14 @@ export class CivicPlatform extends Platform {
   }
 
   banner = {
-    heading: "Click on 'Get Civic Pass' to visit Civic and issue your passes.",
+    heading: "Click on 'Get Civic Pass' to visit Civic and request your passes.",
     content:
-      "Once you have one or more Civic Passes, select them above and click 'Verify'. Note: Polygon is recommended for the lowest gas cost.",
+      "You may also use this link to check and update the status of passes. Note: Polygon is recommended for lowest gas cost. " +
+      "After passes have been issued to your linked wallet, select the corresponding box(es) below and click SAVE. " +
+      "Gitcoin will check for passes and save those that are valid.",
     cta: {
       label: "Get Civic Pass",
-      url: "https://getpass.civic.com?scope=uniqueness,captcha,liveness&chain=polygon,arbitrum%20one,xdc,ethereum,fantom",
+      url: "https://getpass.civic.com?scope=uniqueness,captcha,liveness&chain=polygon,arbitrum%20one,xdc,ethereum,fantom,optimism,base,avalanche&referrer=gitcoin-passport",
     },
   };
 }
