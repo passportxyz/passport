d194ac1354356d8ecb4461299ca478363b0be58a
schultztimothy <schultz.timothy52@gmail.com>
2024-01-24 10:04:19 -0700

    chore(platforms): update copy remove debugger

--- platforms/src/ETH/App-Bindings.ts
+++ platforms/src/ETH/App-Bindings.ts
@@ -7,6 +7,12 @@ export class ETHPlatform extends Platform {
   redirectUri: string = null;
   isEVM = true;
 
+  banner = {
+    heading: `
+      Ethereum Activity: A nuanced, adaptive measure of your authentic involvement, shaping trust in the Ethereum ecosystem.
+    `,
+  };
+
   async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
     const result = await Promise.resolve({});
     return result;
