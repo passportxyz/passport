6cc7f89374c41ce757fe1bc629ed13ebc53a6964
Lucian <lucian@gitcoin.co>
2024-01-25 14:05:00 -0700

    fix(platforms): tweaks to ethereum stamp info (#2077)

--- platforms/src/ETH/App-Bindings.ts
+++ platforms/src/ETH/App-Bindings.ts
@@ -9,7 +9,10 @@ export class ETHPlatform extends Platform {
 
   banner = {
     heading: `
-      Ethereum Activity: A nuanced, adaptive measure of your authentic involvement, shaping trust in the Ethereum ecosystem.
+      Click verify to process your Ethereum L1 transactions. Gitcoin uses a constantly
+      evolving model to review your transaction history and compare against known Sybil
+      behavior. The number of points you'll receive based on many factors related to the
+      overall activity of the address.
     `,
   };
 
