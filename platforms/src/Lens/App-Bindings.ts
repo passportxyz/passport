57c9245094d840de59de8336f04d9c2e1e94b5c0
Lucian Hymer <lucian@gitcoin.co>
2024-02-27 11:25:22 -0700

    feat(platforms): adding more info to Lens sidebar

--- platforms/src/Lens/App-Bindings.ts
+++ platforms/src/Lens/App-Bindings.ts
@@ -8,6 +8,18 @@ export class LensPlatform extends Platform {
   redirectUri: string = null;
   isEVM = true;
 
+  banner = {
+    content: `To add the Lens Stamp to your Gitcoin Passport, ensure you're using
+    your Lens Handle, not your profile. A Lens Handle is your unique identifier on
+    Lens, required for verification. Obtain a Handle either through the Lens beta or
+    by purchasing one from NFT marketplaces. Note: Verification may be delayed after
+    claiming your Handle.`,
+    cta: {
+      label: "Learn more",
+      url: "https://support.gitcoin.co/gitcoin-knowledge-base/gitcoin-passport/how-do-i-add-passport-stamps/guide-to-add-lens-stamp-to-gitcoin-passport",
+    },
+  };
+
   async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
     const result = await Promise.resolve({});
     return result;
