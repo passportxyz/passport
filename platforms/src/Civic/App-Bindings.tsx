399019201e23e739360b996cdfdabf38a9248d54
Lucian Hymer <lucian@gitcoin.co>
2024-02-27 11:53:38 -0700

    feat(platforms): adding more info to Civic sidebar

--- platforms/src/Civic/App-Bindings.tsx
+++ platforms/src/Civic/App-Bindings.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { AppContext, PlatformOptions, ProviderPayload } from "../types";
 import { Platform } from "../utils/platform";
 
@@ -18,10 +19,26 @@ export class CivicPlatform extends Platform {
 
   banner = {
     heading: "Click on 'Get Civic Pass' to visit Civic and request your passes.",
-    content:
-      "You may also use this link to check and update the status of passes. Note: Polygon is recommended for lowest gas cost. " +
-      "After passes have been issued to your linked wallet, select the corresponding box(es) below and click SAVE. " +
-      "Gitcoin will check for passes and save those that are valid.",
+    content: (
+      <div>
+        You may also use this link to check and update the status of passes. Note: Polygon is recommended for lowest gas
+        cost. After passes have been issued to your linked wallet, select the corresponding box(es) below and click
+        SAVE. Gitcoin will check for passes and save those that are valid.{" "}
+        <a
+          href="https://support.gitcoin.co/gitcoin-knowledge-base/gitcoin-passport/how-do-i-add-passport-stamps/guide-to-add-lens-stamp-to-gitcoin-passport"
+          style={{
+            color: "rgb(var(--color-foreground-2))",
+            textDecoration: "underline",
+            cursor: "pointer",
+            paddingLeft: "2px",
+          }}
+          target="_blank"
+        >
+          Learn more
+        </a>
+        .
+      </div>
+    ),
     cta: {
       label: "Get Civic Pass",
       url: "https://getpass.civic.com?scope=uniqueness,captcha,liveness&chain=polygon,arbitrum%20one,xdc,ethereum,fantom,optimism,base,avalanche&referrer=gitcoin-passport",
