f8bae0cf8a7b0ae884c051c4cfb5306d88bd4c8f
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2024-03-29 08:33:37 +0200

    feat(app): update also sample env file + make sure all DATADOG_* values default to the empty string

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -22,7 +22,7 @@ datadogRum.init({
   applicationId: process.env.NEXT_PUBLIC_DATADOG_APPLICATION_ID || "",
   clientToken: process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN || "",
   site: process.env.NEXT_PUBLIC_DATADOG_SITE || "",
-  service: process.env.NEXT_PUBLIC_DATADOG_SERVICE,
+  service: process.env.NEXT_PUBLIC_DATADOG_SERVICE || "",
   env: process.env.NEXT_PUBLIC_DATADOG_ENV || "",
   // Specify a version number to identify the deployed version of your application in Datadog
   // version: '1.0.0',
