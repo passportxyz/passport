ab29967c5e74917b38d68715fa4c3b07eca9b8cd
Lucian Hymer <lucian@gitcoin.co>
2024-04-16 17:03:33 -0600

    feat(platforms): added zksync model providers

--- platforms/src/ZkSync/Providers-config.ts
+++ platforms/src/ZkSync/Providers-config.ts
@@ -1,5 +1,6 @@
 import { PlatformSpec, PlatformGroupSpec, Provider } from "../types";
 import { ZkSyncEraProvider } from "./Providers/zkSyncEra";
+import { ZkSyncScore5Provider, ZkSyncScore20Provider, ZkSyncScore50Provider } from "./Providers/accountAnalysis";
 
 export const PlatformDetails: PlatformSpec = {
   icon: "./assets/zksyncStampIcon.svg",
@@ -13,9 +14,41 @@ export const PlatformDetails: PlatformSpec = {
 
 export const ProviderConfig: PlatformGroupSpec[] = [
   {
-    platformGroup: "zkSync Era",
-    providers: [{ title: "Transacted on zkSync Era", name: "ZkSyncEra" }],
+    platformGroup: "Transactional Verification",
+    providers: [
+      {
+        title: "Verified Transactor",
+        name: "ZkSyncEra",
+        description:
+          "Recognizes users whose transactions on zkSync Era have achieved verified status, confirming their active participation and trust in the platform.",
+      },
+    ],
+  },
+  {
+    platformGroup: "Engagement Levels in zkSync Era",
+    providers: [
+      {
+        title: "Engagement Explorer",
+        name: "zkSyncScore#5",
+        description: "For users who are actively exploring various features of zkSync Era.",
+      },
+      {
+        title: "L2 Believer",
+        name: "zkSyncScore#20",
+        description: "For users who regularly transact and demonstrate reliance on the zkSync Era platform.",
+      },
+      {
+        title: "zkSync Champion",
+        name: "zkSyncScore#50",
+        description: "For leading users who significantly contribute to and influence the zkSync community.",
+      },
+    ],
   },
 ];
 
-export const providers: Provider[] = [new ZkSyncEraProvider()];
+export const providers: Provider[] = [
+  new ZkSyncEraProvider(),
+  new ZkSyncScore5Provider(),
+  new ZkSyncScore20Provider(),
+  new ZkSyncScore50Provider(),
+];
--- platforms/src/ZkSync/__tests__/accountAnalysis.test.ts
+++ platforms/src/ZkSync/__tests__/accountAnalysis.test.ts
@@ -1,11 +1,11 @@
 import { RequestPayload } from "@gitcoin/passport-types";
 import axios from "axios";
 import {
-  ETHAdvocateProvider,
-  ETHMaxiProvider,
+  ZkSyncScore20Provider,
+  ZkSyncScore50Provider,
   ModelResponse,
-  getETHAnalysis,
-  ETHEnthusiastProvider,
+  getZkSyncAnalysis,
+  ZkSyncScore5Provider,
 } from "../Providers/accountAnalysis";
 
 const mockAddress = "0x0";
@@ -27,42 +27,42 @@ describe("AccountAnalysis Providers", () => {
     mockContext = {};
   });
 
-  it("should validate inputs for ETHEnthusiastProvider", async () => {
-    const mockedResponse = mockResponse(1);
+  it("should validate inputs for ZkSyncScore5Provider", async () => {
+    const mockedResponse = mockResponse(5);
     mockedAxios.post.mockResolvedValueOnce(mockedResponse);
 
-    const ethAdvocateProvider = new ETHEnthusiastProvider();
+    const ethAdvocateProvider = new ZkSyncScore5Provider();
     const payload = await ethAdvocateProvider.verify({ address: mockAddress } as RequestPayload, mockContext);
 
     expect(payload.valid).toBe(true);
     expect(payload.record).toEqual({ address: mockAddress });
   });
 
-  it("should validate inputs for ETHAdvocateProvider", async () => {
+  it("should validate inputs for ZkSyncScore20Provider", async () => {
     const mockedResponse = mockResponse(50);
     mockedAxios.post.mockResolvedValueOnce(mockedResponse);
-    const ethAdvocateProvider = new ETHAdvocateProvider();
+    const ethAdvocateProvider = new ZkSyncScore20Provider();
     const payload = await ethAdvocateProvider.verify({ address: mockAddress } as RequestPayload, mockContext);
 
     expect(payload.valid).toBe(true);
     expect(payload.record).toEqual({ address: mockAddress });
   });
 
-  it("should validate inputs for ETHMaxiProvider", async () => {
+  it("should validate inputs for ZkSyncScore50Provider", async () => {
     jest.clearAllMocks();
     const mockedResponse = mockResponse(88);
     mockedAxios.post.mockResolvedValueOnce(mockedResponse);
-    const ethAdvocateProvider = new ETHMaxiProvider();
+    const ethAdvocateProvider = new ZkSyncScore50Provider();
     const payload = await ethAdvocateProvider.verify({ address: mockAddress } as RequestPayload, mockContext);
 
     expect(payload.valid).toBe(true);
     expect(payload.record).toEqual({ address: mockAddress });
   });
 
-  it("should validate invalid inputs for ETHMaxiProvider", async () => {
-    const mockedResponse = mockResponse(70);
+  it("should validate invalid inputs for ZkSyncScore50Provider", async () => {
+    const mockedResponse = mockResponse(40);
     mockedAxios.post.mockResolvedValueOnce(mockedResponse);
-    const ethAdvocateProvider = new ETHMaxiProvider();
+    const ethAdvocateProvider = new ZkSyncScore50Provider();
     const payload = await ethAdvocateProvider.verify({ address: mockAddress } as RequestPayload, mockContext);
 
     expect(payload.valid).toBe(false);
@@ -71,16 +71,16 @@ describe("AccountAnalysis Providers", () => {
   it("should handle errors gracefully", async () => {
     jest.clearAllMocks();
     mockedAxios.post.mockRejectedValueOnce(new Error("Test Error"));
-    const ethAdvocateProvider = new ETHMaxiProvider();
+    const ethAdvocateProvider = new ZkSyncScore50Provider();
     await expect(ethAdvocateProvider.verify({ address: mockAddress } as RequestPayload, mockContext)).rejects.toThrow();
   });
-  describe("getETHAnalysis", () => {
+  describe("getZkSyncAnalysis", () => {
     it("should use value from context if present", async () => {
       const mockedResponse = mockResponse(80);
       mockedAxios.post.mockResolvedValueOnce(mockedResponse);
       mockContext = {};
-      const response1 = await getETHAnalysis(mockAddress, mockContext);
-      const response2 = await getETHAnalysis(mockAddress, mockContext);
+      const response1 = await getZkSyncAnalysis(mockAddress, mockContext);
+      const response2 = await getZkSyncAnalysis(mockAddress, mockContext);
       expect(response1.humanProbability).toEqual(80);
       expect(response2.humanProbability).toEqual(80);
       // eslint-disable-next-line @typescript-eslint/unbound-method
