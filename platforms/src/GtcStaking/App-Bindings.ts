4c1fceff1b51ce0ca9275886114f5a9196d98706
larisa17 <larisabucur17@gmail.com>
2024-05-21 10:28:44 +0300

    feat(links): WIP gtc staking

--- platforms/src/GtcStaking/App-Bindings.ts
+++ platforms/src/GtcStaking/App-Bindings.ts
@@ -1,26 +1,53 @@
 /* eslint-disable */
-import { AppContext, Platform, PlatformOptions, ProviderPayload } from "../types";
+import React from "react";
+import { AppContext, PlatformOptions, ProviderPayload } from "../types";
+import { Platform } from "../utils/platform";
 
-export class GTCStakingPlatform implements Platform {
+export class GTCStakingPlatform extends Platform {
   platformId = "GtcStaking";
   path = "GtcStaking";
   isEVM = true;
   clientId: string = null;
-  redirectUri: string = null;
+
+  constructor(options: PlatformOptions = {}) {
+    super();
+    this.state = options.state as string;
+    this.redirectUri = options.redirectUri as string;
+  }
+
+  async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
+    const result = await Promise.resolve({});
+    return result;
+  }
+
+  getOAuthUrl(state: string): Promise<string> {
+    throw new Error("Method not implemented.");
+  }
+
 
   banner = {
-    content: "If you haven't staked yet, you can do so now.",
+    content: (
+      <div>
+        "If you haven't staked yet, you can do so now."
+        <a
+          href="https://support.passport.xyz/passport-knowledge-base/gtc-staking/identity-staking-questions"
+          style={{
+            color: "rgb(var(--color-foreground-2))",
+            textDecoration: "underline",
+            cursor: "pointer",
+            paddingLeft: "2px",
+          }}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Learn more
+        </a>
+      </div>
+    ),
     cta: {
       label: "Identity Staking",
       url: "https://www.staking.passport.gitcoin.co/",
     },
   };
 
-  async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
-    return {};
-  }
-
-  getOAuthUrl(state: string): Promise<string> {
-    throw new Error("Method not implemented.");
-  }
 }
