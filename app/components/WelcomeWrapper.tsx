8eeb34d8cf475575a73636660683840dcb41d40e
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2024-05-29 12:40:54 +0300

    fix: ensure that text & content does not change in on-boarding flow

--- app/components/WelcomeWrapper.tsx
+++ app/components/WelcomeWrapper.tsx
@@ -56,10 +56,10 @@ const PlatformCard = ({}) => {
 
 export const WelcomeWrapper = ({ content, children }: WelcomeWrapperProps) => {
   return (
-    <div className="grid grid-rows-[minmax(0,1fr)_min-content_minmax(0,2fr)]">
-      <div className="row-start-1 row-end-4 p-4 text-lg text-color-1 md:row-start-2 md:row-end-3">
+    <div className="grid grid-rows-4-4">
+      <div className="row-start-1 row-end-4 p-4 text-lg text-color-1 md:row-end-3 md:pt-80">
         <div className="flex h-full w-full auto-rows-min flex-col gap-2 md:grid md:grid-cols-2">
-          <div className="row-start-2 flex flex-col gap-2 place-items-start">
+          <div className="flex flex-col gap-2 place-items-start">
             <div className="font-heading text-4xl text-foreground-2 md:text-5xl lg:text-7xl">{content.header}</div>
             <div className="flex items-center gap-2 text-xl text-color-2 md:text-2xl lg:text-5xl">
               {content.subHeaderIconSrc ? (
@@ -70,7 +70,7 @@ export const WelcomeWrapper = ({ content, children }: WelcomeWrapperProps) => {
           </div>
 
           {/* Extra row span is for spacing */}
-          <div className="self-center md:col-start-2 md:row-span-5 relative w-full h-[410px]">
+          <div className="self-start md:col-start-2 md:row-span-5 relative w-full h-[410px]">
             <img className="absolute inset-0 w-full h-full" src={content.backgroudIconSrc} alt="Background Icon" />
             {content.stampIcon ? (
               <img className="absolute inset-0 w-64 h-64 m-auto" src={content.stampIcon} alt="Stamp" />
