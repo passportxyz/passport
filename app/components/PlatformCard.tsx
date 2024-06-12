ab995907fa594cfcab40c1d78c62905f3924d8a2
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2024-06-12 10:47:56 +0300

    feat: more styling of platform cards

--- app/components/PlatformCard.tsx
+++ app/components/PlatformCard.tsx
@@ -14,6 +14,7 @@ import { CeramicContext } from "../context/ceramicContext";
 import { useCustomization } from "../hooks/useCustomization";
 import { isDynamicCustomization } from "../utils/customizationUtils";
 import { getStampProviderIds } from "./CardList";
+import { ProgressBar } from "./ProgressBar";
 
 export type SelectedProviders = Record<PLATFORM_ID, PROVIDER_ID[]>;
 
@@ -59,17 +60,13 @@ export const PlatformCard = ({
   // Get the string refering to the eraliest expiration date
   const distanceToExpiration = earliestExpirationDate ? formatDistanceToNow(earliestExpirationDate) : "";
 
-  console.log("geri platformStamps", platformStamps);
-  console.log("geri earliestExpirationDate", earliestExpirationDate);
-  console.log("geri distanceToExpiration", distanceToExpiration);
-
   if (platformIsExcluded) return <></>;
 
   const verified = platform.earnedPoints > 0 || selectedProviders[platform.platform].length > 0;
 
   const platformClasses = verified
-    ? "border-foreground-5 hover:border-foreground-4 hover:to-foreground-5/50 hover:shadow-foreground-4 hover:text-color-2 text-foreground-5"
-    : "border-foreground-6 hover:border-background-3 hover:to-background-2 hover:shadow-background-3";
+    ? "bg-gradient-to-b from-background to-foreground-5/70"
+    : "bg-gradient-to-b from-background to-background-2/70";
 
   const imgFilter = verified
     ? {
@@ -80,7 +77,8 @@ export const PlatformCard = ({
     : {};
 
   // returns a single Platform card
-  return (
+  return verified ? (
+    // The verified state of the card
     <div
       data-testid="platform-card"
       onClick={() => {
@@ -93,15 +91,90 @@ export const PlatformCard = ({
       <div
         onMouseEnter={() => setHovering(true)}
         onMouseLeave={() => setHovering(false)}
-        className={`group relative flex h-full cursor-pointer flex-col rounded-lg border p-0 transition-all ease-out hover:bg-opacity-100 hover:bg-gradient-to-b hover:from-transparent hover:shadow-even-md ${platformClasses}`}
+        className={`group relative flex h-full cursor-pointer flex-col rounded-lg border p-0 transition-all ease-out ${platformClasses}`}
       >
-        {!verified && (
-          <img
-            src="./assets/card-background.svg"
-            alt=""
-            className="absolute bottom-0 right-0 opacity-0 group-hover:opacity-100"
-          />
-        )}
+        <div className="m-6 flex h-full flex-col justify-between">
+          <div className="flex w-full items-center justify-between">
+            {platform.icon ? (
+              <div style={imgFilter}>
+                <img src={platform.icon} alt={platform.name} className={`h-10 w-10 ${verified && "grayscale"}`} />
+              </div>
+            ) : (
+              <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M24.7999 24.8002H28.7999V28.8002H24.7999V24.8002ZM14 24.8002H18V28.8002H14V24.8002ZM3.19995 24.8002H7.19995V28.8002H3.19995V24.8002ZM24.7999 14.0002H28.7999V18.0002H24.7999V14.0002ZM14 14.0002H18V18.0002H14V14.0002ZM3.19995 14.0002H7.19995V18.0002H3.19995V14.0002ZM24.7999 3.2002H28.7999V7.2002H24.7999V3.2002ZM14 3.2002H18V7.2002H14V3.2002ZM3.19995 3.2002H7.19995V7.2002H3.19995V3.2002Z"
+                  fill="var(--color-muted)"
+                />
+              </svg>
+            )}
+            <div className="bg-foreground-4 px-2 py-1 rounded text-right font-alt text-black">
+              <p className="text-xs">Verified</p>
+            </div>
+          </div>
+
+          <div className="mt-4 flex justify-center h-full md:mt-6 md:inline-block md:justify-start">
+            <div
+              className={`flex flex-col place-items-start text-color-2 md:flex-row ${
+                platform.name.split(" ").length > 1 ? "items-center md:items-baseline" : "items-center"
+              }`}
+            >
+              <h1
+                data-testid="platform-name"
+                className={`mr-0 text-xl md:mr-4 ${platform.name.split(" ").length > 1 ? "text-left" : "text-center"}`}
+              >
+                {platform.name}
+              </h1>
+            </div>
+            <p
+              className={`flex-1 pleading-relaxed mt-2 hidden text-sm text-color-1 md:inline-block ${
+                verified ? "invisible" : "visible"
+              } `}
+            >
+              {platform.description}
+            </p>
+          </div>
+
+          <div>Points gained</div>
+          <div className="text-2xl font-bold">{platform.earnedPoints.toFixed(2)}</div>
+          <ProgressBar pointsGained={platform.earnedPoints} pointsAvailable={platform.possiblePoints} />
+        </div>
+        <div className="flex items-center px-4 py-2 border-t border-foreground-4">
+          <div className="flex-none">
+            <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M12 6C12 6.78793 11.8448 7.56815 11.5433 8.2961C11.2417 9.02405 10.7998 9.68549 10.2426 10.2426C9.68549 10.7998 9.02405 11.2417 8.2961 11.5433C7.56815 11.8448 6.78793 12 6 12L6 6H12Z"
+                fill="#C1F6FF"
+              />
+              <circle cx="6" cy="6" r="5.5" stroke="#C1F6FF" />
+            </svg>
+          </div>
+          <div className="flex-1 pl-2 text-foreground-2 override-text-color">{distanceToExpiration} until expiry</div>
+        </div>
+      </div>
+    </div>
+  ) : (
+    // The not-verified state of the card
+    <div
+      data-testid="platform-card"
+      onClick={() => {
+        setCurrentPlatform(platform);
+        onOpen();
+      }}
+      className={className}
+      key={`${platform.name}${i}`}
+    >
+      <div
+        onMouseEnter={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+        className={`group relative flex h-full cursor-pointer flex-col rounded-lg border p-0 transition-all ease-out ${platformClasses}`}
+      >
+        <img
+          src="./assets/card-background.svg"
+          alt=""
+          className="absolute bottom-0 right-0 opacity-0 group-hover:opacity-100"
+        />
         <div className="m-6 flex h-full flex-col justify-between">
           <div className="flex w-full items-center justify-between">
             {platform.icon ? (
@@ -126,7 +199,7 @@ export const PlatformCard = ({
             </div>
           </div>
 
-          <div className="mt-4 flex justify-center md:mt-6 md:inline-block md:justify-start">
+          <div className="mt-4 flex justify-center h-full md:mt-6 md:inline-block md:justify-start">
             <div
               className={`flex flex-col place-items-start text-color-2 md:flex-row ${
                 platform.name.split(" ").length > 1 ? "items-center md:items-baseline" : "items-center"
@@ -140,38 +213,23 @@ export const PlatformCard = ({
               </h1>
             </div>
             <p
-              className={`pleading-relaxed mt-2 hidden text-base text-color-1 md:inline-block ${
+              className={`flex-1 pleading-relaxed mt-2 hidden text-sm text-color-1 md:inline-block ${
                 verified ? "invisible" : "visible"
               } `}
             >
               {platform.description}
             </p>
           </div>
-          {!verified && (
-            <Button
-              data-testid="connect-button"
-              variant="custom"
-              className={`mt-5 w-auto border bg-transparent text-foreground-2 ${
-                verified ? "border-foreground-5 group-hover:border-foreground-4" : "border-foreground-2"
-              }`}
-            >
-              {verified ? "Verified" : "Connect"}
-            </Button>)}
+          <Button
+            data-testid="connect-button"
+            variant="custom"
+            className={`mt-5 w-auto border bg-transparent text-foreground-2 ${
+              verified ? "border-foreground-5 group-hover:border-foreground-4" : "border-foreground-2"
+            }`}
+          >
+            {verified ? "Verified" : "Connect"}
+          </Button>
         </div>
-        {verified && (
-          <div className="flex items-center px-4 py-2 border-t border-foreground-4">
-            <div className="flex-none">
-              <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M12 6C12 6.78793 11.8448 7.56815 11.5433 8.2961C11.2417 9.02405 10.7998 9.68549 10.2426 10.2426C9.68549 10.7998 9.02405 11.2417 8.2961 11.5433C7.56815 11.8448 6.78793 12 6 12L6 6H12Z"
-                  fill="#C1F6FF"
-                />
-                <circle cx="6" cy="6" r="5.5" stroke="#C1F6FF" />
-              </svg>
-            </div>
-            <div className="flex-1 pl-2 text-foreground-2 override-text-color">{distanceToExpiration} until expiry</div>
-          </div>
-        )}
       </div>
     </div>
   );
--- app/components/PlatformDetails.tsx
+++ app/components/PlatformDetails.tsx
@@ -1,17 +1,15 @@
-import React, { Fragment, Ref, useContext, useEffect, useMemo, useState } from "react";
+import React, { Fragment, Ref, useContext, useMemo, useState } from "react";
 import { PlatformBanner, PlatformSpec } from "@gitcoin/passport-platforms";
 import { GenericBanner } from "./GenericBanner";
 import { JsonOutputModal } from "./JsonOutputModal";
 import { CeramicContext } from "../context/ceramicContext";
 
-import { ClockIcon, StarIcon } from "@heroicons/react/20/solid";
 import { ScorerContext } from "../context/scorerContext";
 import { Popover, Transition } from "@headlessui/react";
-import { usePopper } from "react-popper";
 import { RemoveStampModal } from "./RemoveStampModal";
-import { useDisclosure } from "@chakra-ui/react";
 import { STAMP_PROVIDERS } from "../config/providers";
 import { PLATFORM_ID, PROVIDER_ID } from "@gitcoin/passport-types";
+import { ProgressBar } from "./ProgressBar";
 
 const PlatformJsonButton = ({
   platformPassportData,
@@ -91,53 +89,6 @@ const PlatformJsonButton = ({
         platformId={platform.name as PLATFORM_ID}
       />
     </>
-  );
-};
-
-const ProgressBar = ({ pointsGained, pointsAvailable }: { pointsGained: number; pointsAvailable: number }) => {
-  const percentGained = (pointsGained / (pointsGained + pointsAvailable)) * 100 || 0;
-
-  const pageWidth = window.innerWidth;
-
-  const padding = 80; // 40 px on either side
-  const sliderWidth = 332;
-  let fullSliderWidth = 332;
-
-  if (pageWidth < sliderWidth + padding) {
-    fullSliderWidth = pageWidth - padding;
-  }
-
-  const indicatorWidth = fullSliderWidth * (percentGained / 100);
-
-  return (
-    <div className="relative h-6">
-      <svg className="absolute top-1.5 z-0" viewBox="0 8 104 4">
-        <path d="M102,10 L102,10" strokeLinecap="round" strokeWidth={2} stroke="rgb(var(--color-foreground-4))" />
-        <path d="M2,10 L102,10" strokeLinecap="butt" strokeWidth={2} stroke="rgb(var(--color-foreground-4))" />
-        <path d="M2,10 L2,10" strokeLinecap="round" strokeWidth={2} stroke="rgb(var(--color-foreground-4))" />
-      </svg>
-      <svg
-        className="absolute"
-        id="mySvg"
-        width="366"
-        height="26"
-        viewBox="0 0 366 26"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <rect
-          x="2.5"
-          y="2.5"
-          width={indicatorWidth}
-          height="21"
-          rx="10.5"
-          fill="#C1F6FF"
-          stroke="#0E2825"
-          strokeWidth="5"
-          className="transition-all duration-700 ease-in-out"
-        />
-      </svg>
-    </div>
   );
 };
 
