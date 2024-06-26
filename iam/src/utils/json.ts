03e81c500ffadf462b3a98e586c52d00bb9db317
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2024-06-26 16:46:34 +0300

    fix: linter complaints

--- iam/src/utils/json.ts
+++ iam/src/utils/json.ts
@@ -1,7 +1,11 @@
-export const toJsonObject = (obj: any): object => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const toJsonObject = (obj: any): any => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return JSON.parse(JSON.stringify(obj, (key, value) => (typeof value === "bigint" ? value.toString() : value)));
 };
 
-export const serializeJson = (obj: any): string => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const serializeJson = (obj: object): string => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return JSON.stringify(obj, (key, value) => (typeof value === "bigint" ? value.toString() : value));
 };
