0f61bcfef9eb62fb89f01646eee7a3c7a2a4c5f0
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2024-07-02 16:31:58 +0300

    fix: restoring sidebar content

--- app/components/GenericBanner.tsx
+++ app/components/GenericBanner.tsx
@@ -1,18 +1,18 @@
 import { PlatformBanner, Hyperlink } from "@gitcoin/passport-platforms";
 
 export function GenericBanner({ banner }: { banner: PlatformBanner }) {
+  console.log("geri banner", banner);
+  const heading = banner.heading ? <>{banner.heading} </> : null;
   return (
     <div className="mt-8 text-sm">
-      {banner.heading && (
-        <div className="mb-2 font-normal inline-block">
-          {banner.heading}{" "}
-          {banner.cta && (
-            <Hyperlink href={banner.cta.url} className="inline-block underline text-color-5 font-bold">
-              {banner.cta.label}
-            </Hyperlink>
-          )}
-        </div>
-      )}
+      <div className="mb-2 font-normal inline-block">
+        {heading}
+        {banner.cta && (
+          <Hyperlink href={banner.cta.url} className="inline-block underline text-color-5 font-bold">
+            {banner.cta.label}
+          </Hyperlink>
+        )}
+      </div>
       <div className="mb-2 inline-block">{banner.content}</div>
     </div>
   );
