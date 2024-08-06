34766bce115d246c53a6a9ab1bc2b851661a9174
Lucian <lucian@gitcoin.co>
2024-08-06 15:37:23 -0600

    feat(platforms): updating Outdid sidebar (#2765)

--- platforms/src/Outdid/App-Bindings.tsx
+++ platforms/src/Outdid/App-Bindings.tsx
@@ -1,6 +1,8 @@
+import React from "react";
 import axios from "axios";
 import { AppContext, PlatformOptions, ProviderPayload } from "../types";
 import { Platform } from "../utils/platform";
+import { Hyperlink } from "../utils/Hyperlink";
 
 export class OutdidPlatform extends Platform {
   platformId = "Outdid";
@@ -9,8 +11,26 @@ export class OutdidPlatform extends Platform {
   redirectUri: string = null;
 
   banner = {
-    heading:
-      "Outdid is an app which scans the NFC chip of your passport and generates a Zero-Knowledge Proof that you are a unique human. Most importantly, all of your private data stays on your phone - not even Outdid can see it :)",
+    heading: (
+      <>
+        To get the Stamp:
+        <br className="mb-1" />
+        <ol>
+          <li className="mt-1">Step 1: Click the button below &ldquo;Get Stamp&rdquo;</li>
+          <li className="mt-1">
+            Step 2: Select the country of your identity document and the type of document (eg. Germany, passport)
+          </li>
+          <li className="mt-1">Step 3: Scan the QR code, download our app and scan your passport.</li>
+          <li className="mt-1">Step 4: You got your Stamp.</li>
+        </ol>
+        <br />
+        Need help? Message us at <Hyperlink href="http://t.me/outdidsupport">t.me/outdidsupport</Hyperlink>
+        <br />
+        <br />
+        Outdid is an app which scans the NFC chip of your passport and generates a Zero-Knowledge Proof that you are a
+        unique human. All of your private data stays on your phone - not even Outdid can see it 🙂
+      </>
+    ),
     cta: {
       label: "Learn more",
       url: "https://support.passport.xyz/passport-knowledge-base/stamps/how-do-i-add-passport-stamps/guide-to-add-outdid-stamp-to-passport",
