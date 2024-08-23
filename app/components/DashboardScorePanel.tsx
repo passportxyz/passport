93ad39219aa6ca0da6dcc1223e8b56511e7f2d95
Lucian Hymer <lucian@gitcoin.co>
2024-08-23 16:42:33 -0600

    feat(app): wip - adding new score designs and loading animation

--- app/components/DashboardScorePanel.tsx
+++ app/components/DashboardScorePanel.tsx
@@ -1,23 +1,60 @@
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect, useRef } from "react";
 import { ScorerContext } from "../context/scorerContext";
 
 import { useCustomization } from "../hooks/useCustomization";
 import { useAtom } from "jotai";
 import { mutableUserVerificationAtom } from "../context/userState";
+import { setInterval } from "timers";
 
-// Hexagon SVGs generated using https://codepen.io/wvr/pen/WrNgJp
-// with the values listed below for each ring
+const PanelDiv = ({ className, children }: { className: string; children: React.ReactNode }) => {
+  return (
+    <div
+      className={`flex flex-col items-center p-4 w-full rounded-lg bg-gradient-to-t from-background to-foreground-5/[.4] ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
 
-// The SVG path starts before the first corner, but we
-// want it to start at the top of the ring. So, we will
-// cover it with a ring segment that matches the background
-// and account for this earlyStartOffset in our calculations.
-const earlyStartOffset = 6;
+const LoadingScoreImage = () => <div>Low</div>;
 
-// dashes and dashoffset are used to create a progress ring
-const dashLength = 255;
+const SuccessScoreImage = () => <div>Good</div>;
 
-const ScoreRing = ({ className }: { className: string }) => {
+const Ellipsis = () => {
+  const firstDotRef = useRef<HTMLDivElement>(null);
+  const secondDotRef = useRef<HTMLDivElement>(null);
+  const dotIntervalRef = useRef<any>(null);
+
+  const [showFirstDot, setShowFirstDot] = React.useState(false);
+  const [showSecondDot, setShowSecondDot] = React.useState(false);
+
+  useEffect(() => {
+    dotIntervalRef.current = setInterval(() => {
+      setShowFirstDot(true);
+      setTimeout(() => setShowFirstDot(false), 2000);
+      setTimeout(() => {
+        setShowSecondDot(true);
+        setTimeout(() => setShowSecondDot(false), 1000);
+      }, 1000);
+    }, 3000);
+
+    return () => clearInterval(dotIntervalRef.current);
+  }, []);
+
+  return (
+    <div className="flex">
+      .
+      <div ref={firstDotRef} className={showFirstDot ? "visible" : "invisible"}>
+        .
+      </div>
+      <div ref={secondDotRef} className={showSecondDot ? "visible" : "invisible"}>
+        .
+      </div>
+    </div>
+  );
+};
+
+export const DashboardScorePanel = ({ className }: { className: string }) => {
   const { rawScore, passportSubmissionState } = React.useContext(ScorerContext);
   const [verificationState, _setUserVerificationState] = useAtom(mutableUserVerificationAtom);
   const [displayScore, setDisplayScore] = React.useState(0);
@@ -31,95 +68,39 @@ const ScoreRing = ({ className }: { className: string }) => {
     }
   }, [rawScore, verificationState.loading]);
 
-  return (
-    <div className={`${className} grid place-items-center`}>
-      <svg className="col-start-1 row-start-1" width="82" height="94" viewBox="0 0 81.40638795573723 94">
-        {/* progress ring l = 43, r=7 */}
-        <path
-          className="translate-x-[3.5px] translate-y-[4.0px] transition-[stroke-dashoffset] delay-300 duration-1000 ease-in-out"
-          fill="transparent"
-          stroke="currentColor"
-          strokeWidth="8"
-          strokeDasharray={dashLength}
-          strokeDashoffset={dashLength * (1 - displayScore / 100) - earlyStartOffset}
-          d="M31.176914536239785 3.4999999999999996Q37.23909236273086 0 43.30127018922193 3.5L68.41600689897065 18Q74.47818472546172 21.5 74.47818472546172 28.5L74.47818472546172 57.5Q74.47818472546172 64.5 68.41600689897065 68L43.30127018922193 82.5Q37.23909236273086 86 31.17691453623979 82.5L6.06217782649107 68Q0 64.5 0 57.5L0 28.5Q0 21.5 6.062177826491071 18Z"
-        ></path>
-        {/* cover too-early start of progress ring, backdrop combines */}
-        {/* with next ring to match background color l = 43, r=7 */}
-        <path
-          className="translate-x-[3.5px] translate-y-[4.0px]"
-          fill="transparent"
-          stroke={displayScore < 100 ? "rgb(var(--color-background))" : "currentColor"}
-          strokeWidth="8"
-          strokeDasharray={dashLength}
-          strokeDashoffset={dashLength - earlyStartOffset}
-          d="M31.176914536239785 3.4999999999999996Q37.23909236273086 0 43.30127018922193 3.5L68.41600689897065 18Q74.47818472546172 21.5 74.47818472546172 28.5L74.47818472546172 57.5Q74.47818472546172 64.5 68.41600689897065 68L43.30127018922193 82.5Q37.23909236273086 86 31.17691453623979 82.5L6.06217782649107 68Q0 64.5 0 57.5L0 28.5Q0 21.5 6.062177826491071 18Z"
-        ></path>
-        {/* cover too-early start of progress ring, partial transparent color */}
-        {/* combines with previous ring to match color l = 43, r=7 */}
-        <path
-          className="translate-x-[3.5px] translate-y-[4.0px]"
-          fill="transparent"
-          stroke={displayScore < 100 ? "rgb(var(--color-background-4) / .6)" : "currentColor"}
-          strokeWidth="8"
-          strokeDasharray={dashLength}
-          strokeDashoffset={dashLength - earlyStartOffset}
-          d="M31.176914536239785 3.4999999999999996Q37.23909236273086 0 43.30127018922193 3.5L68.41600689897065 18Q74.47818472546172 21.5 74.47818472546172 28.5L74.47818472546172 57.5Q74.47818472546172 64.5 68.41600689897065 68L43.30127018922193 82.5Q37.23909236273086 86 31.17691453623979 82.5L6.06217782649107 68Q0 64.5 0 57.5L0 28.5Q0 21.5 6.062177826491071 18Z"
-        ></path>
-        {/* inner ring l = 38, r=7 */}
-        <path
-          fill="transparent"
-          stroke="currentColor"
-          strokeWidth="2"
-          className="translate-x-[7.8px] translate-y-[9px]"
-          d="M26.846787517317594 3.4999999999999996Q32.90896534380867 0 38.97114317029974 3.5L59.75575286112626 15.5Q65.81793068761733 19 65.81793068761733 26L65.81793068761733 50Q65.81793068761733 57 59.75575286112626 60.5L38.97114317029974 72.5Q32.90896534380867 76 26.846787517317598 72.5L6.06217782649107 60.5Q0 57 0 50L0 26Q0 19 6.062177826491071 15.5Z"
-        ></path>
-        {/* outer ring l = 48, r=7 */}
-        <path
-          fill="transparent"
-          stroke="currentColor"
-          strokeWidth="2"
-          d="M34.64101615137754 3.4999999999999996Q40.703193977868615 0 46.76537180435969 3.4999999999999996L75.34421012924616 20Q81.40638795573723 23.5 81.40638795573723 30.5L81.40638795573723 63.5Q81.40638795573723 70.5 75.34421012924616 74L46.76537180435969 90.5Q40.703193977868615 94 34.64101615137754 90.5L6.062177826491071 74Q0 70.5 0 63.5L0 30.5Q0 23.5 6.062177826491071 20Z"
-        ></path>
-      </svg>
+  const customization = useCustomization();
+  const customTitle = customization?.scorerPanel?.title;
+  // TODO
+  const customText = customization?.scorerPanel?.text;
 
-      <div className="col-start-1 row-start-1 text-2xl">
-        {passportSubmissionState === "APP_REQUEST_PENDING" || verificationState.loading ? (
-          <div className="w-full">
-            <p className="text-sm mt-1/2">Updating</p>
-            <p className="text-sm font-extrabold text-center leading-3">...</p>
-          </div>
+  const loading = true || passportSubmissionState === "APP_REQUEST_PENDING" || verificationState.loading;
+
+  return (
+    <PanelDiv className={className}>
+      <div className="flex items-center w-full">
+        <span className="grow">{customTitle || "Unique Humanity Score"}</span>
+        <div>Tooltip</div>
+      </div>
+      <div className="flex grow items-center align-middle py-4">
+        {loading ? (
+          <>
+            <LoadingScoreImage />
+            <div className="flex">
+              Updating score
+              <Ellipsis />
+            </div>
+          </>
         ) : (
-          <span>{+displayScore.toFixed(2)}</span>
+          <>
+            <SuccessScoreImage />
+            <span>{+displayScore.toFixed(2)}</span>
+          </>
         )}
       </div>
-    </div>
+    </PanelDiv>
   );
 };
 
-export const DashboardScorePanel = ({ className }: { className: string }) => {
-  const customization = useCustomization();
-  const customTitle = customization?.scorerPanel?.title;
-  const customText = customization?.scorerPanel?.text;
-
-  return (
-    <div
-      className={`${className} flex flex-col border  border-foreground-6 rounded bg-gradient-to-b from-background to-background-2`}
-    >
-      <div className="flex p-4 border-b border-foreground-6">
-        <img alt="Person Icon" className="mr-2" src="/assets/personIcon.svg" />
-        <span>{customTitle || "Default Humanity Score"}</span>
-      </div>
-      <div className="flex grow items-center text-foreground-2">
-        <div className="border-r border-foreground-6 p-4 h-full flex items-center">
-          <ScoreRing className="shrink-0" />
-        </div>
-
-        <p className="shrink p-4 text-foreground">
-          {customText ||
-            "Your Unique Humanity Score measures how unique and human you are. Passport-protected apps use this score to ensure authentic engagement. Most applications require a score of at least 20 to participate. Scores can vary across apps, especially due to misuse or attacks."}
-        </p>
-      </div>
-    </div>
-  );
+export const DashboardScoreExplanationPanel = ({ className }: { className: string }) => {
+  return <div />;
 };
