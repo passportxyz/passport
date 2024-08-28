9d88d636ebbc30541177cc778be79fce7f4ec04b
schultztimothy <schultz.timothy52@gmail.com>
2024-08-28 10:33:37 -0600

    feat: remov gitcoin logo

--- app/components/MinimalHeader.tsx
+++ app/components/MinimalHeader.tsx
@@ -12,7 +12,6 @@ type MinimalHeaderProps = {
 const getAssets = () => {
   return {
     passportLogo: "/assets/passportLogoWhite.svg",
-    gitcoinLogo: "/assets/gitcoinLogoWhite.svg",
     logoLine: "/assets/logoLine.svg",
     emphasisColor: "white",
   };
@@ -28,8 +27,6 @@ const MinimalHeader = ({ className }: MinimalHeaderProps): JSX.Element => {
       <OnchainSidebar isOpen={showSidebar} onClose={() => setShowSidebar(false)} />
       <div className={`flex items-center h-16 ${className}`}>
         <div className="flex-1 flex items-center">
-          <img className="" src={assets.gitcoinLogo} alt="Gitcoin Logo" />
-          <img className="mx-3 md:mx-6" src={assets.logoLine} alt="Logo Line" />
           <img className="h-8" src={assets.passportLogo} alt="Passport Logo" />
           <div className="ml-3 hidden md:inline-block text-2xl text-color-1">Passport</div>
         </div>
