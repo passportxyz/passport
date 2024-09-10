02dc4f5f98da87fb2b04c43e942e49b31b686a2e
schultztimothy <schultz.timothy52@gmail.com>
2024-09-10 17:33:40 -0600

    fix: get confetti dimensions on render

--- app/components/Confetti.tsx
+++ app/components/Confetti.tsx
@@ -5,48 +5,21 @@ import { useAtom } from "jotai";
 import { ScorerContext } from "../context/scorerContext";
 import { colors } from "../utils/theme/palette";
 
-const useWindowDimensions = () => {
-  const getDimensions = useCallback(() => {
-    const width = document.documentElement.clientWidth;
-    const scrollableHeight = document.body.offsetHeight;
-    return {
-      width,
-      height: scrollableHeight,
-    };
-  }, []);
-
-  const [windowDimensions, setWindowDimensions] = useState(getDimensions());
-
-  useEffect(() => {
-    const onResize = () => {
-      setWindowDimensions(getDimensions());
-    };
-
-    // run on mount to set initial state
-    onResize();
-
-    // remove existing event listeners
-    window.removeEventListener("resize", onResize);
-
-    // add listeners
-    // passive stops the browser from waiting to see if the event
-    // listener will call preventDefault() -- better for performance
-    window.addEventListener("resize", onResize, { passive: true });
-
-    // clean up on dismount
-    return () => {
-      window.removeEventListener("resize", onResize);
-    };
-  }, [getDimensions]);
-
-  return { windowDimensions };
+const getDimensions = () => {
+  const width = document.documentElement.clientWidth;
+  const scrollableHeight = document.body.offsetHeight;
+  return {
+    width,
+    height: scrollableHeight,
+  };
 };
 
 export const Confetti: React.FC = () => {
   const [verificationState] = useAtom(mutableUserVerificationAtom);
-  const { windowDimensions } = useWindowDimensions();
   const [showConfetti, setShowConfetti] = useState(false);
   const { rawScore, threshold } = useContext(ScorerContext);
+
+  const currentDimensions = getDimensions();
 
   useEffect(() => {
     setShowConfetti(!verificationState.loading && rawScore >= threshold);
@@ -59,16 +32,16 @@ export const Confetti: React.FC = () => {
   return (
     <div data-testid="react-confetti">
       <ReactConfetti
-        width={windowDimensions.width}
-        height={windowDimensions.height - 100}
+        width={currentDimensions.width}
+        height={currentDimensions.height - 100}
         numberOfPieces={4000}
         recycle={false}
         run={true}
         confettiSource={{
           x: 0,
           y: 0,
-          w: windowDimensions.width,
-          h: windowDimensions.height,
+          w: currentDimensions.width,
+          h: currentDimensions.height,
         }}
         colors={Object.values(colors)}
       />
