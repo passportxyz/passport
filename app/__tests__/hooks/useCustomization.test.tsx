553ea878c861560b5180ba973d518b577a9550b7
schultztimothy <schultz.timothy52@gmail.com>
2024-09-11 17:08:43 -0600

    fix(app): dont skip

--- app/__tests__/hooks/useCustomization.test.tsx
+++ app/__tests__/hooks/useCustomization.test.tsx
@@ -119,7 +119,7 @@ describe("useCustomization", () => {
     });
   });
 
-  it.skip("should cleanly ignore invalid customizationKey", async () => {
+  it("should cleanly ignore invalid customizationKey", async () => {
     render(<TestingComponent customizationKey="invalid" />);
     await waitFor(() => {
       expect(document.querySelector("[data-testid=customizationKey]")?.textContent).toBe(DEFAULT_CUSTOMIZATION_KEY);
