23ea8d2a11a7dfeb268fb62fd67ae96c0bf28291
Lucian Hymer <lucian@gitcoin.co>
2024-09-18 08:15:57 -0600

    fix(platforms): update default platform name for dev list

--- platforms/src/CustomGithub/Providers-config.ts
+++ platforms/src/CustomGithub/Providers-config.ts
@@ -4,7 +4,7 @@ import { CustomGithubProvider } from "./Providers";
 export const PlatformDetails: PlatformSpec = {
   icon: "./assets/dev-icon.svg",
   platform: "DeveloperList",
-  name: "Custom Github Stamp",
+  name: "Developer List",
   description: "Verify you are part of a community",
   connectMessage: "Verify",
   isEVM: false,
