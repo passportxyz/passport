011317eb68ac05b3134e33806b246eadd67a92d0
Tim <6887938+tim-schultz@users.noreply.github.com>
2024-10-31 08:35:24 -0600

    chore(platforms): update link (#3016)

--- platforms/src/Gitcoin/App-Bindings.tsx
+++ platforms/src/Gitcoin/App-Bindings.tsx
@@ -15,7 +15,10 @@ export class GitcoinPlatform extends Platform {
       <div>
         To qualify for the Gitcoin Stamp, your contributions must be made to grant rounds officially funded by Gitcoin.
         This includes designated rounds such as Citizens rounds, GG18, GG19, among others, as detailed{" "}
-        <Hyperlink href="https://github.com/ufkhan97/gitcoin-grants-heroku/blob/main/all_rounds.csv">here</Hyperlink>.
+        <Hyperlink href="https://github.com/ufkhan97/gitcoin-grants-heroku/blob/main/data/all_rounds.csv">
+          here
+        </Hyperlink>
+        .
       </div>
     ),
     content: (
