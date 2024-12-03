068a473f0ecfefb14ed32c260a8413e3ff8b70a3
Lucian Hymer <lucian@gitcoin.co>
2024-12-03 08:32:59 -0700

    feat(app): adding ENV var for resetting wallet connection

--- app/components/PageRoot.tsx
+++ app/components/PageRoot.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDisconnect } from "wagmi";
 
-const PageRoot = ({ children, className }: { children: React.ReactNode; className?: string }) => (
-  <div className={`bg-background font-body ${className}`}>{children}</div>
-);
+// Resets wallet connection if the ENV var is incremented
+const useResetWalletConnection = () => {
+  const { disconnect } = useDisconnect();
+
+  useEffect(() => {
+    const savedWalletResetIndex = localStorage.getItem("walletResetIndex");
+    const currentWalletResetIndex = process.env.NEXT_PUBLIC_WALLET_CONNECTION_RESET_INDEX || "";
+
+    if (currentWalletResetIndex && currentWalletResetIndex !== savedWalletResetIndex) {
+      localStorage.setItem("walletResetIndex", currentWalletResetIndex);
+      try {
+        disconnect();
+      } catch {}
+    }
+  }, [disconnect]);
+};
+
+const PageRoot = ({ children, className }: { children: React.ReactNode; className?: string }) => {
+  useResetWalletConnection();
+
+  return <div className={`bg-background font-body ${className}`}>{children}</div>;
+};
 
 export default PageRoot;
