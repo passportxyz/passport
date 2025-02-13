9983efb0a377b0c700fabbf0b994ff3355792108
Lucian Hymer <lucian@gitcoin.co>
2025-02-13 15:05:44 -0700

    fix: jest fixes

--- embed/jest.config.ts
+++ embed/jest.config.ts
@@ -5,11 +5,11 @@ const config: JestConfigWithTsJest = {
   preset: "ts-jest",
   testEnvironment: "node",
   moduleNameMapper: {
-    "@gitcoin/passport-identity": "<rootDir>/../node_modules/@gitcoin/passport-identity/dist/esm/index.js",
+    "^@gitcoin/passport-identity$": "<rootDir>/../node_modules/@gitcoin/passport-identity/dist/esm/index.js",
     "^multiformats/(.*)$": "<rootDir>/../node_modules/multiformats/dist/src/$1.js",
     "^multiformats$": "<rootDir>/../node_modules/multiformats/dist/index.min.js",
-    "@ipld/dag-cbor": "<rootDir>/../node_modules/@ipld/dag-cbor/esm/index.js",
-    uint8arrays: "<rootDir>/../node_modules/uint8arrays/dist/index.min.js",
+    "^@ipld/dag-cbor$": "<rootDir>/../node_modules/@ipld/dag-cbor/esm/index.js",
+    "^uint8arrays(/|$)": "<rootDir>/../node_modules/uint8arrays/dist/index.min.js",
     "^(\.\.?/.*)\\.js$": "$1",
   },
   transform: {
