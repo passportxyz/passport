4af3763309540e28a16ea48f16745a8ba9ede91f
larisa17 <larisabucur17@gmail.com>
2025-02-17 19:03:33 +0200

    feat(useLoginFlow): add more detail logs

--- app/hooks/useLoginFlow.tsx
+++ app/hooks/useLoginFlow.tsx
@@ -25,7 +25,7 @@ export const useLoginFlow = ({
   isLoggingIn: boolean;
   signIn: () => void;
 } => {
-  const { address, isConnected } = useAccount();
+  const { address, isConnected, connector } = useAccount();
   const { data: walletClient } = useWalletClient();
   const { open: web3ModalIsOpen } = useAppKitState();
   const { disconnect } = useDisconnect();
@@ -47,10 +47,13 @@ export const useLoginFlow = ({
   }, []);
 
   useEffect(() => {
+    console.error("Debug Login Flow loginStep: ", loginStep);
+    console.error("Debug Login Flow web3modalEvent: ", web3modalEvent);
+    console.error("Debug Login Flow resetLogin: ", resetLogin);
     if (web3modalEvent.data.event === "MODAL_CLOSE" && web3modalEvent.data.properties.connected === false) {
       resetLogin();
     }
-  }, [web3modalEvent, resetLogin]);
+  }, [web3modalEvent, resetLogin, loginStep]);
 
   const showConnectionError = useCallback(
     (e: any) => {
@@ -58,17 +61,23 @@ export const useLoginFlow = ({
         title: "Connection Error",
         message: (e as Error).message,
       });
+
     },
     [failure]
   );
 
   useEffect(() => {
+    console.error("Debug Login Flow enabled: ", enabled);
+    console.error("Debug Login Flow isConnected: ", isConnected);
+    console.error("Debug Login Flow dbAccessTokenStatus: ", dbAccessTokenStatus);
+
     const newLoginStep = (() => {
       if (!enabled) return "NOT_STARTED";
       else if (!isConnected) return "PENDING_WALLET_CONNECTION";
       else if (dbAccessTokenStatus !== "connected") return "PENDING_DATABASE_CONNECTION";
       else return "DONE";
     })();
+    console.error("Debug Login Flow newLoginStep: ", newLoginStep);
     setLoginStep(newLoginStep);
   }, [enabled, isConnected, dbAccessTokenStatus]);
 
@@ -76,6 +85,8 @@ export const useLoginFlow = ({
   // the dashboard, the web3ModalIsOpen state is incorrect
   // until we call disconnect
   useEffect(() => {
+    console.error("Debug Login Flow web3ModalIsOpen: ", web3ModalIsOpen);
+    console.error("Debug Login Flow loginStep: ", loginStep);
     (async () => {
       if (web3ModalIsOpen && loginStep === "NOT_STARTED") {
         try {
@@ -85,9 +96,11 @@ export const useLoginFlow = ({
         }
       }
     })();
-  }, [web3ModalIsOpen, loginStep]);
+  }, [web3ModalIsOpen, loginStep, disconnect]);
 
   useEffect(() => {
+    console.error("Debug Login Flow loginStep: ", loginStep);
+    console.error("Debug Login Flow navigateToPage: ", navigateToPage);
     if (loginStep === "DONE") {
       if (onLoggedIn) {
         onLoggedIn();
@@ -99,9 +112,16 @@ export const useLoginFlow = ({
         }
       }
     }
-  }, [loginStep, navigateToPage]);
+  }, [loginStep, navigateToPage, onLoggedIn]);
 
   useEffect(() => {
+    console.error("Debug Login Flow loginStep: ", loginStep);
+    console.error("Debug Login Flow address: ", address);
+    console.error("Debug Login Flow connector: ", connector?.name);
+    console.error("Debug Login Flow walletClient: ", walletClient);
+    console.error("Debug Login Flow connectDatastore: ", connectDatastore);
+    console.error("Debug Login Flow showConnectionError: ", showConnectionError);
+    console.error("Debug Login Flow resetLogin: ", resetLogin);
     (async () => {
       if (
         !isConnectingToDatabaseRef.current &&
@@ -126,6 +146,11 @@ export const useLoginFlow = ({
   const isLoggingIn = loginStep !== "DONE" && loginStep !== "NOT_STARTED";
 
   const signIn = useCallback(async () => {
+    console.error("Debug Login Flow initiateLogin: ", initiateLogin);
+    console.error("Debug Login Flow isConnected: ", isConnected);
+    console.error("Debug Login Flow openWeb3Modal: ", openWeb3Modal);
+    console.error("Debug Login Flow showConnectionError: ", showConnectionError);
+    console.error("Debug Login Flow resetLogin: ", resetLogin);
     try {
       initiateLogin();
       if (!isConnected) {
@@ -136,7 +161,7 @@ export const useLoginFlow = ({
       showConnectionError(e);
       resetLogin();
     }
-  }, [openWeb3Modal, isConnected, showConnectionError]);
+  }, [initiateLogin, isConnected, openWeb3Modal, showConnectionError, resetLogin]);
 
   return useMemo(() => ({ loginStep, isLoggingIn, signIn }), [loginStep, isLoggingIn, signIn]);
 };
