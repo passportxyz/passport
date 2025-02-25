ae1f76cde14f28946d271bd844de88527da66d8a
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2025-02-25 14:32:21 +0200

    feat: removing more env vars from embed and iam example files

--- embed/src/jest.setup.ts
+++ embed/src/jest.setup.ts
@@ -3,7 +3,6 @@ import DIDKit from "@spruceid/didkit-wasm-node";
 process.env.IAM_JWK = DIDKit.generateEd25519Key();
 process.env.IAM_JWK_EIP712 =
   '{"kty":"EC","crv":"secp256k1","x":"PdB2nS-knyAxc6KPuxBr65vRpW-duAXwpeXlwGJ03eU","y":"MwoGZ08hF5uv-_UEC9BKsYdJVSbJNHcFhR1BZWer5RQ","d":"z9VrSNNZXf9ywUx3v_8cLDhSw8-pvAT9qu_WZmqqfWM"}';
-process.env.GITCOIN_VERIFIER_CHAIN_ID = "84531";
 process.env.SCORER_ENDPOINT = "http://127.0.0.1:8002";
 process.env.SCORER_API_KEY = "abcd";
 process.env.SCROLL_BADGE_PROVIDER_INFO =
