f00f875c212285a1f629e4b35cd157ca48e93980
nutrina <8137830+nutrina@users.noreply.github.com>
2025-05-05 13:10:41 +0300

    3350 create new stamp provider for proof of clean hands 2 (#3447)
    * fix: updating copy + changing the query for attestations
    
    * fix: tests for CleanHands

--- platforms/src/CleanHands/App-Bindings.tsx
+++ platforms/src/CleanHands/App-Bindings.tsx
@@ -9,11 +9,15 @@ export class CleanHandsPlatform extends Platform {
   path = "clean_hands";
   isEVM = true;
   banner = {
-    heading: "Privately prove you are not sanctioned using Proof of Clean Hands",
     content: (
-      <>
-        To add the Clean Hands Stamp to your Passport...{" "}
-        <Hyperlink href="">Learn more - TODO (link to new knowledge base article)</Hyperlink>
+      <div>
+        <Hyperlink href="https://support.passport.xyz/passport-knowledge-base/stamps/how-do-i-add-passport-stamps/the-proof-of-clean-hands-stamp">
+          Clean Hands Stamp Guide
+        </Hyperlink>
+        <br />
+        <br />
+        To add the Clean Hands Stamp to your Passport:
+        <br />
         <ul style={{ listStyleType: "disc", paddingLeft: "20px" }}>
           <li>
             Have a smartphone and an Ethereum wallet with $5 in ETH (mainnet, OP, or Aurora), AVAX, or FTM for Clean
@@ -28,18 +32,16 @@ export class CleanHandsPlatform extends Platform {
           </li>
           <li>After verification, mint the SBT to your wallet, then link it to your Passport by verifying it.</li>
         </ul>
-        <br></br>
+        <br />
+        <br />
         Check the Sign attestation protocol to validate:
+        <br />
         <ul style={{ listStyleType: "disc", paddingLeft: "20px" }}>
           <li>Default to checking all 3 chains</li>
           <li>End users will only be able to mint Passport on OP (out of Sui, Near & OP)</li>
         </ul>
-      </>
+      </div>
     ),
-    cta: {
-      label: "Learn more",
-      url: "https://support.passport.xyz/passport-knowledge-base/stamps/how-do-i-add-passport-stamps/connecting-snapshot-to-passport",
-    },
   };
 
   async getProviderPayload(appContext: AppContext): Promise<ProviderPayload> {
