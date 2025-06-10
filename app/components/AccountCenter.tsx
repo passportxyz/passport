624d08b5c4c5cb3c041a9a15ca9ad7324d54a9e6
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2025-06-10 12:53:22 +0300

    feat: updating the top app bar

--- app/components/AccountCenter.tsx
+++ app/components/AccountCenter.tsx
@@ -7,7 +7,7 @@ export const AccountCenter = () => {
 
   return (
     <div
-      className={`fixed top-3 rounded-2xl w-fit h-fit bg-background z-10 flex justify-end ${verificationComplete ? "right-14 md:right-20 lg:right-36" : "right-2 md:right-10 lg:right-20"}`}
+      className={`rounded-2xl w-fit h-fit bg-background lex justify-end ${verificationComplete ? "right-14 md:right-20 lg:right-36" : "right-2 md:right-10 lg:right-20"}`}
     >
       <w3m-button balance="hide" size="sm" />
     </div>
