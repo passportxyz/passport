992822526b3c077e716fdef95842d7d0b6fbf808
Rute Figueiredo <rfigueiredo.dev@gmail.com>
2025-06-28 22:27:05 +0100

    fix: exports

--- platforms/src/ZKEmail/types.ts
+++ platforms/src/ZKEmail/types.ts
@@ -4,3 +4,11 @@ export const AMAZON_HEAVY_USER_THRESHOLD = 50;
 export const UBER_OCCASIONAL_RIDER_THRESHOLD = 3;
 export const UBER_REGULAR_RIDER_THRESHOLD = 25;
 export const UBER_POWER_USER_THRESHOLD = 75;
+
+// TODO: we will get this dynamically from the sdk
+export const UBER_BLUEPRINTS = ["94f5ba10-2670-4b5c-9e91-56f06246d176", "f9de1c4a-b90c-47af-941f-d21a0ecf1411"];
+export const AMAZON_BLUEPRINTS = [
+  "07d83b53-67ce-48b7-96c1-07d26cd3cdef",
+  "b28b7734-e57b-4e3d-8044-0da1e20f782f",
+  "5d0cee90-7805-4052-8eb9-6c71bc969806",
+];
