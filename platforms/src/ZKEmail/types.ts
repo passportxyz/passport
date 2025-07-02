532815e070304d61d58cf2e46325927ba95533a6
Rute Figueiredo <rfigueiredo.dev@gmail.com>
2025-07-02 10:14:52 +0100

    feat: get blueprints by group and explicit google authorization consent

--- platforms/src/ZKEmail/types.ts
+++ platforms/src/ZKEmail/types.ts
@@ -5,10 +5,5 @@ export const UBER_OCCASIONAL_RIDER_THRESHOLD = 3;
 export const UBER_REGULAR_RIDER_THRESHOLD = 25;
 export const UBER_POWER_USER_THRESHOLD = 75;
 
-// TODO: we will get this dynamically from the sdk
-export const UBER_BLUEPRINTS = ["94f5ba10-2670-4b5c-9e91-56f06246d176", "f9de1c4a-b90c-47af-941f-d21a0ecf1411"];
-export const AMAZON_BLUEPRINTS = [
-  "07d83b53-67ce-48b7-96c1-07d26cd3cdef",
-  "b28b7734-e57b-4e3d-8044-0da1e20f782f",
-  "5d0cee90-7805-4052-8eb9-6c71bc969806",
-];
+export const UBER_GROUP = "86de72c1-bd22-4f56-b120-eaf18e33eeeb";
+export const AMAZON_GROUP = "0d462a92-8872-4a48-a557-05e20c493408";
