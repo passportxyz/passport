8dde17e2ad332a18bee10b1782fb1cff7710cbc8
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2025-07-03 12:02:12 +0300

    fix: react warnings

--- app/components/CancelButton.tsx
+++ app/components/CancelButton.tsx
@@ -1,7 +1,7 @@
 export const CancelButton = ({ onClose, className }: { onClose: () => void; className?: string }) => (
   <button onClick={onClose} className={className}>
     <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M24 8L8 24M8 8L24 24" stroke="#737373" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+      <path d="M24 8L8 24M8 8L24 24" stroke="#737373" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
     </svg>
   </button>
 );
